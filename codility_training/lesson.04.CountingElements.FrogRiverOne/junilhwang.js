06e3f764c7bec7e9e4ad1142c388b12cba86c21a
junilHwang <tjsdlf4261@naver.com>
2020-04-25 20:33:45 +0900

    코딜리티 - Lessons - 04.CountingElements - FrogRiverOne 해결 완료

--- codility_training/lesson.04.CountingElements.FrogRiverOne/junilhwang.js
+++ codility_training/lesson.04.CountingElements.FrogRiverOne/junilhwang.js
@@ -8,9 +8,9 @@ function solution(X, A) {
   return -1
 }
 
-// console.log(solution(1, [1]) === 0)
-// console.log(solution(5, [1, 3, 1, 4, 2, 3, 5, 4]) === 6)
-// console.log(solution(5, [1, 2, 3, 4, 4]) === -1)
-// console.log(solution(4, [1, 2, 3, 4, 4]) === 3)
-// console.log(solution(4, [1, 2, 3, 4, 5, 6, 7, 8]) === 3)
+console.log(solution(1, [1]) === 0)
+console.log(solution(5, [1, 3, 1, 4, 2, 3, 5, 4]) === 6)
+console.log(solution(5, [1, 2, 3, 4, 4]) === -1)
+console.log(solution(4, [1, 2, 3, 4, 4]) === 3)
+console.log(solution(4, [1, 2, 3, 4, 5, 6, 7, 8]) === 3)
 console.log(solution(3, [1, 3, 1, 3, 2, 1, 3]) === 4)

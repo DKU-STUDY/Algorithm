c42e51bf2ea5826f69686a703a29288b48ddc35e
junilHwang <tjsdlf4261@naver.com>
2020-04-25 20:32:47 +0900

    코딜리티 - Lessons - 04.CountingElements - FrogRiverOne 풀이 중 / 시간복잡도 계산

--- codility_training/lesson.04.CountingElements.FrogRiverOne/junilhwang.js
+++ codility_training/lesson.04.CountingElements.FrogRiverOne/junilhwang.js
@@ -1,16 +1,19 @@
 function solution(X, A) {
-  const set = new Set()
+  const arr = [...new Array(X).keys()].map(v => false) // O(n)
   const len = A.length
-  for (let i = 0; i < len; i++) {
-    set.add(A[i])
-    if (set.size === X) return i
+  for (let i = 0; i < len; i++) { // O(n)
+    const v = A[i]
+    arr[v - 1] = true
+    if (arr.indexOf(false) === -1) return i // O(n)
   }
+
+  // 최종 : O(n^2)
   return -1
 }
 
-console.log(solution(1, [1]) === 0)
-console.log(solution(5, [1, 3, 1, 4, 2, 3, 5, 4]) === 6)
-console.log(solution(5, [1, 2, 3, 4, 4]) === -1)
-console.log(solution(4, [1, 2, 3, 4, 4]) === 3)
-console.log(solution(4, [1, 2, 3, 4, 5, 6, 7, 8]) === 3)
+// console.log(solution(1, [1]) === 0)
+// console.log(solution(5, [1, 3, 1, 4, 2, 3, 5, 4]) === 6)
+// console.log(solution(5, [1, 2, 3, 4, 4]) === -1)
+// console.log(solution(4, [1, 2, 3, 4, 4]) === 3)
+// console.log(solution(4, [1, 2, 3, 4, 5, 6, 7, 8]) === 3)
 console.log(solution(3, [1, 3, 1, 3, 2, 1, 3]) === 4)

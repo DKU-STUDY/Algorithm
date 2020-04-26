cc4ec255125c5fa4f418bc88daaa4196d2b36f1f
junil-hwang <tjsdlf4261@naver.com>
2020-04-26 20:31:45 +0900

    codility - lesson04 - Counting Elements - MaxCounters 효율성 통과

--- codility_training/lesson.04.CountingElements.MaxCounters/junilhwang.js
+++ codility_training/lesson.04.CountingElements.MaxCounters/junilhwang.js
@@ -1,15 +1,20 @@
 function solution(N, A) {
   const stack = new Array(N).fill(0)
-  let max = 0
+  let start = 0
+  let max = start
+
   for (const v of A) {
+    console.log(stack)
     if (N < v) {
-      stack.forEach((v, k) => stack[k] = max)
+      start = max
       continue
     }
-    stack[v-1] += 1
-    max = Math.max(stack[v-1], max)
+    stack[v - 1] = Math.max(stack[v - 1], start) + 1
+    max = Math.max(stack[v - 1], max)
   }
-  return stack
+  const result = stack.map( v => v < start ? start : v )
+  console.log(result)
+  return result
 }
 
 console.log(

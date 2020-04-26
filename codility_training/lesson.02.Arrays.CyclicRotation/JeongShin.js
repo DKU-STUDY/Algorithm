abe63ed7514a0c3a85ac2762294815cc3f0989ef
JeongShin <64301935+JeongShin@users.noreply.github.com>
2020-04-26 11:44:22 +0900

    Update JeongShin.js

--- codility_training/lesson.02.Arrays.CyclicRotation/JeongShin.js
+++ codility_training/lesson.02.Arrays.CyclicRotation/JeongShin.js
@@ -1,7 +1,3 @@
 function solution(A, K) {
-    return arr.slice(A.length-K).concat(arr.slice(0,A.length-K));
+    return A.slice(A.length-K).concat(ActiveXObject.slice(0,A.length-K));
 }
-var arr=[1,2,3,4];
-let arr2=solution(arr, 3);
-
-console.log(arr2);

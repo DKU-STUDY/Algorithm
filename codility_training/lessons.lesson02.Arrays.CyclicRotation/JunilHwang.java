38fca6030a7ac94c77f2ea6181f324368b1f6d57
junilhwang <tjsdlf4261@naver.com>
2020-04-27 13:48:26 +0900

    refactor: CyclicRotation

--- codility_training/lessons.lesson02.Arrays.CyclicRotation/JunilHwang.java
+++ codility_training/lessons.lesson02.Arrays.CyclicRotation/JunilHwang.java
@@ -1,18 +1,14 @@
 package lessons.lesson02.Arrays.CyclicRotation;
 
 import java.util.Arrays;
-import java.util.stream.Collectors;
 
 public class JunilHwang {
   public static int[] solution(int[] A, int K) {
     int len = A.length;
-    if (len == 0 || len == K || K == 0) return A;
     int[] newA = new int[A.length];
-    K = K % A.length;
     for (int i = 0; i < len; i++) {
       newA[i] = A[(len - K + i) % len];
     }
-    System.out.println(Arrays.stream(newA).boxed().collect(Collectors.toList()));
     return newA;
   }
 
@@ -28,5 +24,13 @@ public class JunilHwang {
     int[] A3 = {5, -1000};
     int[] Sol3 = {-1000, 5};
     System.out.println(Arrays.equals(solution(A3,1), Sol3));
+
+    int[] A4 = {3,8,9,7,6};
+    int[] Sol4 = {3,8,9,7,6};
+    System.out.println(Arrays.equals(solution(A4,5), Sol4));
+
+    int[] A5 = {3,8,9,7,6};
+    int[] Sol5 = {3,8,9,7,6};
+    System.out.println(Arrays.equals(solution(A5,0), Sol5));
   }
 }

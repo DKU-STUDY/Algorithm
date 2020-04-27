561dba2a262d5239c2b80a11118effb578f7e04e
junilhwang <tjsdlf4261@naver.com>
2020-04-27 13:24:26 +0900

    feat: CyclicRotation 완료 (java)

--- codility_training/lessons.lesson02.Arrays.CyclicRotation/JunilHwang.java
+++ codility_training/lessons.lesson02.Arrays.CyclicRotation/JunilHwang.java
@@ -1,24 +1,22 @@
-package lesson02.Arrays.CyclicRotation;
+package lessons.lesson02.Arrays.CyclicRotation;
+
+import java.util.Arrays;
+import java.util.stream.Collectors;
 
 public class JunilHwang {
-  public static int solution(int n) {
-    String b = Integer.toBinaryString(n);
-    int i = 0, max = 0, len = b.length(), idx; // idx는 일단 정의만
-    while (i < len) { // 반복 횟수를 최대한 줄이기
-      idx = b.indexOf("1", i + 1);
-      if (idx == -1) break;
-      max = Math.max(max, idx - i - 1);
-      i = idx;
-    }
-    return max;
+  public static int[] solution(int[] A, int K) {
+    K = K % A.length;
+    int len = A.length;
+    int[] newA = new int[A.length];
+    for (int i = 0, j = K - 1; i < K; i++, j++) newA[i] = A[j];
+    for (int i = K, j = 0; i < len; i++, j++) newA[i] = A[j];
+    System.out.println( Arrays.stream(newA).boxed().collect(Collectors.toList()));
+    return newA;
   }
 
   public static void main(String[] args) {
-    System.out.println(solution(32) == 0);
-    System.out.println(solution(1041) == 5);
-    System.out.println(solution(9) == 2);
-    System.out.println(solution(529) == 4);
-    System.out.println(solution(20) == 1);
-    System.out.println(solution(15) == 0);
+    int[] A = {3,8,9,7,6};
+    int[] Sol = {9,7,6,3,8};
+    System.out.println(Arrays.equals(solution(A,3), Sol));
   }
 }

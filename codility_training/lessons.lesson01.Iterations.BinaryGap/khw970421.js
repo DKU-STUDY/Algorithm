de2a6a4335985d666fd318da86e25c593dad580d
박은영 <31977543+eyabc@users.noreply.github.com>
2020-04-27 20:50:45 +0900

    Update khw970421.js

--- codility_training/lessons.lesson01.Iterations.BinaryGap/khw970421.js
+++ codility_training/lessons.lesson01.Iterations.BinaryGap/khw970421.js
@@ -1,16 +1,17 @@
+  
 function solution(N) {
-    var a=[];               //배열생성
-    var max_result=0;
+    let a=[];               //배열생성
+    let max_result=0;
     
-     for(var i=0;N>=1;i++)
+     for(let i=0;N>=1;i++)
      {
         a[i]=N%2;
         N=(N-N%2)/2;                  //a배열에 0과1의 이진수 값들 각각 넣기
      }
-     
-     for(var j=0;j<a.length;j++)     //a배열 크기만큼 반복
+     const a_length = a.length;
+     for(let j=0;j<a_length;j++)     //a배열 크기만큼 반복
      {
-         var sum=0,p=1;             //sum 과 p는 j가 바뀔때마다 새로 설정
+         let sum=0,p=1;             //sum 과 p는 j가 바뀔때마다 새로 설정
          
          if(a[j]==1)                //어떤 a[j]가 1이라면
          {
@@ -24,10 +25,8 @@ function solution(N) {
              while(a[j+p]==0)
          }
          
-         if(max_result<sum)
-         {
-             max_result=sum;            //가장 큰 gap 차이 max_result에 넣기
-         }
+         if(max_result<sum) max_result=sum;            //가장 큰 gap 차이 max_result에 넣기
+         
      }
     return max_result;
 }

e256a0497a14a5790786af6df4a1096dade09dd9
junilhwang <tjsdlf4261@naver.com>
2020-04-27 10:07:19 +0900

    feat: Codility.Lesson01.Iterations.BinaryGap (Java)

--- codility_training/lesson.01.Iterations.BinaryGap/JunilHwang.java
+++ codility_training/lesson.01.Iterations.BinaryGap/JunilHwang.java
@@ -1,25 +1,15 @@
 import java.util.Arrays;
 
 public class JunilHwang {
-    public int solution(int n) {
-      if (n == 1) return 0; // 1 = 0
-
-      int tempGap[] = { 0, 0 };
-
-      /*return Arrays
-        .asList(Integer.toBinaryString(n).toCharArray()) // binary로 변환 후 char[] 배열 생성 및 List로 변환
-        .stream()
-        .reduce(tempGap, (start, v) -> { // reduce 내부에서는 외부 변수를 사용할 수 없음
-          if(v.equals('0')) {
-              start[0] += 1; // count 0
-          }
-          else if(v.equals('1')) // end
-          {
-              start[1] = Math.max(start[0], start[1]);
-              start[0] = 0;
-          }
-          return start;
-        })*/
-      return 0;
+  public static int solution(int n) {
+    String b = Integer.toBinaryString(n);
+    int i = 0, max = 0, len = b.length();
+    while (i < len) {
+      int idx = b.indexOf("1", i + 1);
+      if (idx == -1) break;
+      max = Math.max(max, idx - i - 1);
+      i = idx;
     }
+    return max;
+  }
 }

6664a183e5ced798de25623ca11cbca62c92ee39
junilhwang <tjsdlf4261@naver.com>
2020-04-27 10:01:32 +0900

    refactor: Codility.Lesson01.Iterations.BinaryGap

--- codility_training/lesson.01.Iterations.BinaryGap/junilhwang.js
+++ codility_training/lesson.01.Iterations.BinaryGap/junilhwang.js
@@ -1,12 +1,11 @@
 function solution (N) {
-  const b = N.toString(2)
-  const len = b.length
+  const b = N.toString(2), len = b.length
   let i = 0, max = 0;
   while (i < len) {
-    const idx = b.indexOf('1', i + 1)
-    if (idx === -1) break
-    max = Math.max(idx - i - 1, max)
-    i = idx
+    const idx = b.indexOf('1', i+1) // i 이후의 1을 가져옴
+    if (idx === -1) break // 없을 경우 종료
+    max = Math.max(idx - i - 1, max) // 현재 거리와 최대 거리를 비교
+    i = idx // 다음 1의 위치로 이동
   }
   return max
 }

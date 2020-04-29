f9c693e57254b69b313fa6f11065b506f92ef2dc
JeongShin <64301935+JeongShin@users.noreply.github.com>
2020-04-29 23:11:27 +0900

    Update codility_training/lessons.lesson03.TimeComplexity.PermMissingElem/JeongShin.js
    Co-Authored-By: 박은영 <31977543+eyabc@users.noreply.github.com>

--- codility_training/lessons.lesson03.TimeComplexity.PermMissingElem/JeongShin.js
+++ codility_training/lessons.lesson03.TimeComplexity.PermMissingElem/JeongShin.js
@@ -2,7 +2,7 @@ function solution(A) {
   if (!A) return;
   const len = A.length + 1;
   //원래 예상 합
-  let x = ((1 + len) / 2) * len;
+  const x = (1 + len) / 2 * len;
   //현재 배열 합
   const y = A.reduce((sum, curr) => sum + curr, 0);
     return sum + curr;

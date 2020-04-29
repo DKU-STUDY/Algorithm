cf332f0992d80118eea6ecea77c67f18688705cc
JeongShin <sjeong1127@gmail.com>
2020-04-29 16:44:00 +0900

    commit

---
+++ codility_training/lessons.lesson03.TimeComplexity.PermMissingElem/JeongShin.js
@@ -0,0 +1,12 @@
+function solution(A) {
+  if (!A) return;
+  const len = A.length + 1;
+  //원래 예상 합
+  let x = ((1 + len) / 2) * len;
+  //현재 배열 합
+  let y = A.reduce(function (sum, curr) {
+    return sum + curr;
+  }, 0);
+  //missing num = 예상 합 - 현재 합
+  return x - y;
+}

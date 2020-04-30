72e8250896e3e56600f0141de45c8ae687d95f03
sjjyy <43190565+sjjyy@users.noreply.github.com>
2020-05-01 00:13:10 +0900

    Update sjjyy.java

--- codility_training/lessons.lesson03.TimeComplexity.TapeEquilibrium/sjjyy.java
+++ codility_training/lessons.lesson03.TimeComplexity.TapeEquilibrium/sjjyy.java
@@ -1,10 +1,15 @@
-class Solution {
-    public static int solution(int[] A) {
+package lessons.lesson03.TimeComplexity.TapeEquilibrium;
+
+class Solution
+{
+    public static int solution(int[] A)
+    {
         
-        int left = 0, right=0, result = 0, sum = 0;
+        int left = 0, right = 0, sum = 0;
+        int result = Integer.MAX_VALUE; // 비교군으로 MAX값을 넣어야 min에서 최소값 도출 가능
         int len = A.length;
         
-        for(int i = 0; i <= len ; i++)
+        for(int i = 0; i < len ; i++) // 수정
             sum += A[i];
         
         for (int j = 1 ; j < len ; j++)
@@ -13,6 +18,14 @@ class Solution {
             right = sum - left;
             result = Math.min(result, Math.abs(left-right));
         }
+        
         return result;
     }
+    
+    public static void main(String[] args)
+    {
+        int[] A = new int[] {2, 1, 3, 5, 4, 6};
+        System.out.println(solution(A));
+    }
+    
 }

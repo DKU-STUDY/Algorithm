fc13f9a53c2be66fdded83d234959768a9a02186
JeongShin <64301935+JeongShin@users.noreply.github.com>
2020-04-30 21:57:48 +0900

    Update codility_training/lessons.lesson03.TimeComplexity.TapeEquilibrium/JeongShin.js
    Co-authored-by: JunilHwang <tjsdlf4261@naver.com>

--- codility_training/lessons.lesson03.TimeComplexity.TapeEquilibrium/JeongShin.js
+++ codility_training/lessons.lesson03.TimeComplexity.TapeEquilibrium/JeongShin.js
@@ -8,7 +8,7 @@ function solution(A) {
   //앞, 뒤 배열 차이를 현재 인덱스 (P) 기준으로 덧셈 뺄셈으로 계산 후 arr에 push
   while (P < len) {
     arr.push(diff);
-    diff += A[P] * 2;
+    diff += A[P++] * 2;
     P++;
   }
   // arr의 값을 전부 다 절대값 매핑 해준뒤 최소 값 반환

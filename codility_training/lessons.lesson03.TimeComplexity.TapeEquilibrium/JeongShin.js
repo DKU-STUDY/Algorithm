8d6dd3ceaa2c85279a36fd10b20edf7dd32e2843
JeongShin <64301935+JeongShin@users.noreply.github.com>
2020-04-30 21:57:42 +0900

    Update codility_training/lessons.lesson03.TimeComplexity.TapeEquilibrium/JeongShin.js
    Co-authored-by: JunilHwang <tjsdlf4261@naver.com>

--- codility_training/lessons.lesson03.TimeComplexity.TapeEquilibrium/JeongShin.js
+++ codility_training/lessons.lesson03.TimeComplexity.TapeEquilibrium/JeongShin.js
@@ -2,7 +2,7 @@ function solution(A) {
   if (!A) return;
   const len = A.length;
   let P = 1;
-  var arr = [];
+  const arr = [];
   //reduce 가 반복문 안으로 들어가면 시간 복잡도가 n^2이 되기 때문에 빼준다.
   var diff = A[0] - A.slice(P).reduce((prev, curr) => prev + curr);
   //앞, 뒤 배열 차이를 현재 인덱스 (P) 기준으로 덧셈 뺄셈으로 계산 후 arr에 push

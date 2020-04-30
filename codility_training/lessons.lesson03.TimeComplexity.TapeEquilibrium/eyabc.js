8e528bd30c758bd6045f15ed90cf101f339aec34
박은영 <31977543+eyabc@users.noreply.github.com>
2020-04-30 21:54:02 +0900

    lessons.lesson03.TimeComplexity.TapeEquilibrium

---
+++ codility_training/lessons.lesson03.TimeComplexity.TapeEquilibrium/eyabc.js
@@ -0,0 +1,16 @@
+function solution(A) {
+  let frontSum = 0;
+  let backSum = A.reduce((sum, value) => sum + value);
+  const len = A.length - 1;
+  return A.reduce((sum, v, k) => {
+    if (k === len) return sum;
+    frontSum += v;
+    backSum -= v;
+    const diff = Math.abs(frontSum - backSum);
+    if (k === 0 || diff < sum) return diff;
+    return sum;
+  }, 0);
+}
+console.log(solution([3, 1, 2, 4, 3]) === 1);
+console.log(solution([-1000, 1000]) === 2000);
+

aadbbda1e68033b762128c54357cb32e1b34cc74
박은영 <31977543+eyabc@users.noreply.github.com>
2020-04-30 22:32:19 +0900

    Update eyabc.js

--- codility_training/lessons.lesson03.TimeComplexity.TapeEquilibrium/eyabc.js
+++ codility_training/lessons.lesson03.TimeComplexity.TapeEquilibrium/eyabc.js
@@ -3,13 +3,11 @@ function solution(A) {
   let backSum = A.reduce((sum, value) => sum + value);
   const len = A.length - 1;
   return A.reduce((min, v, k) => {
-    if (k === len) return sum;
+    if (k === len) return min;
     frontSum += v;
     backSum -= v;
-    const diff = Math.abs(frontSum - backSum);
-    if (k === 0 || diff < sum) return diff;
-    return sum;
-  }, 0);
+    return Math.min(Math.abs(frontSum - backSum), min);
+  }, infinity);
 }
 console.log(solution([3, 1, 2, 4, 3]) === 1);
 console.log(solution([-1000, 1000]) === 2000);

304dab78a140074641ce377ed56a08cc11274c3c
khw970421 <khw970421@kakao.com>
2020-05-03 22:53:20 +0900

    dkddkdkdkdk

--- codility_training/lessons.lesson03.TimeComplexity.TapeEquilibrium/khw970421.js
+++ codility_training/lessons.lesson03.TimeComplexity.TapeEquilibrium/khw970421.js
@@ -7,16 +7,20 @@ function solution(A) {
         if(sum<0)sum*=-1;
    }                                 
    first_size=sum-=A[0];             //P=1일때 sum의 값을 구한다.
-   
-   for(let p=1;p<size;p++)
-   {
-       sum-=A[p]*2;              //P의 다음값은 원래 있던 A[P-1]*2만큼 차이 나기때문에 계산
-       if(sum<0)sum*=-1;        // 음수일경우 절대값을 씌운다.
-       min=Math.min(min,sum);   // 이를 만족하는 가장 작은 min을 찾는다.
-       
+   //ddddd
+   for(let p=1;p<size;p++) {
+       sum -= A[p] * 2;              //P의 다음값은 원래 있던 A[P-1]*2만큼 차이 나기때문에 계산
+       if (sum < 0) sum *= -1;        // 음수일경우 절대값을 씌운다.
+       min = Math.min(min, sum);   // 이를 만족하는 가장 작은 min을 찾는다.
+
    }
-   
    min=Math.min(min,first_size);    //처음 P=1일때와는 비교하지 못했으므로 마지막으로 비교
-   
    return min;
+
 }
+
+console.log(
+    solution([3, 1, 2, 4, 3]) === 1,
+    solution([-1000, 1000]) === 2000,
+    solution([1, 2]) === 1
+);

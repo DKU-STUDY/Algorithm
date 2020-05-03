fdbd684f784888a8aeec94444bfa52d76ce8c6b4
junilHwang <tjsdlf4261@naver.com>
2020-05-03 21:57:15 +0900

    feat: lessons03.TimeComplexity - TapeEquilibrium 해결 완료

---
+++ codility_training/lessons.lesson03.TimeComplexity.PermMissingElem/junilhwang.js
@@ -0,0 +1,13 @@
+function solution(A) {
+  let sum1 = 0, sum2 = 0
+  const last = A.length + 1;
+  for (let i = 1; i < last; i++) {
+    sum1 += i
+    sum2 += A[i - 1]
+  }
+  return sum1 - sum2 + last
+}
+
+console.log(
+  solution([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 12]) === 11
+);

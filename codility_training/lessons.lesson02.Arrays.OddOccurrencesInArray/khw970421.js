a10ce7a3de3c4faef7bc16be50d32bf549f06f74
김형욱 <59253551+khw970421@users.noreply.github.com>
2020-05-03 11:28:33 +0900

    final update khw970421.js

--- codility_training/lessons.lesson02.Arrays.OddOccurrencesInArray/khw970421.js
+++ codility_training/lessons.lesson02.Arrays.OddOccurrencesInArray/khw970421.js
@@ -1,25 +1,11 @@
-// you can write to stdout for debugging purposes, e.g.
-// console.log('this is a debug message');
-
 function solution(A) {
-     const size = A.length;   
-     var mySet = new Set();
+     const size = A.length;                 //A의 크기 const로 저장
+     var mySet = new Set();                 //Set 객체 챗엇
       for(let i=0;i<size;i++)             //크기만큼 반복
     {       
-        if(mySet.has(A[i])==false)mySet.add(A[i]);
-        else mySet.delete(A[i]);
+        if(mySet.has(A[i])==false)mySet.add(A[i]);      //A[i]가 존재하지않으면 A[i]를 mySet에 추가
+        else mySet.delete(A[i]);                        //A[i]가 존재하면 A[i]를 mySet에서 제거
     }
-    console.log(mySet);
-    return mySet[0];
+    
+    return [...mySet][0];                               //그후 남은 [0]의 set을 배열로 바꿔 return
 }
-
-
-Example test:   [9, 3, 9, 3, 9, 7, 9]
-Output (stderr):
-Invalid result type, integer expected, 'undefined' found
-Perhaps you are missing a 'return'?
-Output:
-Set { 7 }
-RUNTIME ERROR (tested program terminated with exit code 1)
-
-원하는 Set의 남은 한가지를 찾긴했는데 return 부분에서 어떻게 mySet부분의 저걸 return 해야 하는지 잘 모르겠어서여

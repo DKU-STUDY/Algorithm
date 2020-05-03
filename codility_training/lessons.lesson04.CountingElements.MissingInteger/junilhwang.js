73faca10c2506caab29e04908deb19493bdd1d8a
junilHwang <tjsdlf4261@naver.com>
2020-05-03 14:59:04 +0900

    feat: Codility - Lessons04.ConutingElements - MissingInteger / Set 대신 Hash 사용

--- codility_training/lessons.lesson04.CountingElements.MissingInteger/junilhwang.js
+++ codility_training/lessons.lesson04.CountingElements.MissingInteger/junilhwang.js
@@ -6,8 +6,18 @@ function solution(A) {
   return set.size + 1;
 }
 
+function solution2(A) {
+  const obj = {}
+  for (const v of A) obj[v] = true
+  const len = Object.entries(obj).length
+  for (let i = 1; i <= len; i++) {
+    if (!obj[i]) return i;
+  }
+  return len + 1;
+}
+
 console.log(
-  solution([1, 3, 6, 4, 1, 2]) === 5,
-  solution([1, 2, 3]) === 4,
-  solution([-1, -3]) === 1
+  solution2([1, 3, 6, 4, 1, 2]) === 5,
+  solution2([1, 2, 3]) === 4,
+  solution2([-1, -3]) === 1
 );

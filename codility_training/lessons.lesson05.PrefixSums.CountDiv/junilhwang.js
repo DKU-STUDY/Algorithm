aeb7df5ad1526d53362c0926a3fa624bf034515f
junil-hwang <tjsdlf4261@naver.com>
2020-05-04 22:15:03 +0900

    fix: Codility - Lessons05.PrefixSUms - CountDiv 일부 로직 수정

--- codility_training/lessons.lesson05.PrefixSums.CountDiv/junilhwang.js
+++ codility_training/lessons.lesson05.PrefixSums.CountDiv/junilhwang.js
@@ -3,8 +3,9 @@
  * 이 범위 사이의 수 중 (A <= i <= B) 가 K로 나누었을 때 나머지가 0이 되는 수의 갯수를 return 하는 문제입니다.
  */
 function solution (A, B, K) {
-  let cnt = A === 0 ? 1 : 0
-  const start = A + K - A % K
+  let cnt = 0
+  const mod = A % K
+  const start = A + (mod === 0 ? 0 : K - mod)
   for (let i = start; i < B + 1; i += K) cnt += 1
   return cnt
 }

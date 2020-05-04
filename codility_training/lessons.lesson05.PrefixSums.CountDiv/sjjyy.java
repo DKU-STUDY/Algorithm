966da5aa1f5a74d2c04e2601a8d086d5dfab9593
sjjyy <43190565+sjjyy@users.noreply.github.com>
2020-05-05 01:43:04 +0900

    Create sjjyy.java

---
+++ codility_training/lessons.lesson05.PrefixSums.CountDiv/sjjyy.java
@@ -0,0 +1,28 @@
+package lessons.lesson05.PrefixSums.CountDiv;
+
+class sjjyy
+{
+    public static int solution(int A, int B, int K) // 수학적으로 접근
+    {
+        return (B / K) - (A / K) + (A % K == 0 ? 1 : 0);
+    }
+    
+    public static int solution0(int A, int B, int K) // 문제 그대로만 풀었을 때
+    {
+        int c;
+        
+        for (int i = A ; i <= B ; i++)
+        {
+            if (i % K == 0) c++;
+        }
+        
+        return c;
+    }
+    
+    public static void main(String[] args)
+    {
+        System.out.println(solution(1, 5, 3));
+        System.out.println(solution(6, 11, 2));
+    }
+    
+}

f9246935e9d2ff39d5c6d1d51e2e6eefbc326c51
김형욱 <59253551+khw970421@users.noreply.github.com>
2020-05-04 22:42:54 +0900

    Update khw970421.js

--- codility_training/lessons.lesson03.TimeComplexity.TapeEquilibrium/khw970421.js
+++ codility_training/lessons.lesson03.TimeComplexity.TapeEquilibrium/khw970421.js
@@ -1,14 +1,14 @@
 function solution(A) {
-   let size=A.length,sum=0;//길이저장
-    let all=0,min=100000,final;             //배열의 총합저장 하는 all, P에따른 difference값인 final
+   let sum=0;
+   const size = A.length,last = A.length+1 ;//길이저장
+    let all=0,min=Infinity,final;             //배열의 총합저장 하는 all, P에따른 difference값인 final
     for(let k=0;k<size;k++)                 //배열 총합구하기
         all+=A[k];
 
     for(let i=0;i<size-1;i++)
     {
         sum+=A[i];                          //앞에 있는 P=i+1일때의 앞의값 구하기
-        final = all-sum*2;                  //final은 앞의값 (all-sum)-sum
-        if(final<0)    final*=-1;           //음수면 절대값을 붙인다.
+        final = Math.abs(all - sum * 2)                  //final은 뒤의값-앞의값의 절대값 | (all-sum)-sum | ,음수면 절대값을 붙인다.
         min=Math.min(min,final);            // 작은 값을 min에 넣기
      }
    return min;                              //P를 만족하는 가장 작은 min return

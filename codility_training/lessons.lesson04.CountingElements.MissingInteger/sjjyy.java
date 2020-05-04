db2da3e733040be7ba3f2d9daf296ea5a7fb2e30
sjjyy <43190565+sjjyy@users.noreply.github.com>
2020-05-04 23:24:12 +0900

    Create sjjyy.java

---
+++ codility_training/lessons.lesson04.CountingElements.MissingInteger/sjjyy.java
@@ -0,0 +1,29 @@
+package lessons.lesson04.CountingElements.MissingInteger;
+
+import java.util.*;
+
+class Solution
+{
+    public int solution(int[] A)
+    {
+        Arrays.sort(A);
+        int cnt = 1;
+        int len = A.length;
+        for(int i = 0; i < len; i++)
+        {
+            if(A[i] == cnt)
+                cnt++;
+        }
+        
+        return cnt;
+    }
+    
+    public static void main(String[] args)
+    {
+        int[] A = new int[] {1, 3, 1, 4, 2, 3, 5, 4};
+        int[] B = new int[] {1, 1, 4, 2, 5};
+        System.out.println(solution(A));
+        System.out.println(solution(B));
+    }
+    
+}

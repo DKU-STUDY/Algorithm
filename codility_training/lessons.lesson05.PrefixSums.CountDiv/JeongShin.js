3617db5efbd7416e96796d2ebbf28739466dd8e1
JeongShin <sjeong1127@gmail.com>
2020-05-04 21:12:53 +0900

    code review edited

--- codility_training/lessons.lesson05.PrefixSums.CountDiv/JeongShin.js
+++ codility_training/lessons.lesson05.PrefixSums.CountDiv/JeongShin.js
@@ -11,13 +11,19 @@ function solution(A, B, K) {
 }
 
 /*@eyabc 님 코드보고 훨씬 가독성도 좋고 효율적인거 같아서 다시 작성해보았습니다.*/
-
+//
 function solution2(A, B, K) {
     // B까지 총 K 배수의 개수
     const rear = Math.floor(B / K);
     const front = Math.floor(A / K) - (A % K ? 0 : 1);
-    // return (Math.floor(B / K) - Math.floor(A / K) - (A % K ? 0 : 1));
+    return (Math.floor(B / K) - Math.floor(A / K) - (A % K ? 0 : 1));
     // 이렇게 하면 왜 아예 다른 결과가 나오는지 잘 모르겠네요 ㅠ
-    return rear - front;
+    // return rear - front;
 }
 
+function solution3(A, B, K) {
+    if (A === B) return B % K === 0 ? 1 : 0;
+    return ~~(B / K) - ~~(A / K) - (A % K ? 0 : 1);
+}
+
+console.log(solution3(3, 10, 4));

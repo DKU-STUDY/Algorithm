cd0a74683134c61d7a4693903179f07fc485966d
junil-hwang <tjsdlf4261@naver.com>
2020-05-05 20:35:00 +0900

    refactor: Codility - Lessons05.PrefixSums - MinAvgTwoSlice 리팩토링 완료

--- codility_training/lessons.lesson05.PrefixSums.MinAvgTwoSlice/junilhwang.js
+++ codility_training/lessons.lesson05.PrefixSums.MinAvgTwoSlice/junilhwang.js
@@ -7,13 +7,10 @@ function solution(A) {
   const sum = (a, b) => a + b
   let min = Infinity, i = 0, resolve = -1
   do {
-    const arr1 = A.slice(i, i + 2), len1 = arr1.length
-    const arr2 = A.slice(i, i + 3), len2 = arr2.length
-    let tmp = Math.min(arr1.reduce(sum) / len1, arr2.reduce(sum) / len2, min)
-    if (tmp !== min) {
-      resolve = i
-      min = tmp
-    }
+    const arr1 = A.slice(i, i + 2), avg1 = arr1.reduce(sum) / arr1.length
+    const arr2 = A.slice(i, i + 3), avg2 = arr2.reduce(sum) / arr2.length
+    let tmp = Math.min(avg1, avg2, min)
+    if (tmp !== min) ([ resolve, min ] = [ i, tmp ])
     i += 1
   } while (i < len - 1)
   return resolve

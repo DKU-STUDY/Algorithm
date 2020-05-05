c3ef5d75523c80e5a2bbcd5a26c58660e63139cd
junil-hwang <tjsdlf4261@naver.com>
2020-05-05 18:04:58 +0900

    feat: Codility - Lessons05.PrefixSums - GenomicRangeQuery / 효율성 완료

--- codility_training/lessons.lesson5.PrefixSum.GenomicRangeQuery/junilhwang.js
+++ codility_training/lessons.lesson5.PrefixSum.GenomicRangeQuery/junilhwang.js
@@ -1,7 +1,15 @@
 function solution(S, P, Q) {
-  const obj = { 'A': 1, 'C': 2, 'G': 3, 'T': 4 }
-  const arr = [ ...S ].map(v => obj[v])
-  return P.map((start, k) => Math.min(...arr.slice(start, Q[k] + 1)))
+  return P.map((start, k) => {
+    const end = Q[k]
+    const A = S.indexOf('A', start)
+    if ( A !== -1 && A <= end) return 1
+    const C = S.indexOf('C', start)
+    if ( C !== -1 && C <= end) return 2
+    const G = S.indexOf('G', start)
+    if ( G !== -1 && G <= end) return 3
+    const T = S.indexOf('T', start)
+    if ( T !== -1 && T <= end) return 4
+  })
 }
 
 const testCase = require("./test.json")

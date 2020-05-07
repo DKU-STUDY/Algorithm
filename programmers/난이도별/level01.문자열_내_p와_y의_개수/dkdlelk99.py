7c13aa757001b27d48dabd960ed29bab724a7a1f
dkdlelk99 <49552766+dkdlelk99@users.noreply.github.com>
2020-05-07 21:08:57 +0900

    Update dkdlelk99.py

--- programmers/난이도별/level01.문자열_내_p와_y의_개수/dkdlelk99.py
+++ programmers/난이도별/level01.문자열_내_p와_y의_개수/dkdlelk99.py
@@ -1,9 +1,6 @@
 def solution(s):
     s = s.lower()
     print(s)
-    p, y = s.count('p'), s.count('y')
-    if p == y:
-        return True
     return s.count('p') == s.count('y')
 
 print(solution("oooo") == True)

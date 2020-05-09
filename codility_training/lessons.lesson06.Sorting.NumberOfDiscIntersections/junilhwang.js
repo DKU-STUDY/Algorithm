00d9666f8e937ce870d37c6c0da5e53cb57c2dac
junilHwang <tjsdlf4261@naver.com>
2020-05-09 23:04:06 +0900

    feat: codility - lesson06.Sorting - NumberOfDiscIntersections 풀이 중 (2)

--- codility_training/lessons.lesson06.Sorting.NumberOfDiscIntersections/junilhwang.js
+++ codility_training/lessons.lesson06.Sorting.NumberOfDiscIntersections/junilhwang.js
@@ -1,12 +1,26 @@
 function solution (A) {
   const arr = []
   const len = A.length
+  let sum = 0
   for (let i = 0; i < len; i++) {
+    const start = i - A[i]
+    const end = i + A[i]
     for (let j = 0; j < len; j++) {
       if (i === j) continue;
+      const start2 = j - A[j]
+      const end2 = j + A[j]
+      if (!(start > end2 || end < start2)) {
+        arr[i] = arr[i] || new Set()
+        arr[j] = arr[j] || new Set()
+        if (!arr[i].has(j)) {
+          arr[i].add(j)
+          arr[j].add(i)
+          sum += 1
+        }
+      }
     }
   }
-  return 0
+  return sum
 }
 
 const testCase = require("./test.json")

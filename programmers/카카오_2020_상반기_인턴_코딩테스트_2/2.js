e634064858f5b5fff5c85b0838abf90229ca6d17
junilHwang <tjsdlf4261@naver.com>
2020-05-09 16:18:27 +0900

    feat: 카카오 상반기 인턴 2번 문제 풀이 완료

--- programmers/카카오_2020_상반기_인턴_코딩테스트_2/2.js
+++ programmers/카카오_2020_상반기_인턴_코딩테스트_2/2.js
@@ -20,28 +20,27 @@ function solution(expression) {
   }
   if (token.length) numberStack.push(token)
 
-  console.log(numberStack, operStack)
-
   const sums = ['+*-','+-*','-*+','-+*','*-+','*+-'].map(v => {
-    const stack1 = []
-    const stack2 = []
     const opers = [ ...v ]
-    while (operStack.length) {
-      let start = operStack.indexOf(opers[0])
-      console.log(start, opers[0])
-      while (start !== -1) {
-        numberStack[start] = `${numberStack[start]}${opers[0]}${numberStack[start + 1]}`
-        numberStack.splice(start + 1, 1)
-        operStack.splice(start, 1)
+    const oStack = [ ...operStack ]
+    const nStack = [ ...numberStack ]
+    while (opers.length !== 0) {
+      const oper = opers.shift()
+      let idx = oStack.indexOf(oper)
+      while (idx !== -1) {
+        const exp = `${nStack[idx]} ${oper} ${nStack[idx + 1]}`
+        nStack[idx] = eval(exp)
+        nStack.splice(idx + 1, 1)
+        oStack.splice(idx, 1)
+        idx = oStack.indexOf(oper)
       }
-      console.log(numberStack, operStack)
-      return false
     }
+    return Math.abs(nStack.pop())
   })
-  console.log(sums)
+  return Math.max(...sums)
 }
 
 console.log(
   solution("100-200*300-500+20") ===60420,
-  //solution("50*6-3*2") === 300,
+  solution("50*6-3*2") === 300,
 )

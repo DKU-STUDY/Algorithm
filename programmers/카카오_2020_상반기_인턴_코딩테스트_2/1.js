6541f948138888862ee265404ea8cf5ce3919bc9
junilHwang <tjsdlf4261@naver.com>
2020-05-09 14:05:22 +0900

    feat: 카카오 상반기 인턴 1번문제 풀이 중

---
+++ programmers/카카오_2020_상반기_인턴_코딩테스트_2/1.js
@@ -0,0 +1,10 @@
+function solution(numbers, hand) {
+  var answer = '';
+  return answer;
+}
+
+console.log(
+  solution([1, 3, 4, 5, 8, 2, 1, 4, 5, 9, 5], "right") === "LRLLLRLLRRL",
+  solution([7, 0, 8, 2, 8, 3, 1, 5, 7, 6, 2], "left") === "LRLLRRLLLRR",
+  solution([1, 2, 3, 4, 5, 6, 7, 8, 9, 0], "right") === "LLRLLRLLRL",
+)

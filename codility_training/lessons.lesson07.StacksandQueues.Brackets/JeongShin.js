fb35303d28a1a603d664576064a2db32f11e1d7f
JeongShin <64301935+JeongShin@users.noreply.github.com>
2020-05-11 20:36:28 +0900

    Update codility_training/lessons.lesson07.StacksandQueues.Brackets/JeongShin.js
    Co-authored-by: JunilHwang <tjsdlf4261@naver.com>

--- codility_training/lessons.lesson07.StacksandQueues.Brackets/JeongShin.js
+++ codility_training/lessons.lesson07.StacksandQueues.Brackets/JeongShin.js
@@ -5,7 +5,7 @@ function solution(S) {
     const open = [];
     let o_top = 0, c_top = 0;
     [...S].forEach(el => {
-        Object.keys(brackets).indexOf(el) === -1 ?
+        bracketsKeys.indexOf(el) === -1 ?
             Object.values(brackets)[Object.keys(brackets).indexOf(open[o_top - 1])] === el ? o_top-- : c_top++ :
             open[o_top++] = el;
     });

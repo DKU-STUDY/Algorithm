75c685ba9fdb9f36fcfa9cf2785e53ce956526fc
JeongShin <64301935+JeongShin@users.noreply.github.com>
2020-05-13 15:47:33 +0900

    Update codility_training/lessons.lesson07.StacksandQueues.Nesting/JeongShin.js
    Co-authored-by: JunilHwang <tjsdlf4261@naver.com>

--- codility_training/lessons.lesson07.StacksandQueues.Nesting/JeongShin.js
+++ codility_training/lessons.lesson07.StacksandQueues.Nesting/JeongShin.js
@@ -2,7 +2,7 @@ function solution(S) {
     const len = S.length;
     let top = 0;
     // forEach 문은 도중에 break로 loop 탈출이 불가능 하다 하여 일반 포문을 사용 하였습니다.
-    for (let i = 0; i < len; i++) {
+    for (const v of S) {
         if (top < 0)
             break;
         S[i] === '(' ? top++ : top = S[i] === ')' ? top - 1 : top;

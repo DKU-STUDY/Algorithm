e5c1352b8aec520df8682f0bf7cec866f2842af1
sApOOn <won7963kr@gmail.com>
2020-05-13 22:22:12 +0900

    Trying lesson08

--- codility_training/lessons.lessons08.Leader.EquiLeader/sAp00n.py
+++ codility_training/lessons.lessons08.Leader.EquiLeader/sAp00n.py
@@ -1,6 +1,6 @@
 # you can write to stdout for debugging purposes, e.g.
 # print("this is a debug message")
-
+#[1, 2, 1, 1, 2, 1]
 def solution(A):
     # write your code in Python 3.6
     leader = search_leader(A)
@@ -11,36 +11,42 @@ def solution(A):
     else:
         eq_leadernum = 0
         ind_list = []
-        for ind in range(len(A)):
+        n = len(A)
+        for ind in range(n):
             if A[ind] == leader:
-                ind_list.append(ind + 1)
-
-        for i in ind_list:
-            left = A[:i]
-            right = A[i:]
-            left_leader = search_leader(left)
-            right_leader = search_leader(right)
-            if left_leader == right_leader:
+                ind_list.append(ind)
+        #print(ind_list)
+        for i in range(len(ind_list)-1):
+            #print(f'left ={A[:ind_list[i]+1]},{ind_list[:i+1]}   right = {A[ind_list[i]+1:]}, {ind_list[i+1:]}')
+            left_condition = len(ind_list[:i+1])/(ind_list[i]+1)
+            right_condition = len(ind_list[i+1:])/len(A[ind_list[i]+1:])
+            '''print(f'left_c = {len(ind_list[:i+1])}/{(ind_list[i]+1)} = {left_condition}  right_c = {len(ind_list[i+1:])}/{len(A[ind_list[i]+1:])} = {right_condition}')'''
+            if left_condition and right_condition > 1/2:
                 eq_leadernum += 1
-                # print(f'num++ i={ind_list[i]}')
+                #print('eq +1')
         return eq_leadernum
 
 
 def search_leader(A):
-    condition = len(A) // 2
-    leader = -1
-    dic = {}
-    if len(A) == 1:
-        leader = A[0]
-        return leader
-
-    for i in A:
-        if i in dic:
-            dic[i] += 1
-            if dic[i] > condition:
-                leader = i
-                # print(dic)
-                return leader
+    n = len(A)
+    size = 0
+    for k in range(n):
+        if size == 0:
+            size += 1
+            value = A[k]
+        elif value != A[k]:
+            size -= 1
         else:
-            dic[i] = 1
+            size += 1
+    candidate = -1
+    if size > 0 :
+        candidate = value
+    leader = -1
+    count = 0
+
+    for k in range(n):
+        if A[k] == candidate:
+            count += 1
+    if count > n//2:
+        leader =candidate
     return leader

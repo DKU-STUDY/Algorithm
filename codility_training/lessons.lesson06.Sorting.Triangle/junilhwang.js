d8927cdefb553a3641a561b2365e4e952bf9d741
junil-hwang <tjsdlf4261@naver.com>
2020-05-14 19:33:35 +0900

    refactor: Codility - Lessons06.PrefixSums - Triangle

--- codility_training/lessons.lesson06.Sorting.Triangle/junilhwang.js
+++ codility_training/lessons.lesson06.Sorting.Triangle/junilhwang.js
@@ -1,11 +1,7 @@
 function solution(A) {
   A.sort((a, b) => a - b)
   for (let i = 0, len = A.length - 2; i < len; i++) {
-    if (
-      A[i] + A[i + 1] > A[i + 2] &&
-      A[i + 1] + A[i + 2] > A[i] &&
-      A[i] + A[i + 2] > A[i + 1]
-    ) return 1;
+    if (A[i + 1] > A[i + 2] - A[i]) return 1;
   }
   return 0
 }

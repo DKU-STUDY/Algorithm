5c1c13b6b7b52769627532c296e09b269910c333
junil-hwang <tjsdlf4261@naver.com>
2020-05-14 21:46:11 +0900

    fix: Codility - Lessons08.Leader - EquilLeader / 효율성 최적화

--- codility_training/lessons.lessons08.Leader.EquiLeader/junilhwang.js
+++ codility_training/lessons.lessons08.Leader.EquiLeader/junilhwang.js
@@ -1,24 +1,33 @@
 function solution(A) {
-  const len = A.length
   const back = [ A.pop() ]
-  let cnt = 0
-
-  const getLeader = arr => {
-    const obj = {}
-    const half = arr.length / 2
-    let max = 0;
-    for (let i = 0, len = arr.length; i < len; i++) {
-      const v = A[i]
-      obj[v] = (obj[v] || 0) + 1
-      if (obj[v] > half) return v;
-    }
+  const FO = {}
+  const BO = { [back[0]]: 1 }
+  let BLMax = 1, FLMax = 0,
+      BC = 1, FC = A.length - 1,
+      BL = back[0], FL
+  for (let i = 0; A[i] !== undefined; i++) {
+    const v = A[i]
+    FO[v] = (FO[v] || 0) + 1
+    FLMax = Math.max(FLMax, FO[v])
+    if (FLMax > FC / 2) FL = v
   }
 
-  for (let i = 1; i < len - 1; i++) {
-    const FL = getLeader(A)
-    const BL = getLeader(back)
-    cnt += FL !== undefined && BL !== undefined && FL === BL
-    back.push(A.pop())
+  let cnt = (FL === BL) * 1
+
+  while (A[0] !== undefined) {
+    const v = A.pop()
+
+    BO[v] = (BO[v] || 0) + 1;
+    if (BO[v] > BLMax) ([BL, BLMax] = [v, BO[v]]);
+    BC += 1
+
+    FO[v] -= 1
+    FL = Object.keys(FO).sort((a, b) => FO[b] - FO[a])[0];
+    FLMax = FO[FL]
+    FC -= 1
+
+    if (BLMax < BC / 2 || FLMax < FC / 2) continue
+    cnt += (~~FL === ~~BL) * 1
   }
   return cnt
 }

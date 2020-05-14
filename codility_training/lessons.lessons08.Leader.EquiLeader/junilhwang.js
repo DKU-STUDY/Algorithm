e01155e47abdb19b349f34470a772f5ddfbe3a1a
junil-hwang <tjsdlf4261@naver.com>
2020-05-14 23:25:11 +0900

    fix: Codility - Lessons08.Leader - EquilLeader / 성능 최적화 (6)

--- codility_training/lessons.lessons08.Leader.EquiLeader/junilhwang.js
+++ codility_training/lessons.lessons08.Leader.EquiLeader/junilhwang.js
@@ -1,20 +1,17 @@
 function solution(A) {
   if (A.length < 2) return 0
   let shift = A.shift()
-  const FO = { [shift]: 1 }
-  const BO = {}
+  const FO = { [shift]: 1 }, BO = {}
   const capture = []
   let FLMax = 1, BLMax = 0,
-    FC = 1, BC = A.length,
-    FL = shift, BL
+      FC = 1, BC = A.length,
+      FL = shift, BL
   for (let i = 0; A[i] !== undefined; i++) {
     const v = A[i]
     BO[v] = (BO[v] || 0) + 1
     BLMax = Math.max(BLMax, BO[v])
     if (BO[v] > BC / 2) BL = v
   }
-
-  const sorted = Object.keys(BO).sort((a, b) => BO[b] - BO[a])
 
   let cnt = (~~FL === ~~BL) * 1
 
@@ -23,18 +20,14 @@ function solution(A) {
     shift = A.shift()
 
     FO[shift] = (FO[shift] || 0) + 1;
-    if (FO[shift] > FLMax) ([FL, FLMax] = [shift, FO[shift]]);
-    FC += 1
-
     BO[shift] -= 1
+    FC += 1
     BC -= 1
-    if (BO[shift] === 0) delete BO[shift]
-
-    if (FLMax <= FC / 2) continue;
 
     BLMax = BO[BL]
+    if (FO[shift] > FLMax) ([FL, FLMax] = [shift, FO[shift]]);
 
-    cnt += (BLMax > BC / 2 && ~~FL === ~~BL)
+    cnt += (FLMax > FC / 2 && BLMax > BC / 2 && ~~FL === ~~BL)
   }
   return cnt
 }

6d379dc513f19a831a048bcfae6aef7dc75051b7
junil-hwang <tjsdlf4261@naver.com>
2020-05-14 22:02:13 +0900

    fix: Codility - Lessons08.Leader - EquilLeader / 정확성 최적화 (1)

--- codility_training/lessons.lessons08.Leader.EquiLeader/junilhwang.js
+++ codility_training/lessons.lessons08.Leader.EquiLeader/junilhwang.js
@@ -1,10 +1,11 @@
 function solution(A) {
-  let pop = A.pop()
+  let shift = A.shift()
+  const front = [ shift ]
   const FO = {}
-  const BO = { [pop]: 1 }
+  const BO = { [shift]: 1 }
   let BLMax = 1, FLMax = 0,
       BC = 1, FC = A.length - 1,
-      BL = pop, FL
+      BL = shift, FL
   for (let i = 0; A[i] !== undefined; i++) {
     const v = A[i]
     FO[v] = (FO[v] || 0) + 1
@@ -12,17 +13,23 @@ function solution(A) {
     if (FLMax > FC / 2) FL = v
   }
 
-  let cnt = (FL === BL) * 1
+  let cnt = (~~FL === ~~BL) * 1
 
+
+  console.log(front, A)
   while (A[1] !== undefined) {
-    pop = A.pop()
 
-    BO[pop] = (BO[pop] || 0) + 1;
-    if (BO[pop] > BLMax) ([BL, BLMax] = [pop, BO[pop]]);
+    shift = A.shift()
+    front.push(shift)
+
+    console.log(front, A)
+
+    BO[shift] = (BO[shift] || 0) + 1;
+    if (BO[shift] > BLMax) ([BL, BLMax] = [shift, BO[shift]]);
     BC += 1
 
-    FO[pop] -= 1
-    if (FO[pop] === 0) delete FO[pop]
+    FO[shift] -= 1
+    if (FO[shift] === 0) delete FO[shift]
     FL = Object.keys(FO).sort((a, b) => FO[b] - FO[a])[0];
     FLMax = FO[FL]
     FC -= 1

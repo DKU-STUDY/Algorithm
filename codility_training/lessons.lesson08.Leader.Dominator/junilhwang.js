b644e30fc7547f9b50b47a7ff963f15a7f1977fd
junil-hwang <tjsdlf4261@naver.com>
2020-05-14 20:04:06 +0900

    feat: Codility - Lessons08.Leader - Dominator / 풀이 완료

--- codility_training/lessons.lesson08.Leader.Dominator/junilhwang.js
+++ codility_training/lessons.lesson08.Leader.Dominator/junilhwang.js
@@ -1,5 +1,12 @@
 function solution(A) {
-
+  const half = A.length / 2
+  const obj = {}
+  const [ max, dominator ] = A.reduce(([m, d ], v) => {
+    obj[v] = (obj[v] || 0) + 1
+    const tmp = Math.max(obj[v], m)
+    return tmp > m ? [ tmp, v ] : [ m, d ]
+  }, [0, -1])
+  return max >= half ? dominator : -1
 }
 
 const assert = require('assert').strict

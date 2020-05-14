02c2bd8c1a0c1c759917812c177d876b75d000a5
junil-hwang <tjsdlf4261@naver.com>
2020-05-14 20:44:57 +0900

    feat: Codility - Lessons08.Leader - EquilLeader / 일단 풀이 완료

--- codility_training/lessons.lessons08.Leader.EquiLeader/junilhwang.js
+++ codility_training/lessons.lessons08.Leader.EquiLeader/junilhwang.js
@@ -1,8 +1,27 @@
 function solution(A) {
+  const len = A.length
+  const back = [ A.pop() ]
+  let cnt = 0
 
+  const getLeader = arr => {
+    const obj = {}
+    const half = arr.length / 2
+    let max = 0;
+    for (let i = 0, len = arr.length; i < len; i++) {
+      const v = A[i]
+      obj[v] = (obj[v] || 0) + 1
+      if (obj[v] >= half) return i;
+    }
+  }
+
+  for (let i = 1; i < len; i++) {
+    cnt += (getLeader(A) === getLeader(back)) * 1
+    back.push(A.pop())
+  }
+  return cnt
 }
 
 const assert = require('assert').strict
 require("./test.json").forEach(({ input, output }) => {
-  assert.notEqual(expected, 2)
+  assert.notEqual(solution(...input), 2)
 })

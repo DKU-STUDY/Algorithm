4e18ce73d1542c068091aeb254b193b1a4dbcc9b
junil-hwang <tjsdlf4261@naver.com>
2020-05-14 18:40:10 +0900

    feat: Codility - Lessons06.PrefixSums - Distinct / Set으로 풀이 완료

--- codility_training/lessons.lesson06.Sorting.Distinct/junilhwang.js
+++ codility_training/lessons.lesson06.Sorting.Distinct/junilhwang.js
@@ -2,7 +2,12 @@ function solution (A) {
   return Object.keys(A.reduce((obj, v) => (obj[v] = true, obj), {})).length
 }
 
+function solution2 (A) {
+  return new Set(A).size
+}
+
 const assert = require('assert')
 require('./test.json').forEach(([ input ,output ]) => {
   assert.deepEqual(solution(...input), output)
+  assert.deepEqual(solution2(...input), output)
 })

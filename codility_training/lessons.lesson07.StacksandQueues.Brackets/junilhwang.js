2b1652b447642a5d5b60f26e5f56b7d9893c0276
junil-hwang <tjsdlf4261@naver.com>
2020-05-14 19:46:46 +0900

    refactor: Codility - Lessons07.PrefixSums - Brackets

--- codility_training/lessons.lesson07.StacksandQueues.Brackets/junilhwang.js
+++ codility_training/lessons.lesson07.StacksandQueues.Brackets/junilhwang.js
@@ -1,11 +1,10 @@
 function solution(A) {
   const obj = { ']': '[', ')': '(', '}': '{' }
   const stack = []
-  for (const v of [...A]) {
+  return ![ ...A ].some(v => {
     if ('[({'.indexOf(v) !== -1) stack.push(v)
-    else if(stack.pop() !== obj[v]) return 0
-  }
-  return 1
+    else if(stack.pop() !== obj[v]) return true
+  }) * 1
 }
 
 const assert = require('assert').strict

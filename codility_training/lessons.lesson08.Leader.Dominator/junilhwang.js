275d1edb5962c3c9318112bb21916d5f7be97331
junil-hwang <tjsdlf4261@naver.com>
2020-05-14 20:27:47 +0900

    refactor: Codility - Lessons08.Leader - Dominator (2)

--- codility_training/lessons.lesson08.Leader.Dominator/junilhwang.js
+++ codility_training/lessons.lesson08.Leader.Dominator/junilhwang.js
@@ -1,17 +1,17 @@
 function solution(A) {
-  if (!A.length) return -1
   const obj = {}
-  const [ max, dominator ] = A.reduce(([ m, d ], v, k) => {
-    obj[v] = (obj[v] || []).concat([k])
-    const tmp = Math.max(obj[v].length, m)
-    return tmp > m ? [ tmp, v ] : [ m, d ]
-  }, [0, -1])
-  return max > A.length / 2 ? obj[dominator][0] : -1
+  const half = A.length / 2
+  let max = 0;
+  for (const i in A) {
+    const v = A[i]
+    obj[v] = (obj[v] || 0) + 1
+    if (obj[v] > half) return i*1;
+  }
+  return -1
 }
 
 const assert = require('assert').strict
 require("./test.json").forEach(({ input, output }) => {
   const expected = solution(...input)
-  console.log(expected)
-  assert.deepEqual(expected, output)
+  assert.notEqual(output.indexOf(expected), -1)
 })

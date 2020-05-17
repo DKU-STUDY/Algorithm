9e59cb70ea5ec5a99b1e13eb738a8af913dffdac
JunilHwang <tjsdlf4261@naver.com>
2020-05-18 00:13:10 +0900

    Update programmers/난이도별/level01.정수_내림차순으로_배치하기/dkdlelk99.py

--- programmers/난이도별/level01.정수_내림차순으로_배치하기/dkdlelk99.py
+++ programmers/난이도별/level01.정수_내림차순으로_배치하기/dkdlelk99.py
@@ -1,10 +1,5 @@
 def solution(n):
-    s = str(n)
-    a = list(s)
-    a.sort()
-    a.reverse()
-    answer = ''.join(a)
-    return int(answer)
+    return int(''.join(sorted(list(str(n)), reverse=True)))
 
 print(solution(118372) == 873211)
 print(solution(12421356) == 65432211)

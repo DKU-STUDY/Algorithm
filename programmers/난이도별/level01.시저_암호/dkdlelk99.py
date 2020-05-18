d184bc0492c6a33be10951a4e8ed13492e058122
dkdlelk99 <49552766+dkdlelk99@users.noreply.github.com>
2020-05-18 20:41:46 +0900

    Update dkdlelk99.py

--- programmers/난이도별/level01.시저_암호/dkdlelk99.py
+++ programmers/난이도별/level01.시저_암호/dkdlelk99.py
@@ -1,18 +1,13 @@
 def solution(s, n):
-    small = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r',
-            's','t','u','v','w','x','y','z']
-    big = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R',
-            'S','T','U','V','W','X','Y','Z']
+    small = [chr(i) for i in range(ord('a'), ord('z') + 1)]
+    big = [chr(i) for i in range(ord('A'), ord('Z') + 1)]
     answer = []
-    for i in s:
-        if i.islower():
-            a = small.index(i)
-            answer.append(small[(a+n)%26])
-        elif i.isupper():
-            b = big.index(i)
-            answer.append(big[(b+n)%26])
-        else:
-            answer.append(' ')
+    for c in s :
+        if (c.isspace()):
+            answer += c
+            continue
+        target = small if c.islower() else big
+        answer += target[(target.index(c) + n) % 26]
     return ''.join(answer)
 
 print(solution('sdf Z',1) == 'teg A')

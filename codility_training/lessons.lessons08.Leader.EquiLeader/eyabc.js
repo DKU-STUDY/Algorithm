ac589373171aa7a0f67286d51bde6e5cceba3049
eyabc <bey4314@naver.com>
2020-05-19 22:37:18 +0900

    lessons.lessons08.Leader.EquiLeader

--- codility_training/lessons.lessons08.Leader.EquiLeader/eyabc.js
+++ codility_training/lessons.lessons08.Leader.EquiLeader/eyabc.js
@@ -1,25 +1,21 @@
 function solution(A) {
   let res = 0;
-  const len = A.length;
-  const lObj = {};
-  const rObj = {};
+  const len = A.length - 1;
+  const lObj = {}, rObj = {};
 
   const setSize = (new Set(A)).size;
-  if(setSize > (len / 2)) return 0;
+  if(setSize > (A.length / 2)) return 0;
 
-  for (const v of A) {
-    if(!rObj[v]) rObj[v] = 1;
-    else rObj[v]++;
-  }
+  for (const v of A)
+    rObj[v] = (rObj[v] || 0) + 1;
 
-  for (let i = 0 ; i < len - 1 ; i++) {
-    if(!lObj[A[i]]) lObj[A[i]] = 1;
-    else lObj[A[i]]++;
+  for (let i = 0 ; i < len ; i++) {
+    lObj[A[i]] = (lObj[A[i]] || 0) + 1;
     rObj[A[i]]--;
     const lMax = Math.max(...Object.values(lObj));
     const rMax = Math.max(...Object.values(rObj));
     /// l r 둘다 과반수가 존재하는지
-    if(lMax/(i+1) > 0.5 && rMax/(len - i - 1) > 0.5) {
+    if(lMax/(i+1) > 0.5 && rMax/(len - i) > 0.5) {
       const maxLKey = Object.entries(lObj).find(([, value]) => value === lMax);
       const maxRKey = Object.entries(rObj).find(([, value]) => value === rMax);
       if(maxRKey[0] === maxLKey[0]) res++;

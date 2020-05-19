7872db172fac1c18140e1224cf13f2478c3aa90c
eyabc <bey4314@naver.com>
2020-05-19 22:38:16 +0900

    lessons.lessons08.Leader.EquiLeader lint

--- codility_training/lessons.lessons08.Leader.EquiLeader/eyabc.js
+++ codility_training/lessons.lessons08.Leader.EquiLeader/eyabc.js
@@ -1,10 +1,10 @@
-function solution(A) {
+function solution (A) {
   let res = 0;
   const len = A.length - 1;
   const lObj = {}, rObj = {};
 
   const setSize = (new Set(A)).size;
-  if(setSize > (A.length / 2)) return 0;
+  if (setSize > (A.length / 2)) return 0;
 
   for (const v of A)
     rObj[v] = (rObj[v] || 0) + 1;
@@ -15,17 +15,18 @@ function solution(A) {
     const lMax = Math.max(...Object.values(lObj));
     const rMax = Math.max(...Object.values(rObj));
     /// l r 둘다 과반수가 존재하는지
-    if(lMax/(i+1) > 0.5 && rMax/(len - i) > 0.5) {
+    if (lMax / (i + 1) > 0.5 && rMax / (len - i) > 0.5) {
       const maxLKey = Object.entries(lObj).find(([, value]) => value === lMax);
       const maxRKey = Object.entries(rObj).find(([, value]) => value === rMax);
-      if(maxRKey[0] === maxLKey[0]) res++;
+      if (maxRKey[0] === maxLKey[0]) res++;
     }
   }
   return res;
 }
+
 console.log(
   solution([4, 3, 4, 4, 4, 2]),
   solution([4, 4, 2, 5, 3, 4, 4, 4]),
-  solution([-1000000000, -1000000000] ),
-  solution([1,2,3,4,5,6,7,8,9])
-)
+  solution([-1000000000, -1000000000]),
+  solution([1, 2, 3, 4, 5, 6, 7, 8, 9]),
+);

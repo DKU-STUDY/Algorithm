f02ea5169f78447dd5c9d5e8d7f358837abca602
JeongShin <64301935+JeongShin@users.noreply.github.com>
2020-05-20 20:56:09 +0900

    Update codility_training/lessons.lesson09.MaximumSliceProblem.MaxSliceSum/JeongShin.js
    Co-authored-by: JunilHwang <tjsdlf4261@naver.com>

--- codility_training/lessons.lesson09.MaximumSliceProblem.MaxSliceSum/JeongShin.js
+++ codility_training/lessons.lesson09.MaximumSliceProblem.MaxSliceSum/JeongShin.js
@@ -1,7 +1,6 @@
 function solution(A) {
     return A.reduce(([max, acc], curr) => {
         max = Math.max(acc + curr, max, curr);
-        return acc < 0 ? [max, curr] : [max, acc + curr]
+        return [max, curr + !(acc < 0) * acc]
     }, [-Infinity, 0])[0];
 }
-

55a482fb9396ec6412de7114ea8a74219050d725
quavious <leenowon97@gmail.com>
2020-05-20 20:58:59 +0900

    Update NowonLee.js
    reduce function and infinity number used

--- codility_training/lessons.lesson10.PrimeAndCompositeNumbers.MinPerimeterRectangle/NowonLee.js
+++ codility_training/lessons.lesson10.PrimeAndCompositeNumbers.MinPerimeterRectangle/NowonLee.js
@@ -3,10 +3,15 @@ function solution(N) {
     for(let i = 1;i*i<=N;i++){
         if(N%i == 0) l.push(i)
     }
+    /*
     const nums = l.map(el => {
         return 2*(el + (N/el))
     })
     return Math.min(...nums)
+    */
+    return l.reduce((min, el) => {
+        return Math.min(min, 2 * (el + N/el))
+     }, Infinity)
 }
 
 console.log(solution(36)===24)

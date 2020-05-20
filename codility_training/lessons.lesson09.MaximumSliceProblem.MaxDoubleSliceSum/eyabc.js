4f756335e958e8d742120a68cd5984c100ce6399
eyabc <bey4314@naver.com>
2020-05-20 23:33:50 +0900

    lessons.lesson09.MaximumSliceProblem.MaxDoubleSliceSum

--- codility_training/lessons.lesson09.MaximumSliceProblem.MaxDoubleSliceSum/eyabc.js
+++ codility_training/lessons.lesson09.MaximumSliceProblem.MaxDoubleSliceSum/eyabc.js
@@ -1,27 +1,22 @@
-/* Correctness 100% Performance 14% (2 wrong answers) */
+/* @JeongShing 님꺼, MaximumSliceProblem 문제랑 prefixSums 의 문제를 합쳐서 푼것같다 .*/
 function solution(A) {
-  let res = 0;
-  for (let m = 1; m < A.length - 1; m++) {
-    let lower_max_ending = 0, lower_max_slice = 0, upper_max_ending = 0, upper_max_slice = 0;
-    console.log('---------------------')
-    for(let i = 1; i < m; i++) {
-      lower_max_ending = Math.max(0, lower_max_ending + A[i]);
-      lower_max_slice = Math.max(lower_max_slice, lower_max_ending);
-      console.log({i, lower_max_ending, lower_max_slice});
-    }
-    for(let i = m + 1; i < A.length - 1 ; i++) {
-        // console.log({i, upper_max_ending, upper_max_slice});
-      upper_max_ending = Math.max(0, upper_max_ending + A[i]);
-      upper_max_slice = Math.max(upper_max_slice, upper_max_ending);
-    }
-    console.log({m, lower_max_slice, upper_max_slice})
-    res = Math.max(res, lower_max_slice + upper_max_slice)
-  }
+  const len = A.length;
+  const lArr = [], rArr=[];
 
-  return res;
+  for (let i = 1; i < len; i++)
+    lArr[i] = Math.max(0, (lArr[i - 1] || 0) + A[i]);
+
+  for(let i = len-2; i >= 1; i--)
+    rArr[i] = Math.max(0, (rArr[i+1] || 0) + A[i]);
+
+  let maxSum = 0;
+
+  for(let i = 1; i < len-1; i++)
+    maxSum = Math.max(maxSum, (lArr[i-1] || 0) + (rArr[i+1] || 0));
+
+  return maxSum;
 }
 
 console.log(solution([5, -7, 3, 5, -2, 4, -1]))
-// console.log(solution([3, 2, 6, -1, 4, 5, -1, 2]))
-  // === 17
+console.log(solution([3, 2, 6, -1, 4, 5, -1, 2]))
 

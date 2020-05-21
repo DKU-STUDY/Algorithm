04ad0c19f8995688c155db6651120466a156d25f
khw970421 <khw970421@kakao.com>
2020-05-21 23:45:45 +0900

    5/21 수정

--- codility_training/lessons.lesson04.CountingElements.MaxCounters/khw970421.js
+++ codility_training/lessons.lesson04.CountingElements.MaxCounters/khw970421.js
@@ -1,5 +1,6 @@
+//large size time out
 function solution(N, A) {
-    let arrNumber = new Array(); //배열선언
+    const arrNumber = new Array().fill(0); //배열선언
     let max=0;
     for(let i=0;i<N;i++){
         arrNumber[i]=0;
@@ -14,6 +15,19 @@ function solution(N, A) {
         }
     },0);
     return arrNumber;
+}
+//준일형 소스코드 이해
+function solution1(N, A) {
+    let max = 0, Save = 0;
+    const arr = new Array(N).fill(0);
+    for(const v of A) {                             //A의 배열값을 v에 저장해 실행
+        if (v > N) Save = max;                      //배열값이 N 한계치보다 크다면 실행
+        else {
+            arr[v-1] = Math.max(arr[v-1], Save) + 1;        //Save랑 arr[v-1]의 값을 비교해 큰값에 1을 더한것을 저장
+            max = Math.max(arr[v-1], max);                  //변한값과 max 비교해 큰값을 max에 저장
+        }
+    }
+    return arr.map(v => Math.max(v, Save));                 //arr의 배열값중에 Save보다 작은값은 Save로 채우기
 }
 
 console.log(solution(5, [3, 4, 4, 6, 1, 4, 4]).toString()=== [3, 2, 2, 4, 2].toString());
--- codility_training/lessons.lesson04.CountingElements.PermCheck/khw970421.js
+++ codility_training/lessons.lesson04.CountingElements.PermCheck/khw970421.js
@@ -4,10 +4,10 @@ function solution(A) {
     A.reduce(function(accumulator,currentvalue){     //accumulator 0으로 설정 currentvalue A[0]으로 시작
         if(currentvalue-accumulator!=1)              // 둘의 차이가 1이 아니면 함수종료
             return -1;
-        else{                                        //둘의 차이가 1이면 count증가와 currentvalue값을 accumulator값으로 return
+                                             //둘의 차이가 1이면 count증가와 currentvalue값을 accumulator값으로 return
             count++;
             return currentvalue;
-        }
+
     },0)
 
     return (count==A.length)?1:0;                   //배열을 reduce에서 전부 순회하여 count와 A.length가 같으면 1 return 아니면 0 return

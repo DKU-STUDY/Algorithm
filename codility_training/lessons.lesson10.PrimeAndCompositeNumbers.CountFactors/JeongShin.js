94189ce0a89951219d0f33cde11a84ae466c08c0
JeongShin <sjeong1127@gmail.com>
2020-05-21 14:58:41 +0900

    daily commit

---
+++ codility_training/lessons.lesson10.PrimeAndCompositeNumbers.CountFactors/JeongShin.js
@@ -0,0 +1,9 @@
+function solution(N) {
+    if (N === 1)
+        return 1
+    const n = ~~Math.sqrt(N)
+    let count = 0
+    for (let i = 1; i <= n; i++)
+        count += (N % i === 0) * 1
+    return count * 2 - (N / n === n) * 1
+}

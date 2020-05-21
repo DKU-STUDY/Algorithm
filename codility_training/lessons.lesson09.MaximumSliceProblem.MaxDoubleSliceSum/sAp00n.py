bdd525c781d24f45a1accb9d4660e8b7cdef8b89
sAp00n <won7963kr@gmail.com>
2020-05-21 17:58:26 +0900

    nowWorking

--- codility_training/lessons.lesson09.MaximumSliceProblem.MaxDoubleSliceSum/sAp00n.py
+++ codility_training/lessons.lesson09.MaximumSliceProblem.MaxDoubleSliceSum/sAp00n.py
@@ -1,43 +1,21 @@
 # test case [5, 17, 0, 3], [0, 10, -5, -2, 0], [5, 5, 5] and all negative
 
 def solution(A):
-    if len(A) <= 3:
-        return 0
-    else:
-        B = A[1:-1]
-        print(f'B = {B}')
-        start_ind, end_ind = find_max_slice(B)
-
-        resulte = B[start_ind:end_ind]
-        if end_ind == None:
-            print(f'B[{start_ind}:{end_ind}] =  [{A[start_ind]} {resulte} {A[-1]}]')
+    print(f'A: {A}')
+    n = len(A)
+    left_sublist = []
+    right_sublist = []
+    maxsum = A[0]
+    for i in range(n - 1):
+        if i == 0:
+            left_sublist.append(max(0, A[i]))
         else:
-            print(f'B[{start_ind}:{end_ind}] =  [{A[start_ind]} {resulte} {A[end_ind + 1]}]')
-        if len(resulte) == 1:
-            if resulte[0] > 0:
-                return resulte[0]
-            else:
-                return 0
+            left_sublist.append(max(0, left_sublist[i - 1] + A[i]))
+        print(left_sublist)
+
+    for i in range(n - 2, 1, -1):
+        if i == 0:
+            right_sublist.append(max(0, A[i]))
         else:
-            resulte.remove(min(resulte))
-            return sum(resulte)
-
-
-def find_max_slice(A):
-    maximum = A[0]
-    temp = A[0]
-    start_ind = 0
-    end_ind = 1
-    for i in range(len(A)):
-        if A[i] >= temp + A[i]:
-            start_ind = i
-            end_ind = i + 1
-        temp = max(temp + A[i], A[i])
-        if temp >= maximum:
-            end_ind = i + 1
-        maximum = max(temp, maximum)
-        """print(f'temp = {temp}       max = {maximum}')
-        print(f'A[{start_ind}:{end_ind}] ={A[start_ind:end_ind]}')"""
-    if end_ind == len(A):
-        end_ind = None
-    return start_ind, end_ind
+            right_sublist.append(max(0, right_sublist[i - 1] + A[i]))
+        print(left_sublist)

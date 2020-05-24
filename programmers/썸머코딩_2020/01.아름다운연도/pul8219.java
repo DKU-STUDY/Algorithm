bbc40a3c91072a19ffe8984056757e0c6eaf9cc7
yurim <pul8219@naver.com>
2020-05-24 16:01:34 +0900

    Add solution2()

--- programmers/썸머코딩_2020/01.아름다운연도/pul8219.java
+++ programmers/썸머코딩_2020/01.아름다운연도/pul8219.java
@@ -1,6 +1,7 @@
 import java.util.HashSet;
 import java.util.Set;
 import java.util.Iterator;
+import java.util.stream.Collectors; //solution2()에서 사용
 
 class Solution {
     public int solution(int p) {
@@ -9,19 +10,19 @@ class Solution {
         Set<Integer> set = new HashSet<>();
 
         answer = p + 1;
-        for(;; answer++){
+        for (; ; answer++) {
             set.clear();
             String a_str = Integer.toString(answer);
             int[] a_arr = new int[a_str.length()];
-            for(int i = 0; i < a_str.length(); i++){
+            for (int i = 0; i < a_str.length(); i++) {
                 a_arr[i] = a_str.charAt(i) - '0';
                 set.add(a_arr[i]);
             }
             //Iterator<Integer> it = set.iterator();
 
-            if(set.size() != 4)
+            if (set.size() != 4)
                 continue;
-            else if(set.size() == 4){
+            else if (set.size() == 4) {
                 // while(it.hasNext())
                 //     System.out.println("set: " + it.next());
                 break;
@@ -30,6 +31,17 @@ class Solution {
         //System.out.println(answer);
 
         return answer;
+    }
+
+    // hwangjunil 풀이
+    // 관련 공부하고 이해할 것
+    public static int solution2(int p) {
+        for (int answer = p + 1; answer < 100000; answer++) {
+            String years = Integer.toString(answer);
+            Set<Integer> set = new HashSet<Integer>(years.chars().boxed().collect(Collectors.toList()));
+            if (set.size() == years.length()) return answer;
+        }
+        return -1;
     }
 }
 

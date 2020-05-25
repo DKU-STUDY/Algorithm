6785c32f63f8c10ff6a089d3b0d5c6953b8afd9f
sAp00n <won7963kr@gmail.com>
2020-05-25 14:30:39 +0900

    CountFactors O(n^(1/2))

--- codility_training/lessons.lesson10.PrimeAndCompositeNumbers.CountFactors/sAp00n.py
+++ codility_training/lessons.lesson10.PrimeAndCompositeNumbers.CountFactors/sAp00n.py
@@ -1,7 +1,16 @@
+import time
+start = time.time()
+
 def solution(N):
     number_of_factors = 0
-    for i in range(1, N+1):
+    i = 1
+    while i*i < N:
         if N % i == 0:
-            print(i)
-            number_of_factors += 1
+            number_of_factors += 2
+        i += 1
+    if i * i == N:
+        number_of_factors += 1
     return number_of_factors
+
+print(solution(1))
+print(f'time = {time.time()-start}')

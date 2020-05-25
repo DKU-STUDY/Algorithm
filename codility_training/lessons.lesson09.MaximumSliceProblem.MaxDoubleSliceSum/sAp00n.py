86bf818709316931f63777617978dcaeaa70d771
sAp00n <won7963kr@gmail.com>
2020-05-25 13:21:31 +0900

    Solved MaxDoubleSliceSum

--- codility_training/lessons.lesson09.MaximumSliceProblem.MaxDoubleSliceSum/sAp00n.py
+++ codility_training/lessons.lesson09.MaximumSliceProblem.MaxDoubleSliceSum/sAp00n.py
@@ -1,21 +1,27 @@
 # test case [5, 17, 0, 3], [0, 10, -5, -2, 0], [5, 5, 5] and all negative
 
 def solution(A):
-    print(f'A: {A}')
-    n = len(A)
-    left_sublist = []
-    right_sublist = []
-    maxsum = A[0]
-    for i in range(n - 1):
-        if i == 0:
-            left_sublist.append(max(0, A[i]))
-        else:
-            left_sublist.append(max(0, left_sublist[i - 1] + A[i]))
-        print(left_sublist)
+    mid = 1
+    total = 0
+    max_slice = 0
 
-    for i in range(n - 2, 1, -1):
-        if i == 0:
-            right_sublist.append(max(0, A[i]))
+    for idx, end in enumerate(A[2:-1], start=2):
+
+        if total < 0:
+            mid = idx
+            total = 0
+
+        elif total == 0 and A[idx - 1] > A[mid]:
+            mid = idx - 1
+            total = end
+
         else:
-            right_sublist.append(max(0, right_sublist[i - 1] + A[i]))
-        print(left_sublist)
+            if A[mid] > end:
+                total += A[mid]
+                mid = idx
+            else:
+                total += end
+
+        max_slice = max(max_slice, total)
+
+    return max_slice

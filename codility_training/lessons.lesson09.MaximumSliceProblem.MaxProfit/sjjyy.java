10e1cb053d8242482d7f62278f9839a7d1730928
sjjyy <43190565+sjjyy@users.noreply.github.com>
2020-05-27 16:39:07 +0900

    Create sjjyy.java

---
+++ codility_training/lessons.lesson09.MaximumSliceProblem.MaxProfit/sjjyy.java
@@ -0,0 +1,27 @@
+package lessons.lesson09.MaximumSliceProblem.MaxProfit;
+
+
+class sjjyy
+{
+	 public static int solution(int [] A)
+	 {
+		   int len = A.length;
+		   int max = Integer.MIN_VALUE;
+	     int min = Integer.MAX_VALUE;
+	     
+	     for (int i = 0 ; i < len ; i++)
+	     {
+	    	 min = Math.min(A[i], min);
+	    	 max = Math.max(max, A[i]-min);
+	     }
+		 return max;
+	 }
+
+
+	public static void main(String[] args)
+	{
+		int A[] = {23171, 21011, 21123, 21366, 21013, 21367};
+		System.out.println(solution(A)); // 356
+	}
+
+}

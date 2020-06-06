3711fe965fe4b800b97e29a4420fe486fd76a214
JeongShin <64301935+JeongShin@users.noreply.github.com>
2020-06-06 12:47:31 +0900

    Update codility_training/lessons.lesson15.CaterpillarMethod.AbsDistinct/JeongShin.js
    Co-authored-by: JunilHwang <tjsdlf4261@naver.com>

--- codility_training/lessons.lesson15.CaterpillarMethod.AbsDistinct/JeongShin.js
+++ codility_training/lessons.lesson15.CaterpillarMethod.AbsDistinct/JeongShin.js
@@ -1,9 +1,5 @@
 function solution(A) {
-    const set = new Set();
-    A.forEach(el => {
-        set.add(el < 0 ? el * -1 : el);
-    })
-    return set.size;
+    return new Set(A.map(v => Math.abs(v))).size;
 }
 
 solution([-5, -3, -1, 0, 3, 6]);

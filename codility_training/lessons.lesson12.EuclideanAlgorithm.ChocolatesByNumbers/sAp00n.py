cc8f1cbbde398e838cf2bdbf7cfdf17a6766b781
sAp00n <won7963kr@gmail.com>
2020-06-10 10:27:08 +0900

    Euclidean score 100%

--- codility_training/lessons.lesson12.EuclideanAlgorithm.ChocolatesByNumbers/sAp00n.py
+++ codility_training/lessons.lesson12.EuclideanAlgorithm.ChocolatesByNumbers/sAp00n.py
@@ -1,18 +1,12 @@
 def solution(N, M):
-    choco_list = [True for i in range(N) ]
-    idx = 0
-    return_val = 0
-    if N == 0:
-        return 0
-    else:
-        while choco_list[idx] != False:
-            print(f'idx: {idx}  ele : {choco_list[idx]}')
-            choco_list[idx] = False
-            idx += M
-            if idx >= N:
-                idx %= N
-            return_val += 1
-        return return_val
+    def gcd(N, M):
+        if N % M == 0:
+            return M
+        else:
+
+            return gcd(M, N % M)
+
+    return N // gcd(N, M)
 
 
-print(solution(947853, 4453))
+print(solution(10, 4))

183e8eab1657a764cc51bb77ec69c0f81fc9514b
JunilHwang <tjsdlf4261@naver.com>
2020-06-11 13:13:31 +0900

    Update programmers/난이도별/level01.문자열_내_마음대로_정렬하기/dkdlelk99.py

--- programmers/난이도별/level01.문자열_내_마음대로_정렬하기/dkdlelk99.py
+++ programmers/난이도별/level01.문자열_내_마음대로_정렬하기/dkdlelk99.py
@@ -1,7 +1,5 @@
 def solution(strings, n):
-    strings.sort()
-    strings = sorted(strings,key= lambda x : x[n])
-    return strings
+    return sorted(sorted(strings), key= lambda x : x[n])
 
 print(solution(	["sun", "bed", "car"],1) == ['car', 'bed', 'sun'])
 print(solution(	["abce", "abcd", "cdx"], 2) == ["abcd", "abce", "cdx"])

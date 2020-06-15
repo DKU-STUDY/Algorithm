891693a522151e4760a67e5971c9108d96550480
sjjyy <sjhy013@gmail.com>
2020-06-15 10:41:13 +0900

    Update sjjyy.java

--- codility_training/lessons.lesson12.EuclideanAlgorithm.ChocolatesByNumbers/sjjyy.java
+++ codility_training/lessons.lesson12.EuclideanAlgorithm.ChocolatesByNumbers/sjjyy.java
@@ -9,10 +9,7 @@ public class sjjyy
 
     public static int gcd(int a, int b) // using Uclidean algorithm
     {
-        if(a % b == 0)
-            return b;
-        else
-            return gcd(b, a % b);
+        return a % b == 0 ? b : gcd(b, a % b);
     }
 
     public static void main(String[] args)

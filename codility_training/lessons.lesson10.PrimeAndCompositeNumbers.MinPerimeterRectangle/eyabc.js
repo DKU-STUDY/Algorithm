2ae20e6c3bd336e2e6065daceffd0de5d5f7d1f2
JunilHwang <tjsdlf4261@naver.com>
2020-06-20 12:01:41 +0900

    Update codility_training/lessons.lesson10.PrimeAndCompositeNumbers.MinPerimeterRectangle/eyabc.js

--- codility_training/lessons.lesson10.PrimeAndCompositeNumbers.MinPerimeterRectangle/eyabc.js
+++ codility_training/lessons.lesson10.PrimeAndCompositeNumbers.MinPerimeterRectangle/eyabc.js
@@ -1,12 +1,9 @@
 // 최소 공약수의 쌍중 갭이 가장 작은 것의 둘레를 구한다
 function solution (N) {
-  const getPerimeter = (a, b) => 2 * (a + b);
-  let a = ~~Math.sqrt(N);
-
-  for (;a > 0; a--) {
-    if (!(N % a)) {
+  for (let a = ~~Math.sqrt(N); a > 0; a--) {
+    if (!(N % a)) { 
       const b = N / a;
-      return getPerimeter(a, b);
+      return 2 * (a + b);
     }
   }
 }
@@ -14,4 +11,3 @@ function solution (N) {
 console.log(solution(982451653))
 console.log(solution(10000000000))
 console.log(solution(1))
-

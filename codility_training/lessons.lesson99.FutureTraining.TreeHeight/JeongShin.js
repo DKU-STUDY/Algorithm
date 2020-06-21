1408cb8eba5de22a233764568a4f0db1612a00e5
JeongShin <sjeong1127@gmail.com>
2020-06-21 15:36:19 +0900

    daily commit

---
+++ codility_training/lessons.lesson99.FutureTraining.TreeHeight/JeongShin.js
@@ -0,0 +1,8 @@
+function solution(T) {
+    const depth = tree => {
+        if (!tree)
+            return 0;
+        return Math.max(depth(tree.l), depth(tree.r)) + 1;
+    }
+    return Math.max(depth(T.l), depth(T.r))
+}

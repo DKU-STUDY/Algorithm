a52791a23269543a2fc7bcc9a3fb804ba1e88395
JeongShin <64301935+JeongShin@users.noreply.github.com>
2020-06-24 22:38:38 +0900

    Update programmers/난이도별/level03.단속카메라/JeongShin.js
    Co-authored-by: JunilHwang <tjsdlf4261@naver.com>

--- programmers/난이도별/level03.단속카메라/JeongShin.js
+++ programmers/난이도별/level03.단속카메라/JeongShin.js
@@ -1,14 +1,12 @@
 function solution(routes) {
-    let ans = 0;
     let min = -Infinity;
-    routes.sort((a, b) => a[1] - b[1]);
-    routes.forEach(val => {
-        if (min < val[0]) {
-            min = val[1];
+    return routes.sort((a, b) => a[1] - b[1]).reduce((ans, [start, end]) => {
+        if (min < start) {
+            min = end;
             ans++;
         }
-    })
-    return ans;
+        return ans;
+    }, 0);
 }
 
 solution([[-20, 15], [-14, -5], [-18, -13], [-5, -3]]);

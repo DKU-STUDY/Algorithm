e284ac7a3e61d855506c83ec10aa44dc30ca3e2e
eyabc <bey4314@naver.com>
2020-06-29 22:38:45 +0900

    lessons.lesson11.SieveofEratosthenes.CountNonDivisible 2

--- codility_training/lessons.lesson11.SieveofEratosthenes.CountNonDivisible/eyabc.js
+++ codility_training/lessons.lesson11.SieveofEratosthenes.CountNonDivisible/eyabc.js
@@ -1,6 +1,19 @@
-function solution(A) {
-  return A.map(v => A.filter(w => v < w || v % w).length);
+function solution (A) {
+  const sortedA = [...A];
+  const len = A.length;
+  sortedA.sort((a, b) => a - b);
+
+  return A.map(v => {
+      let index = sortedA.lastIndexOf(v);
+      let res = 0;
+      for (let i = 0 ; i < index ; i++)
+        if (v % sortedA[i] === 0) res++;
+      return len - res - 1;
+    },
+  );
 }
+
 console.log(
-  solution([3, 1, 2, 3, 6])
-)
+  solution([3, 1, 2, 3, 6]),
+  // .toString() == [2, 4, 3, 2, 0].toString()
+);

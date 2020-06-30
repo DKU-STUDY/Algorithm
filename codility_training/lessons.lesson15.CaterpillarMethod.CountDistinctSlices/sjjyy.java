2bb6e2e1172b84aedc514f68f26e44a73af87706
sjjyy <sjhy013@gmail.com>
2020-06-30 17:22:51 +0900

    Create sjjyy.java

---
+++ codility_training/lessons.lesson15.CaterpillarMethod.CountDistinctSlices/sjjyy.java
@@ -0,0 +1,41 @@
+package lessons.lesson15.CaterpillarMethod.CountDistinctSlices;
+
+public class sjjyy
+{
+    public static int solution(int M, int[] A)
+    {
+        int len = A.length;
+        int [] appearence = new int[M+1];
+
+        int front = 0;
+        int back = 0;
+        int result = 0;
+
+        while(back < len)
+        {
+            while (front < len && appearence[A[front]] == 0)
+            {
+                appearence[A[front]] += 1;
+
+                result += (front - back + 1);
+                front += 1;
+            }
+
+            appearence[A[back]] = 0;
+            back += 1;
+
+            if (result >= 1_000_000_000)
+                return 1_000_000_000;
+
+        }
+        return result;
+    }
+
+
+    public static void main(String[] args)
+    {
+        int M = 6;
+        int[] A = {3, 4, 5, 5, 2};
+        System.out.println(solution(M, A)); // 9
+    }
+}

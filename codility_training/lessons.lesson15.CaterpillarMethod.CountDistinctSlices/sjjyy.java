aa5ca9572ad5b210f273231e6b35dbcb358f8d0c
sjjyy <sjhy013@gmail.com>
2020-07-01 09:44:27 +0900

    Update sjjyy.java

--- codility_training/lessons.lesson15.CaterpillarMethod.CountDistinctSlices/sjjyy.java
+++ codility_training/lessons.lesson15.CaterpillarMethod.CountDistinctSlices/sjjyy.java
@@ -2,10 +2,10 @@ package lessons.lesson15.CaterpillarMethod.CountDistinctSlices;
 
 public class sjjyy
 {
-    public static int solution(int M, int[] A) // performance 80%
+    public static int solution(int M, int[] A)
     {
         int len = A.length;
-        int [] appearence = new int[M+1];
+        boolean [] appearence = new boolean[M+1];
 
         int front = 0;
         int back = 0;
@@ -13,22 +13,23 @@ public class sjjyy
 
         while(back < len)
         {
-            while (front < len && appearence[A[front]] == 0)
+            while (front < len && !appearence[A[front]])
             {
-                appearence[A[front]] += 1;
+                appearence[A[front]] = true;
 
                 result += (front - back + 1);
                 front += 1;
+
+                if (result >= 1000000000)
+                    return 1000000000;
             }
 
-            appearence[A[back]] = 0;
+            appearence[A[back]] = false;
             back += 1;
 
-            // if (result >= 1_000_000_000)
-            //    return 1_000_000_000;
-
         }
-        return Math.min(result, 1000000000);
+
+        return result;
     }
 
 

4419735c88292652c24fc043e8e82a55ff05f211
JeongShin <sjeong1127@gmail.com>
2020-07-03 18:36:30 +0900

    code review edited

--- programmers/난이도별/level03.베스트앨범/JeongShin.js
+++ programmers/난이도별/level03.베스트앨범/JeongShin.js
@@ -1,8 +1,7 @@
 function solution(genres, plays) {
     const [album, genre] = plays.reduce(([alb, gen], curr, idx) => {
         const genre = genres[idx];
-        if (!alb[genre])
-            alb[genre] = [];
+        alb[genre] = alb[genre] || [];
         alb[genre].push([idx, curr]);
         gen[genre] = (gen[genre] || 0) + curr;
         return [alb, gen];
@@ -10,20 +9,14 @@ function solution(genres, plays) {
 
     const sortedGenre = [];
 
-    for (const [key, val] of Object.entries(genre)) {
-        sortedGenre.push([key, val]);
-    }
+    sortedGenre.push(...Object.entries(genre));
 
     sortedGenre.sort((a, b) => b[1] - a[1]);
 
     return sortedGenre.reduce((answer, curr) => {
         const genre = curr[0];
         const sortedAlbum = album[genre];
-        sortedAlbum.sort((a, b) => {
-            if (a[1] === b[1]) // 앨범 판매량이 같을 경우 인덱스를 비교
-                return b[0] - a[0];
-            return a[1] - b[1]
-        });
+        sortedAlbum.sort((a, b) => a[1] === b[1] ? b[0] - a[0] : a[1] - b[1]);
         let count = 0;
         while (sortedAlbum.length && count < 2) {
             const idx = sortedAlbum.pop()[0];

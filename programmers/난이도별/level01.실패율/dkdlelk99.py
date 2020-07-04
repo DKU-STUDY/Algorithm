8d1e5a56b33a8bef7f3b7e0c839a8d3e7af3eacf
dkdlelk99 <49552766+dkdlelk99@users.noreply.github.com>
2020-07-04 22:02:57 +0900

    Update dkdlelk99.py

--- programmers/난이도별/level01.실패율/dkdlelk99.py
+++ programmers/난이도별/level01.실패율/dkdlelk99.py
@@ -1,17 +1,14 @@
 def solution(N, stages):
-    answer = []
-    failure_list = []
+    user = len(stages)
     failure = {}
     fail_sum = 0
     for stage in range(1, N+1):
-        failure[stage] = (stages.count(stage) / (len(stages) - fail_sum))
-        fail_sum += stages.count(stage)
-    failure_list = sorted(failure.items(), key = lambda x: x[1], reverse=True)
-    for key, val in failure_list:
-        answer.append(key)
-    return answer
+        if user != fail_sum:
+            failure[stage] = (stages.count(stage) / (user - fail_sum))
+            fail_sum += stages.count(stage)
+        else:
+            failure[stage] = 0
+    return [ x[0] for x in sorted(failure.items(), key = lambda x: x[1], reverse=True) ]
 
 print(solution(5, [2, 1, 2, 6, 2, 4, 3, 3]) == [3,4,2,1,5])
 print(solution(4, [4,4,4,4,4]) == [4,1,2,3])
-
-# 런타임 에러로 실패

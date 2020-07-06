9286b52ae6660e2500eeb634c165071c63796b6e
sjjyy <sjhy013@gmail.com>
2020-07-06 17:39:34 +0900

    Create sjjyy.java

---
+++ programmers/난이도별/level01.2016년/sjjyy2.java
@@ -0,0 +1,31 @@
+public class sjjyy2
+{
+    public static String solution(int a, int b)
+    {
+        int days = 0;
+        String [] Day = {"SUN","MON","TUE","WED","THU","FRI","SAT"};
+
+        for(int i = 1 ; i < a ; i++)
+        {
+            if(i == 2)
+                days += 29;
+            else if(i == 4 || i == 6 || i == 9 || i == 11)
+                days += 30;
+            else
+                days += 31;
+        }
+
+        days += 4 + b;
+
+        return Day[days % 7];
+
+
+    }
+
+    public static void main(String[] args)
+    {
+        int a = 5;
+        int b = 24;
+        System.out.println(solution(a,b));
+    }
+}

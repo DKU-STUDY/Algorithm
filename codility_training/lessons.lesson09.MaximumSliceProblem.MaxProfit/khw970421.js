bd0460055bdf53acb7dc6429ab7bc1e5003cc7d0
khw970421 <khw970421@kakao.com>
2020-07-06 21:12:56 +0900

    7/6수정

--- codility_training/lessons.lesson09.MaximumSliceProblem.MaxProfit/khw970421.js
+++ codility_training/lessons.lesson09.MaximumSliceProblem.MaxProfit/khw970421.js
@@ -3,12 +3,12 @@
 function solution(A) {
     let max=0;
     let count=0;
-    let p=0;
-    let A_size=A.length;
+    const A_size=A.length;
     for(let i=0;i<A_size;i++){
         for(let j=i+1;j<A_size;j++){
             if(A[j]<A[i])                   //이전값보다 작으면 이윤이 없으므로 아예 시도 x
                 break;
+            count=A[j]-A[i];
             max=(count<max)?max:count;      //더큰 이윤을 계산
 
         }

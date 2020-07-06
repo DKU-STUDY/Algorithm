0319ed0e0ee9f59801f65bb8f0eb445d1f86cf8c
eyabc <bey4314@naver.com>
2020-07-06 20:04:59 +0900

    lessons.lesson11.SieveofEratosthenes.CountSemiprimes 3

--- codility_training/lessons.lesson11.SieveofEratosthenes.CountSemiprimes/eyabc.js
+++ codility_training/lessons.lesson11.SieveofEratosthenes.CountSemiprimes/eyabc.js
@@ -1,32 +1,17 @@
 function solution (N, P, Q) {
   const semiCnt = new Array(N + 1).fill(0),
     maxNum = N + 1;
-
-  const isPrime = (i) => {
-    for (let j = 2 ; j * j <= i ; j++)
-      if (i % j === 0) return false;
-    return true;
-  };
-
   // 참고함
   const isSemi = (num) => {
     let cnt = 0;
     for (let i = 2 ; cnt < 2 && i * i <= num ; ++i)
-      while (num % i === 0)
-        num /= i, ++cnt; // Increment count of prime numbers
-
-    // If number is greater than 1, add it to
-    // the count variable as it indicates the
-    // number remain is prime number
-    if (num > 1)
-      ++cnt;
-
-    // 나누어 지는 수가 2개 초과면 semi 가 아니다.
+      while (num % i === 0) num /= i, ++cnt;
+    if (num > 1) ++cnt;
     return cnt === 2;
   };
 
   for (let i = 1 ; i < maxNum ; i++)
-    semiCnt[i] = semiCnt[i - 1] + (isPrime(i) ? 0 : isSemi(i) ? 1 : 0);
+    semiCnt[i] = semiCnt[i - 1] + (isSemi(i) ? 1 : 0);
 
   return P.map((start, k) => semiCnt[Q[k]] - semiCnt[start - 1]);
 }

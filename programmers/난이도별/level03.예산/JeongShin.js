295c3b275d9a9956c7acc35a831dd8fc07799fd0
JeongShin <sjeong1127@gmail.com>
2020-07-08 01:55:33 +0900

    daily commit

---
+++ programmers/난이도별/level03.예산/JeongShin.js
@@ -0,0 +1,25 @@
+function solution(budgets, M) {
+    const dividable = (arr, bud) => {
+        const result = arr.reduce((acc, curr) => acc + (curr < bud ? curr : bud), 0);
+        return result <= M
+    };
+
+    const len = budgets.length;
+    const sorted = budgets.sort((a, b) => a - b);
+    let [low, high] = [sorted[0], sorted[len - 1]];
+
+    if (M < low * len)
+        return ~~(M/len);
+
+    while (low <= high) {
+        const mid = ~~((low + high) / 2);
+        if (dividable(sorted, mid))
+            low = mid + 1;
+        else
+            high = mid - 1;
+    }
+
+    return high;
+}
+
+solution([120, 110, 140, 150], 100);

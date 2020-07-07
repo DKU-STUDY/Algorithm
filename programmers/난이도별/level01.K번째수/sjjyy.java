408103b04985112faeab712c32dc26a36cbe9eef
sjjyy <sjhy013@gmail.com>
2020-07-07 10:58:50 +0900

    Update sjjyy.java

--- programmers/난이도별/level01.K번째수/sjjyy.java
+++ programmers/난이도별/level01.K번째수/sjjyy.java
@@ -25,10 +25,26 @@ public class sjjyy {
         return answer;
     }
 
+    public static int[] solution2(int[] array, int[][] commands) // 다른 사람 풀이
+    {
+        int[] answer = new int[commands.length];
+
+        for(int i = 0 ; i < commands.length ; i++)
+        {
+            int [] temp = Arrays.copyOfRange(array, commands[i][0]-1, commands[i][1]);
+            Arrays.sort(temp);
+            answer[i] = temp[commands[i][2]-1];
+        }
+
+        return answer;
+
+    }
+
     public static void main(String [] args)
     {
         int[] arr= {1, 5, 2, 6, 3, 7, 4};
         int[][] commands = {{2, 5, 3}, {4, 4, 1}, {1, 7, 3}};
         System.out.println(Arrays.toString(solution(arr, commands))); // [5, 6, 3]
+        System.out.println(Arrays.toString(solution2(arr, commands))); // [5, 6, 3]
     }
 }

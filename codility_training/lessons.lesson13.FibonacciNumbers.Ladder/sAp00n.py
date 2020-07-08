1c601bf43379c8baa96c577aa0fa5866f75f7448
sAp00n <59313590+sAp00n@users.noreply.github.com>
2020-07-08 09:03:03 +0900

    Update codility_training/lessons.lesson13.FibonacciNumbers.Ladder/sAp00n.py
    Co-authored-by: JunilHwang <tjsdlf4261@naver.com>

--- codility_training/lessons.lesson13.FibonacciNumbers.Ladder/sAp00n.py
+++ codility_training/lessons.lesson13.FibonacciNumbers.Ladder/sAp00n.py
@@ -6,12 +6,9 @@ def solution(A, B):
 
 
 def getFibo(N):
-    fibo_list = []
-    fibo_list = [0] * N
-    fibo_list[0] = 1
-    fibo_list[1] = 1
+    fibo_list = [1, 1]
     for i in range(2, N):
-        fibo_list[i] = fibo_list[i - 1] + fibo_list[i - 2]
+        fibo_list.append(fibo_list[i - 1] + fibo_list[i - 2])
 
     return fibo_list
 

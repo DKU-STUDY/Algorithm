cfb5aa5cf624201db14c8d44bfd78d0df8d99b6c
sAp00n <won7963kr@gmail.com>
2020-07-08 17:28:58 +0900

    lesson11 ContSemiPrimes 재도전.. sevie func 구현 완료. 리스트 이진탐색 구현중....

--- codility_training/lessons.lesson13.FibonacciNumbers.Ladder/sAp00n.py
+++ codility_training/lessons.lesson13.FibonacciNumbers.Ladder/sAp00n.py
@@ -2,19 +2,18 @@ def solution(A, B):
     # write your code in Python 3.6
     N = max(A) + 1
     fibo_list = getFibo(N)
-    return_list = [0] * len(A)
+    return_list = []
     for idx in range(len(A)):
-        return_list[idx] = fibo_list[A[idx]] % 2 ** B[idx]
+        return_list.append(fibo_list[A[idx]] % 2 ** B[idx])
     return return_list
 
 
 def getFibo(N):
     fibo_list = []
-    fibo_list = [0] * N
-    fibo_list[0] = 1
-    fibo_list[1] = 1
+    fibo_list.append(1)
+    fibo_list.append(1)
     for i in range(2, N):
-        fibo_list[i] = fibo_list[i - 1] + fibo_list[i - 2]
+        fibo_list.append(fibo_list[i - 1] + fibo_list[i - 2])
 
     return fibo_list
 

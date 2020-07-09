d9759a98f6ddb946d8adafb6c421f9d04a08b7c3
RockMiin <52374789+RockMiin@users.noreply.github.com>
2020-07-09 10:46:58 +0900

    Update rockmiin.py

--- programmers/난이도별/level01.K번째수/rockmiin.py
+++ programmers/난이도별/level01.K번째수/rockmiin.py
@@ -7,9 +7,3 @@ def solution(array, commands):
         cut=sorted(cut)
         result.append(cut[commands[i][2]-1])
     return result
-
-if __name__=="__main__":
-    array=[1,5,2,6,3,7,4]
-    commands=[[2,5,3],[4,4,1],[1,7,3]]
-
-    solution(array, commands)

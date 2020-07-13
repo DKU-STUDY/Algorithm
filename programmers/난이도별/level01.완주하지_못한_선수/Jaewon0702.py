d7f77d7aaa156e9a5742d36aefdd336fc3ae6c25
Jaewon LEE <67958329+Jaewon0702@users.noreply.github.com>
2020-07-13 11:13:41 +0900

    Update Jaewon0702.py

--- programmers/난이도별/level01.완주하지_못한_선수/Jaewon0702.py
+++ programmers/난이도별/level01.완주하지_못한_선수/Jaewon0702.py
@@ -2,7 +2,7 @@ def solution(participant, completion):
     answer=''  
     participant.sort()
     completion.sort()
-    completion.append(0)
+    completion.append('')
     for i in range(len(completion)) :
         if participant[i]!=completion[i] :
             answer=participant[i]

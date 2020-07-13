fd28d7c1499bff675482a152967c6d17125125fd
sjjyy <sjhy013@gmail.com>
2020-07-13 09:33:42 +0900

    Update sjjyy.java

--- programmers/난이도별/level01.같은_숫자는_싫어/sjjyy.java
+++ programmers/난이도별/level01.같은_숫자는_싫어/sjjyy.java
@@ -26,9 +26,27 @@ public class sjjyy
         return ans;
     }
 
+    public static int [] solution2(int[] array)
+    {
+        List<Integer> arrayList = new ArrayList<>();
+        int len = array.length - 1;
+
+        for(int i = 0 ; i < len ; i++)
+        {
+            if(array[i] != array[i+1])
+                arrayList.add(array[i]);
+        }
+
+        arrayList.add(array[len]); // 마지막 인덱스 값
+
+       return arrayList.stream().mapToInt(Integer::intValue).toArray();
+    }
+
+
     public static void main(String [] args)
     {
         int[] arr = {1, 1, 3, 3, 0, 1, 1};
         System.out.println(Arrays.toString(solution(arr))); // [1, 3, 0, 1]
+        System.out.println(Arrays.toString(solution2(arr))); // [1, 3, 0, 1]
     }
 }

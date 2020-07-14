ebd85db9ba0aa1eedb380557027107d7ede433f9
H-yeonJeong <h-yeonjeong@naver.com>
2020-07-14 23:27:51 +0900

    level01.약수의_합

---
+++ programmers/난이도별/level01.모의고사/HyeonJeong.java
@@ -0,0 +1,20 @@
+package 난이도별.level01.모의고사;
+
+/*public class HyeonJeong {
+    class Solution {
+        public int[] solution(int[] answers) {
+            int st1_scores = 0, st2_scores = 0, st3_scores = 0;
+            int [] student1 = [1, 2, 3, 4, 5];
+            int student2 = [2, 1, 2, 3, 2, 4, 2, 5];
+            int student3 = [3, 3, 1, 1, 2, 2, 4, 4, 5, 5];
+
+            if(answer[] % 5 == students1 % 5)
+
+                return answer;
+        }
+        public static void main(String[] args){
+
+        }
+    }
+}
+*/

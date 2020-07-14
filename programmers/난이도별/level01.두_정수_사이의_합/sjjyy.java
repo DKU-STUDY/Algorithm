d2f70b11b2a3ea61f9d2c8957cfc965963194035
sjjyy <sjhy013@gmail.com>
2020-07-14 10:36:30 +0900

    Update sjjyy.java

--- programmers/난이도별/level01.두_정수_사이의_합/sjjyy.java
+++ programmers/난이도별/level01.두_정수_사이의_합/sjjyy.java
@@ -1,5 +1,7 @@
 package 난이도별.level01.두_정수_사이의_합;
 
+import java.util.stream.LongStream;
+
 public class sjjyy
 {
     public static long solution(int a, int b)
@@ -31,6 +33,12 @@ public class sjjyy
         return ans;
     }
 
+    public static long solution3(int a, int b)
+    {
+        int min = Math.min(a, b), max = Math.max(a, b);
+        return LongStream.rangeClosed(min, max).reduce(0, Long::sum);
+    }
+
     public static void main(String [] args)
     {
         System.out.println(solution(3, 5)); // 12
@@ -40,5 +48,9 @@ public class sjjyy
         System.out.println(solution2(3, 5)); // 12
         System.out.println(solution2(3, 3)); // 3
         System.out.println(solution2(5, 3)); // 12
+
+        System.out.println(solution3(3, 5)); // 12
+        System.out.println(solution3(3, 3)); // 3
+        System.out.println(solution3(5, 3)); // 12
     }
 }

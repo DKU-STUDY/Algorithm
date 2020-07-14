6ffa7df408c840126d209a3d1af61977df7c32b5
H-yeonJeong <67951802+H-yeonJeong@users.noreply.github.com>
2020-07-14 20:19:01 +0900

    Update HyeonJeong.java

--- programmers/난이도별/level01.평균_구하기/HyeonJeong.java
+++ programmers/난이도별/level01.평균_구하기/HyeonJeong.java
@@ -3,8 +3,8 @@ package 난이도별.level01.평균_구하기;
 public class HyeonJeong {
     class Solution {
         public double solution(int[] arr) {
-            float average;
-            float total = 0;
+            double average;
+            double total = 0;
 
             for(int i = 0; i < arr.length; i++)
                 total += arr [i];
@@ -13,12 +13,3 @@ public class HyeonJeong {
         }
     }
 }
-
-/*
-문제 설명
-정수를 담고 있는 배열 arr의 평균값을 return하는 함수, solution을 완성해보세요.
-
-제한사항
-arr은 길이 1 이상, 100 이하인 배열입니다.
-arr의 원소는 -10,000 이상 10,000 이하인 정수입니다.
- */

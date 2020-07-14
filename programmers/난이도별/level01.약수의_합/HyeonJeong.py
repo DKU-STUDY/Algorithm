1b0d842a59c7f325f00ba0352d24d9a852654751
H-yeonJeong <h-yeonjeong@naver.com>
2020-07-14 23:19:01 +0900

    level01.약수의_합

---
+++ programmers/난이도별/level01.약수의_합/HyeonJeong.py
@@ -0,0 +1,22 @@
+#처음에 한 코드
+'''
+def solution(n):
+    total = 0
+    for i in range(1, n+1):
+        if (n % i == 0):
+            total += i
+    return total
+'''
+
+
+#더 효율적인 코드
+def solution(n):
+    total = 0
+    for i in range(1, n // 2 + 1):
+        if (n % i == 0):
+            total += i
+    return total + n
+
+#테스트를 위한 코드
+print(solution(12))
+

9eef50d8e28b279394e5dd9852996d636fc30f49
sangmandu <45033215+sangmandu@users.noreply.github.com>
2020-07-15 14:23:41 +0900

    Add files via upload

---
+++ codility_training/lessons.lesson02.Arrays.CyclicRotation/sangmandu.py
@@ -0,0 +1,9 @@
+# you can write to stdout for debugging purposes, e.g.
+# print("this is a debug message")
+
+def solution(A, K):
+    # write your code in Python 3.6
+    for i in range(K):
+        A.insert(0,A.pop())
+    return A
+    pass

870cc6072c714cc2cd8531247e2309c7b069ca3b
H-yeonJeong <67951802+H-yeonJeong@users.noreply.github.com>
2020-07-15 20:16:24 +0900

    Update HyeonJeong.py

--- programmers/난이도별/level01.약수의_합/HyeonJeong.py
+++ programmers/난이도별/level01.약수의_합/HyeonJeong.py
@@ -12,7 +12,7 @@ def solution(n):
 def solution(n):
     total = 0
     for i in range(1, n // 2 + 1):
-        if (n % i == 0):
+        if n % i == 0:
             total += i
     return total + n
 

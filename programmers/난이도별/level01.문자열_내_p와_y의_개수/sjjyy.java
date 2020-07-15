5058eb8f89f00d23c88bbf7c224aab230c3e79c2
sjjyy <sjhy013@gmail.com>
2020-07-15 13:43:34 +0900

    Update sjjyy.java

--- programmers/난이도별/level01.문자열_내_p와_y의_개수/sjjyy.java
+++ programmers/난이도별/level01.문자열_내_p와_y의_개수/sjjyy.java
@@ -10,11 +10,31 @@ public class sjjyy {
         return s.toLowerCase().chars().filter(i -> 'p' == i).count() == s.toLowerCase().chars().filter(i -> 'y' == i).count();
     }
 
+    public static boolean solution2(String s)
+    {
+        int p = 0, y = 0;
+        s = s.toLowerCase();
+
+        for (int i = 0 ; i < s.length(); i++)
+        {
+            if (s.charAt(i) == 'p')
+                p++;
+            if(s.charAt(i) == 'y')
+                y++;
+        }
+
+        return p == y;
+    }
+
     public static void main(String[] args)
     {
         String s = "Pyy";
         System.out.println(solution(s)); // false
         System.out.println(solution("pPoooyY")); // true
         System.out.println(solution("oooo")); // true
+
+        System.out.println(solution2("PypY")); // true
+        System.out.println(solution2("oooo")); // true
+        System.out.println(solution2("pPoooyY")); // true
     }
 }

00c48387b27d013ca8823b6cc2638c8b48353393
sjjyy <sjhy013@gmail.com>
2020-07-16 14:42:15 +0900

    Create sjjyy.java

---
+++ programmers/난이도별/level01.문자열_다루기_기본/sjjyy.java
@@ -0,0 +1,26 @@
+package 난이도별.level01.문자열_다루기_기본;
+
+public class sjjyy {
+    public static boolean solution(String s)
+    {
+        int len = s.length();
+
+        if (len == 4 || len == 6)
+        {
+            try {
+                int x = Integer.parseInt(s); // 문자열 -> 정수
+                return true;
+            } catch (NumberFormatException e) { // 숫자 형식 오류
+                return false;
+            }
+        }
+
+        return false;
+    }
+
+    public static void main(String[] args)
+    {
+        System.out.println(solution("1234")); // true
+        System.out.println(solution("s123")); // false
+    }
+}

8c42176ca393d169667ac54ca1a48150f096991e
dkdlelk99 <49552766+dkdlelk99@users.noreply.github.com>
2020-07-16 22:31:34 +0900

    Update dkdlelk99.py

--- programmers/난이도별/level02.쇠막대기/dkdlelk99.py
+++ programmers/난이도별/level02.쇠막대기/dkdlelk99.py
@@ -1,24 +1,15 @@
 def solution(arrangement):
-    index = []
     stack = []
     answer = 0
     arrangement = arrangement.replace("()", "|")
-    
-    # (에 대응하는 ) 찾아서 인덱싱
-    for i in range(len(arrangement)):
-        if arrangement[i] == '(':
-            index.append(i)
-        elif arrangement[i] ==')':
-            stack.append([index.pop(), i])        
-    
-    # 막대기 양끝 사이에 몇개의 레이저가 있는지 검사
-    for len_of_stick in stack:
-        raser = 1
-        for text in range(len_of_stick[0],len_of_stick[1]):
-            if arrangement[text] == '|':
-                raser += 1
-        answer += raser
-             
+    for i in arrangement:
+        if i == '(':
+            stack.append(i)
+        elif i == ')':
+            stack.pop()
+            answer += 1
+        else:
+            answer += len(stack)
     return answer
     
 print(solution("()(((()())(())()))(())") == 17)

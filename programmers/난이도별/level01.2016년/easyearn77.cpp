7507802dbc16fc2d62175a8cf297e6412ba34a06
easyearn77 <58412156+easyearn77@users.noreply.github.com>
2020-07-17 19:55:06 +0900

    Update programmers/난이도별/level01.2016년/easyearn77.cpp
    Co-authored-by: JunilHwang <tjsdlf4261@naver.com>

--- programmers/난이도별/level01.2016년/easyearn77.cpp
+++ programmers/난이도별/level01.2016년/easyearn77.cpp
@@ -4,8 +4,8 @@
 using namespace std;
 
 string solution(int a, int b) {
-    const string dow[7]={"THU", "FRI", "SAT", "SUN", "MON", "TUE", "WED"};
-    const int dom[12]={31,29,31,30,31,30,31,31,30,31,30,31};
+    const string dow[]={"THU", "FRI", "SAT", "SUN", "MON", "TUE", "WED"};
+    const int dom[]={31,29,31,30,31,30,31,31,30,31,30,31};
     int day=b;
     for(int i=0;i<a-1;i++){
         day+=dom[i];

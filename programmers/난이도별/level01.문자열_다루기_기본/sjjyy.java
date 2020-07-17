80f35da43c89c3afd573bc203267444bbce51802
sjjyy <sjhy013@gmail.com>
2020-07-17 10:00:22 +0900

    Update sjjyy.java

--- programmers/난이도별/level01.문자열_다루기_기본/sjjyy.java
+++ programmers/난이도별/level01.문자열_다루기_기본/sjjyy.java
@@ -18,9 +18,24 @@ public class sjjyy {
         return false;
     }
 
+    public static boolean solution2(String s)
+    {
+        int len = s.length();
+
+        if (len == 4 || len == 6)
+            return s.matches("[0-9]+");
+
+        return false;
+
+    }
+
     public static void main(String[] args)
     {
         System.out.println(solution("1234")); // true
         System.out.println(solution("s123")); // false
+
+        System.out.println(solution2("1234")); // true
+        System.out.println(solution2("s123")); // false
+        System.out.println(solution2("4323s2")); // false
     }
 }

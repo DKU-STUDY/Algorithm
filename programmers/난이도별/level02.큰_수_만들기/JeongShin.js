1194f6f8b7a3153de5f58f101f47bb9b59cab7ee
JeongShin <sjeong1127@gmail.com>
2020-07-18 11:05:45 +0900

    daily commit

--- programmers/난이도별/level02.큰_수_만들기/JeongShin.js
+++ programmers/난이도별/level02.큰_수_만들기/JeongShin.js
@@ -1,22 +1,53 @@
+// function solution(number, k) {
+//
+//     let deleteCount = 0
+//     let removeIdx
+//
+//     while (deleteCount < k) {
+//         const len = number.length
+//         removeIdx = len - 1
+//         for (let i = 0; i < len - 1; i++) {
+//             if (number[i] < number[i + 1]) {
+//                 removeIdx = i
+//                 break
+//             }
+//         }
+//         deleteCount++
+//         number = number.replace(number[removeIdx], "")
+//     }
+//
+//     return number
+// }
+//
+// console.log(solution("4177252841", 4))
+
 function solution(number, k) {
-
+    const stack = []
+    const len = number.length
+    let top = 0
     let deleteCount = 0
-    let removeIdx
 
-    while (deleteCount < k) {
-        const len = number.length
-        removeIdx = len - 1
-        for (let i = 0; i < len - 1; i++) {
-            if (number[i] < number[i + 1]) {
-                removeIdx = i
-                break
-            }
+    for (let i = 0; i < len; i++) {
+        const curr = number[i]
+        if (top === 0) {
+            stack.push(curr)
+            top++
+            continue
         }
-        deleteCount++
-        number = number.replace(number[removeIdx], "")
+        while (top > 0 && stack[top - 1] < curr) {
+            stack.pop()
+            deleteCount++
+            top--;
+            if (top === 0)
+                break
+            if (deleteCount === k)
+                return stack.join('') + number.slice(i, len)
+        }
+        stack.push(curr)
+        top++
     }
-
-    return number
+    return stack.join('').slice(0, len - k)
 }
 
 console.log(solution("4177252841", 4))
+

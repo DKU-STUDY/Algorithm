eab0bd2b8eee29862d001490efd2f5a297b5ac65
H-yeonJeong <h-yeonjeong@naver.com>
2020-07-19 23:04:59 +0900

    level01.문자열_다루기_기본

--- programmers/난이도별/level01.문자열_다루기_기본/HyeonJeong.py
+++ programmers/난이도별/level01.문자열_다루기_기본/HyeonJeong.py
@@ -1,9 +1,9 @@
 def solution(s):
-    if len(s) == 4 or 6 :
-        list(s)
-        for i in s :
-            if s[i] in "0123456789":
-                answer = True
-    else :
-        answer = False
-    return answer
+    # 조건에 안맞는 것들을 바로 return 시키면 더 효율적인 코드가 됨.
+    if len(s) != 4 and 6 :
+        return False
+
+    for i in s :
+        if i not in "0123456789":
+            return False
+    return True

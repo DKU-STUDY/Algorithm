267ab07136c23d14b776623544c5aaf50c569e03
H-yeonJeong <67951802+H-yeonJeong@users.noreply.github.com>
2020-07-19 20:54:00 +0900

    Delete HyeonJeong.py

--- programmers/난이도별/level01.문자열_다루기_기본/HyeonJeong.py
+++
@@ -1,9 +0,0 @@
-def solution(s):
-    if len(s) == 4 or 6 :
-        list(s)
-        for i in s :
-            if s[i] in "0123456789":
-                answer = True
-    else :
-        answer = False
-    return answer

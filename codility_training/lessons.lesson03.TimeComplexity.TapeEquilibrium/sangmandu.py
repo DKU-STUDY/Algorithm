aad3799f84b92f20f3ba0f016e9888c7c80a3e90
sangmandu <45033215+sangmandu@users.noreply.github.com>
2020-07-20 11:09:04 +0900

    Update sangmandu.py

--- codility_training/lessons.lesson03.TimeComplexity.TapeEquilibrium/sangmandu.py
+++ codility_training/lessons.lesson03.TimeComplexity.TapeEquilibrium/sangmandu.py
@@ -13,16 +13,7 @@ def solution(A):
             
     return min
 
-'''
-A[0] = 3
-A[1] = 1
-A[2] = 2
-A[3] = 4
-A[4] = 3
-We can split this tape in four places:
-
-P = 1, difference = |3 − 10| = 7
-P = 2, difference = |4 − 9| = 5
-P = 3, difference = |6 − 7| = 1
-P = 4, difference = |10 − 3| = 7
-'''
+print(
+    solution([3,1,2,4,3]) == 1,
+    # solution(A) == result,
+)

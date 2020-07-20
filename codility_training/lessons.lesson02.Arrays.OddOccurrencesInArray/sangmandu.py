50ff0dd37ab871ce56fc2fad6d91ae752e6fd0a5
sangmandu <45033215+sangmandu@users.noreply.github.com>
2020-07-20 10:29:58 +0900

    Update codility_training/lessons.lesson02.Arrays.OddOccurrencesInArray/sangmandu.py
    Co-authored-by: JunilHwang <tjsdlf4261@naver.com>

--- codility_training/lessons.lesson02.Arrays.OddOccurrencesInArray/sangmandu.py
+++ codility_training/lessons.lesson02.Arrays.OddOccurrencesInArray/sangmandu.py
@@ -1,9 +1,11 @@
 
 def solution(A):
 	sA = sorted(A)
-	if len(sA) == 0:
+        size = len(sA)
+	if size == 0:
 		return sA[2*i]
-	for i in range(len(sA)//2):
-		if sA[2*i] != sA[2*i+1]:
-			return sA[2*i]
+	for i in range(size // 2):
+		if sA[2 * i] != sA[2 * i + 1]:
+			return sA[2 * i]
+
 	

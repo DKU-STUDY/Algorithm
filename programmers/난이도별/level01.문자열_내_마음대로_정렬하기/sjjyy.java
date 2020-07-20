df137712bfa79fedd839a14311419f97847c98da
sjjyy <sjhy013@gmail.com>
2020-07-20 11:02:07 +0900

    Update sjjyy.java

--- programmers/난이도별/level01.문자열_내_마음대로_정렬하기/sjjyy.java
+++ programmers/난이도별/level01.문자열_내_마음대로_정렬하기/sjjyy.java
@@ -1,6 +1,9 @@
 package 난이도별.level01.문자열_내_마음대로_정렬하기;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 
 public class sjjyy
 {
@@ -19,12 +22,31 @@ public class sjjyy
         return ans;
     }
 
+    public static String[] solution2(String[] strings, int n)
+    {
+        List<String> list = new ArrayList<>();
+
+        for (String string : strings)
+            list.add(string.charAt(n) + string);
+
+        Collections.sort(list);
+        String [] ans = new String[list.size()];
+
+        for (int i = 0 ; i < list.size() ; i++)
+            ans[i] = list.get(i).substring(1);
+
+        return ans;
+    }
+
     public static void main(String [] args)
     {
         String [] strings = {"sun", "bed", "cat"};
         String [] strs = {"abcd", "abce", "cdx"};
 
-        System.out.println(Arrays.toString(solution(strings, 1)));
-        System.out.println(Arrays.toString(solution(strs, 2)));
+        //System.out.println(Arrays.toString(solution(strings, 1)));
+        //System.out.println(Arrays.toString(solution(strs, 2)));
+
+        System.out.println(Arrays.toString(solution2(strings, 1)));
+        System.out.println(Arrays.toString(solution2(strs, 2)));
     }
 }

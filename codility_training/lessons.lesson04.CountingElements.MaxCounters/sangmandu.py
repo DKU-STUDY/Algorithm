2358f60d624c48b8a28279be2f8f9710e656cb70
sangmandu <45033215+sangmandu@users.noreply.github.com>
2020-07-20 15:21:49 +0900

    lessons.lesson04.CountingElements.MaxCounters

---
+++ codility_training/lessons.lesson04.CountingElements.MaxCounters/sangmandu.py
@@ -0,0 +1,32 @@
+# you can write to stdout for debugging purposes, e.g.
+# print("this is a debug message")
+
+def solution(N, A):
+    # write your code in Python 3.6
+    pass
+    arr = [0 for i in range(N)]
+    tog = 0
+    mx = 0
+    for i in A:
+        if(i == N+1):
+            if(tog == 0):
+                tog = 1
+                arr = [mx] * N
+            continue
+        arr[i-1] += 1
+        mx = max(arr[i-1], mx)
+        tog = 0
+    return arr
+
+print(
+    solution(1,[1,1,1,2]) == [3]
+    # solution(X,A) == result,
+)
+
+'''
+print(
+    solution(5,[3,4,4,6,1,4,4]) == [3,2,2,4,2]
+    # solution(X,A) == result,
+)
+'''
+

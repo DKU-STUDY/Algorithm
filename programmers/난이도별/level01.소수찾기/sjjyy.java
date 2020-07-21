2f137a5e110576bb1048904b6654c9089be2fb8d
sjjyy <sjhy013@gmail.com>
2020-07-21 14:26:37 +0900

    Update sjjyy.java

--- programmers/난이도별/level01.소수찾기/sjjyy.java
+++ programmers/난이도별/level01.소수찾기/sjjyy.java
@@ -1,17 +1,14 @@
 package 난이도별.level01.소수찾기;
 
-public class sjjyy
-{
-    public static int solution(int n) // 75.0 / 100.0
+public class sjjyy {
+    public static int solution(int n) // 75/100
     {
         int ans = 0;
 
-        for (int i = 2 ; i <= n ; i++)
-        {
+        for (int i = 2; i <= n; i++) {
             boolean isPrime = true;
 
-            for (int j = 2 ; j*j <= i ; j++)
-            {
+            for (int j = 2; j * j <= i; j++) {
                 if (i % j == 0) {
                     isPrime = false;
                     break;
@@ -25,8 +22,38 @@ public class sjjyy
         return ans;
     }
 
-    public static void main(String [] args)
+    public static int solution2(int n) // 100/100
     {
+        int ans = 0;
+        int[] num = new int[n + 1];
+
+        for (int i = 2; i <= n; i++) // 2~n까지 배열에 추가
+            num[i] = i;
+
+        for (int i = 2; i <= n; i++)
+        {
+            if(num[i] == 0)
+                continue;
+
+            for (int j = 2*i ; j <= n ; j+=i) // 값이 0이 아닌 수의 배수는 모두 0ㅇ로 처리
+                num[j]= 0;
+        }
+
+        for (int i = 2 ; i <= n ; i++) // 값이 0이 아닌 수 (= 소수)의 개수 측정
+        {
+            if (num[i] != 0)
+                ans++;
+        }
+
+        return ans;
+    }
+
+    public static void main(String[] args) {
         System.out.println(solution(10));
+        System.out.println(solution2(10)); // 4
+
+        System.out.println(solution(5));
+        System.out.println(solution2(5)); // 3
+
     }
 }

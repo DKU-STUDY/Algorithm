3ac38f6f2c3c9b775320df723dd866a5c1d03f4b
H-yeonJeong <h-yeonjeong@naver.com>
2020-07-22 15:38:34 +0900

    level01.이상한_문자_만들기

--- programmers/난이도별/level01.이상한_문자_만들기/HyeonJeong.py
+++ programmers/난이도별/level01.이상한_문자_만들기/HyeonJeong.py
@@ -1,11 +1,14 @@
 def solution(s):
     answer = ""
-    for c in range(len(s)):
-        if s[c] == " " :
-            answer += " "
-        else:
-            if c % 2 == 0 :
-                answer += s[c].upper()
+    list = s.split(" ")
+    for n in list:
+        for m in range(len(list[n])):
+            str = list[n][m]
+            if "a" <= str <= "z" and c % 2 == 0:
+                answer += str.upper()
+            elif "A" <= str <= "Z" and c % 2 == 1:
+                answer += str.lower()
             else:
-                answer += s[c].lower()
+                answer += str
+        answer += " "
     return answer

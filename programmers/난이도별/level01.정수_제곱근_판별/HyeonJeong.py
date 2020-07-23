cc4c49622b4c095a9cdf53b6b394a8db29efe6e8
H-yeonJeong <h-yeonjeong@naver.com>
2020-07-23 23:39:17 +0900

    level01.정수_제곱근_판별

--- programmers/난이도별/level01.정수_제곱근_판별/HyeonJeong.py
+++ programmers/난이도별/level01.정수_제곱근_판별/HyeonJeong.py
@@ -1,6 +1,6 @@
 def solution(n):
     for i in range(1, n + 1):
-        if n ** 0.5 == i:
+        if n ** 0.5 == i: # if i ** 2 == n도 가능
             return (i + 1) ** 2
         elif i == n:
             return -1

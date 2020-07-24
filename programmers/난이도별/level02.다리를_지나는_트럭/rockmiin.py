f1dc48ff910343e75e247a05caf0158d978dd187
rockmiin <csm1959@naver.com>
2020-07-24 10:55:07 +0900

    update rockmiin.py

--- programmers/난이도별/level02.다리를_지나는_트럭/rockmiin.py
+++ programmers/난이도별/level02.다리를_지나는_트럭/rockmiin.py
@@ -6,13 +6,13 @@ def solution(bridge_length, weight, truck_weight):
     while truck_weight!=[]:
         if sum(on_bridge)+truck_weight[0]<=weight:
             on_bridge.append(truck_weight.pop(0))
-        # print(on_bridge, cnt, total_time)
         for i in range(pass_truck_count, pass_truck_count+len(on_bridge)):
             cnt[i]+=1
             if bridge_length==cnt[i]:
                 on_bridge.pop(0); pass_truck_count+=1
         total_time += 1
     return total_time+bridge_length-1
+
 
 print(
     solution(2, 10, [7, 4, 5, 6])==8

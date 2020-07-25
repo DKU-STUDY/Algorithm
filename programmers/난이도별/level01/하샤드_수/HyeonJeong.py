1f79bff3a233e8e581b8e82f475f3b18ee93a102
H-yeonJeong <h-yeonjeong@naver.com>
2020-07-25 19:53:12 +0900

    level01.하샤드_수

--- programmers/난이도별/level01/하샤드_수/HyeonJeong.py
+++ programmers/난이도별/level01/하샤드_수/HyeonJeong.py
@@ -4,5 +4,5 @@ def solution(x):
     for i in str(x):
         if num > 0:
             total += (num % 10)
-            num = num / 10
-    return True if x % total == 0 else False
+            num //= 10
+    return x % total == 0

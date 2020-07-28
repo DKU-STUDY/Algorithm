ac18ce6dc82fd291d4ecde9f9e2cff09581d470d
sAp00n <won7963kr@gmail.com>
2020-07-28 16:13:13 +0900

    solved

--- codility_training/lessons.lesson15.CaterpillarMethod.CountDistinctSlices/sAp00n.py
+++ codility_training/lessons.lesson15.CaterpillarMethod.CountDistinctSlices/sAp00n.py
@@ -1,25 +1,21 @@
+# 딕셔너리로 중복된 인덱스  구하고, 그 인덱스가 들어간 슬라이스의 개수만 계산해서 전체 경우의 수에서 제거하는 방법은? 조은거같은데
 def solution(M, A):
-    slice_count = 0
-    counter = {}
-    for value in range(0, M + 1): counter[value] = 0
-    print(counter)
-    for i in range(len(A)):
-        for j in range(i, len(A)):
-            condition = True
-            temp = counter.copy()
-            print(f'range: A[{i}]:A[{j}]')
-            for k in range(i, j + 1):
-                temp[A[k]] += 1
-                if temp[A[k]] >= 2:
-                    condition = False
-                    break
-            if condition:
-                slice_count += 1
-            print(f'counter: {temp}\n')
-
-    return slice_count
+    appearance = [False] * (M + 1)
+    N = len(A)
+    front = 0
+    slices = 0
+    for back in range(N):
+        while front < N and appearance[A[front]] == 0:
+            appearance[A[front]] += 1
+            slices += front - back +1
+            front += 1
+        appearance[A[back]] -= 1
+        if slices >= 1000000000: return 1000000000
+    return slices
 
 
 M = 6
-A = [0]
+# A = [0]
+A = [3, 4, 5, 5, 2]
+#A = [1, 0, 1, 1, 0, 0]
 print(solution(M, A))

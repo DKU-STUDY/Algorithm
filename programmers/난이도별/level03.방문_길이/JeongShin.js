e8f463b80a1d2ac72a01cb6a530e01e24baef0b7
JeongShin <sjeong1127@gmail.com>
2020-07-30 00:11:46 +0900

    불필요 변수 삭제

--- programmers/난이도별/level03.방문_길이/JeongShin.js
+++ programmers/난이도별/level03.방문_길이/JeongShin.js
@@ -35,30 +35,23 @@ class player {
             return;
 
         const move = currPos + 'to' + nextPos;
-        const opposite = nextPos + 'to' + currPos;
 
         if (!this.visited[move]) {
             this.visited[move] = true;
-            this.visited[opposite] = true;
+            this.visited[nextPos + 'to' + currPos] = true;
             this.count++;
         }
     }
 
     checkOutOfBound() {
-        const [x, y] = [Math.abs(this.x), Math.abs(this.y)]
-        if (x > 5) {
-            this.x = this.x - Math.sign(this.x);
-        }
-        if (y > 5) {
-            this.y = this.y - Math.sign(this.y);
-        }
+        this.x -= (Math.sign(this.x) * (Math.abs(this.x) > 5))
+        this.y -= (Math.sign(this.y) * (Math.abs(this.y) > 5))
     }
 }
 
 function solution(dirs) {
     const p = new player(0, 0);
-    for (const dir of dirs) {
+    for (const dir of dirs)
         p.move(dir);
-    }
     return p.count;
 }

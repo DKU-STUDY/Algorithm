32a705bf51ecb2ed5672cf061b0f2dbb7581ef3c
sangmandu <45033215+sangmandu@users.noreply.github.com>
2020-07-29 17:32:25 +0900

    lessons.lesson07.StacksandQueues.Nesting

---
+++ codility_training/lessons.lesson07.StacksandQueues.Nesting/sangmandu.py
@@ -0,0 +1,20 @@
+# you can write to stdout for debugging purposes, e.g.
+# print("this is a debug message")
+
+def solution(S):
+    # write your code in Python 3.6
+    pass
+    cnt = 0
+    for i in S:
+        if i == "(":
+            cnt += 1
+        else:
+            cnt -= 1
+
+        if (cnt < 0):
+            return 0
+    return 1 if cnt == 0 else 0
+
+pritn(
+    solution("(()(())())") == 1
+)

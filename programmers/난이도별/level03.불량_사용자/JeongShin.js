c9408c554df637ea81e0e7e9a84f53ac3508fad3
JeongShin <sjeong1127@gmail.com>
2020-08-01 22:02:18 +0900

    level03.불량 사용자 풀이완료

--- programmers/난이도별/level03.불량_사용자/JeongShin.js
+++ programmers/난이도별/level03.불량_사용자/JeongShin.js
@@ -1,50 +1,41 @@
 function solution(user_id, banned_id) {
-    const cache = {'0': 1};
-    const factorial = num => {
-        let result;
-        if (cache[num])
-            result = cache[num];
-        else
-            result = cache[num] = num * factorial(num - 1);
-        return result;
-    };
-
-    const rFact = Object.values(banned_id.reduce((acc, curr) => {
-        acc[curr] = (acc[curr] || 0) + 1;
-        return acc;
-    }, {})).reduce((acc, curr) => acc * factorial(curr), 1);
-
-
-    const matches = [];
+    const possible_id = [];
     for (const s of banned_id) {
-        const re = s.replace(/\*/g, "\\w") + '$';
-        const match = (user_id.reduce((arr, str) => {
-            const match = str.match(re);
-            if (match) {
-                arr.push(match.input);
-            }
-            return arr;
-        }, []));
-        matches.push(match);
+        const matched = (user_id.reduce((arr, str) => {
+                const r = str.match('^' + s.replace(/\*/g, ".") + '$');
+                if (r)
+                    arr.push(r.input);
+                return arr;
+            }, [])
+        );
+        possible_id.push(matched);
     }
 
+    const answer = new Set();
+    const checkPossibleAnswer = (stack) => {
+        stack.sort();
+        const r = stack.join('');
+        if (!answer.has(r))
+            answer.add(r);
+    };
+
     const len = banned_id.length;
-    let answer = 0;
 
     const combination = (stack, idx) => {
         if (idx === len) {
-            answer++;
+            checkPossibleAnswer(stack);
             return;
         }
-        matches[idx].forEach((str) => {
-            if (stack.indexOf(str) === -1) {
-                combination([...stack, str], idx + 1)
+        possible_id[idx].forEach((str) => {
+            if (!~stack.indexOf(str)) {
+                combination([...stack, str], idx + 1);
             }
         })
-    };
+    }
 
     combination([], 0);
-    return answer / rFact;
+
+    return answer.size;
 }
 
 

168ae3d4ad79efd23006680751805863f1fa1871
H-yeonJeong <h-yeonjeong@naver.com>
2020-08-03 22:11:24 +0900

    level01.x만큼_간격이_있는_n개의_숫자

--- programmers/난이도별/level01.x만큼_간격이_있는_n개의_숫자/HyeonJeong.py
+++ programmers/난이도별/level01.x만큼_간격이_있는_n개의_숫자/HyeonJeong.py
@@ -1,2 +1,15 @@
+#방법1
 def solution(x, n):
-    return [a for a in range(x, x * (n + 1), x)]
+    if x == 0 :
+        return [0] * n
+    return [a for a in range(x, x * n + x, x)]
+
+#방법2
+def solution(x, n):
+    return [x * a for a in range(1, n + 1)]
+
+print(
+    solution(2, 5) == [2,4,6,8,10],
+    solution(4, 3) == [4,8,12],
+    solution(-4, 2) == [-4, -8]
+)

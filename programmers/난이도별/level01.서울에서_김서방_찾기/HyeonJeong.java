913f4471f2e3e8edef41e549cebfb4cd83b324c0
H-yeonJeong <h-yeonjeong@naver.com>
2020-08-05 12:16:39 +0900

    level01.서울에서_김서방_찾기

--- programmers/난이도별/level01.서울에서_김서방_찾기/HyeonJeong.java
+++ programmers/난이도별/level01.서울에서_김서방_찾기/HyeonJeong.java
@@ -1,18 +1,15 @@
 package level01.서울에서_김서방_찾기;
 
 public class HyeonJeong {
-    class Solution {
-        public String solution(String[] seoul) {
-            String answer = "";
-            for(int i = 0; i < seoul.length; i++)
-                if (seoul [i] == "Kim")
-                    answer = "김서방은 i에 있다";
-
+    public String solution(String[] seoul) {
+        String answer = "";
+        for (int i = 0; i < seoul.length; i++)
+            if (seoul[i] == "Kim")
+                answer = "김서방은 " + i + "에 있다";
             return answer;
-        }
-        public void main(String[] args) {
-            String [] array = {"Jane","Kim"};
-            System.out.println(solution(array) == "김서방은 1에 있다");
+    }
+    public void main(String[] args) {
+        String [] array = {"Jane","Kim"};
+        System.out.println(solution(array) == "김서방은 1에 있다");
         }
     }
-}

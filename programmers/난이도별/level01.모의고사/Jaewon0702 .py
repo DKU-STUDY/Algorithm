5a7c61d62c9c335d2866ea398a5ac33023ece958
Jaewon Lee <67958329+Jaewon0702@users.noreply.github.com>
2020-08-05 20:44:29 +0900

    Update Jaewon0702 .py

--- programmers/난이도별/level01.모의고사/Jaewon0702 .py
+++ programmers/난이도별/level01.모의고사/Jaewon0702 .py
@@ -1,23 +1,15 @@
 def solution(answers):
-    math_hater1=[1,2,3,4,5]
-    math_hater2=[2,1,2,3,2,4,2,5]
-    math_hater3=[3,3,1,1,2,2,4,4,5,5]
-    scores=[0]*3; First_place=[]
+    math_haters=[[1,2,3,4,5],
+                 [2,1,2,3,2,4,2,5],
+                 [3,3,1,1,2,2,4,4,5,5]]
+    scores=[0]*len(math_haters)
 
     for i,answer in enumerate(answers) :
-        if answer==math_hater1[i%len(math_hater1)] :
-            scores[0]+=1
+        for k,marking in enumerate(math_haters) :
+            if answer==marking[i%len(marking)] :
+                scores[k]+=1
+    return [i+1 for i,score in enumerate(scores) if max(scores)==score]
 
-        if answer==math_hater2[i%len(math_hater2)] :
-            scores[1]+=1
-
-        if answer==math_hater3[i%len(math_hater3)] :
-            scores[2]+=1
-
-    for i,score in enumerate(scores) :
-        if max(scores)==score :
-            First_place.append(i+1)
-    return First_place
-
+#100점
 print(solution([1,2,3,4,5])==[1])
 print(solution([1,3,2,4,2])==[1,2,3])

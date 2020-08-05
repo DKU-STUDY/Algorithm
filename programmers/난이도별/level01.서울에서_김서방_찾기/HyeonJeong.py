9f85ef246ef5735e476d8e65df81979d735d658c
H-yeonJeong <h-yeonjeong@naver.com>
2020-08-05 11:52:50 +0900

    level01.서울에서_김서방_찾기

--- programmers/난이도별/level01.서울에서_김서방_찾기/HyeonJeong.py
+++ programmers/난이도별/level01.서울에서_김서방_찾기/HyeonJeong.py
@@ -7,7 +7,7 @@ def solution(seoul):
             # return "김서방은 {}에 있다".format(m)
 '''
 
-#방법2 : "Kim"이 리스트에서 한번만 존재하므로 index()사용
+#방법2 : "Kim"이 리스트에 한번만 존재하므로 index()사용
 def solution(seoul):
     return "김서방은 %d에 있다" %index("Kim")
     # return "김서방은 {}에 있다".format(seoul.index("Kim"))

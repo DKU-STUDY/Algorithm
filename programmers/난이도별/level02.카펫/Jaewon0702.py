2b080378e29665d5c854e9a8e4a3bb2922608d13
Jaewon Lee <67958329+Jaewon0702@users.noreply.github.com>
2020-08-07 14:41:27 +0900

    Update Jaewon0702.py

--- programmers/난이도별/level02.카펫/Jaewon0702.py
+++ programmers/난이도별/level02.카펫/Jaewon0702.py
@@ -1,7 +1,7 @@
 import math
 def solution(brown, yellow) :
-    row=int(( 2 + brown/2 + math.sqrt( (2 + brown/2)**2 - 4*(brown + yellow))) /2) #근의 공식
-    return[row,int (brown/2- row + 2)]
+    row=int((2 + brown/2 + math.sqrt( (2 + brown/2)**2 - 4*(brown + yellow))) /2) #근의 공식
+    return[row,int (brown/2 - row + 2)]
 
 #100점
 print(solution(10,2)==[4,3])

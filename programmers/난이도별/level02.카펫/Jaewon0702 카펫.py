e5b4e798db6669d8d30ecf1868b7085ad45cc41f
Jaewon Lee <67958329+Jaewon0702@users.noreply.github.com>
2020-08-07 12:01:50 +0900

    Jaewon0702 level02. 카펫

---
+++ programmers/난이도별/level02.카펫/Jaewon0702 카펫.py
@@ -0,0 +1,12 @@
+import math
+def solution(brown, yellow):
+    row=int((2+brown/2+math.sqrt((2+brown/2)**2-4*(brown+yellow)))/2) #근의 공식
+    return[row,int(brown/2-row+2)]
+
+#100점
+print(solution(10,2)==[4,3])
+print(solution(8,1)==[3,3])
+print(solution(24,24)==[8,6])
+
+
+

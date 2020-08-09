1634a098ea69a9958d40268b96cccc670a6a6e8a
H-yeonJeong <h-yeonjeong@naver.com>
2020-08-09 15:30:57 +0900

    level01.소수찾기

--- programmers/난이도별/level01.소수찾기/HyeonJeong.py
+++ programmers/난이도별/level01.소수찾기/HyeonJeong.py
@@ -1,3 +1,5 @@
+#시도
+'''
 def solution(n):
     answer = [2]
     for x in range(3, n + 1, 2):
@@ -10,8 +12,17 @@ def solution(n):
                 elif y == x - 2:
                     answer.append(x)
     return len(answer)
+'''
 
+# 에라토스테네스의 체 코드로 완성
+def solution(n):
+    num = [False, False] + [True] * (n - 1)
+    for i in range(2, n + 1):
+        if num[i] == True:
+            for j in range(i + i, n + 1, i):
+                num[j] = False
 
+    return len([i for i in range(2, n + 1) if num[i] == True])
 
 print(
     solution(10) == 4,

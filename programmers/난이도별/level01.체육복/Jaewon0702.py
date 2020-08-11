0923f8e0df1cc2f776c67bf0b898713368a5b24d
Jaewon Lee <67958329+Jaewon0702@users.noreply.github.com>
2020-08-11 21:55:38 +0900

    Update Jaewon0702.py

--- programmers/난이도별/level01.체육복/Jaewon0702.py
+++ programmers/난이도별/level01.체육복/Jaewon0702.py
@@ -1,8 +1,8 @@
 def solution(n, lost, reserve):
     inter=set(lost)&set(reserve); lost=set(lost)-inter; reserve=set(reserve)-inter
     for res in reserve:
-        if res-1 in lost : lost.remove(res-1); continue
-        elif res+1 in lost : lost.remove(res+1); continue
+        if res-1 in lost : lost.remove(res-1)
+        elif res+1 in lost : lost.remove(res+1)
     return n-len(lost)
 #100점
 print(solution(5,[2,4],[1,3,5])==5)

7217afd57367fe724e117d46cc178f52d15eb1d6
Jaewon Lee <67958329+Jaewon0702@users.noreply.github.com>
2020-08-11 21:53:09 +0900

    Jaewon0702 level 01. 체육복

---
+++ programmers/난이도별/level01.체육복/체육복.py
@@ -0,0 +1,11 @@
+def solution(n, lost, reserve):
+    inter=set(lost)&set(reserve); lost=set(lost)-inter; reserve=set(reserve)-inter
+    for res in reserve:
+        if res-1 in lost : lost.remove(res-1); continue
+        elif res+1 in lost : lost.remove(res+1); continue
+    return n-len(lost)
+#100점
+print(solution(5,[2,4],[1,3,5])==5)
+print(solution(5,[2,4],[3])==4)
+
+

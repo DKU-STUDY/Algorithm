eea89a74ae14657b16d2959368e3a3a3e1e788dd
sjjyy <sjhy013@gmail.com>
2020-08-13 14:39:07 +0900

    Update sjjyy.java

--- programmers/난이도별/level01.정수_내림차순으로_배치하기/sjjyy.java
+++ programmers/난이도별/level01.정수_내림차순으로_배치하기/sjjyy.java
@@ -2,6 +2,9 @@ package 난이도별.level01.정수_내림차순으로_배치하기;
 
 import java.util.Arrays;
 
+import static java.util.stream.Collectors.collectingAndThen;
+import static java.util.stream.Collectors.joining;
+
 public class sjjyy
 {
     public static long solution(long n)
@@ -22,8 +25,18 @@ public class sjjyy
         return Long.parseLong(stringBuilder.toString());
     }
 
+    public static long solution2(long n)
+    {
+        return Arrays.stream(Long.toString(n).split(""))
+                .map(Integer::parseInt)
+                .sorted((a, b) -> b - a)
+                .map(String::valueOf)
+                .collect(collectingAndThen(joining(""), Long::parseLong));
+    }
+
     public static void main(String[] args)
     {
         System.out.println(solution(98023214)); // 98432210
+        System.out.println(solution2(98023214)); // 98432210
     }
 }

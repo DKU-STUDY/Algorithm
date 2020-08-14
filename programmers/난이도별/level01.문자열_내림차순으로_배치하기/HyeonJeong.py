4f5f760419b20f8320ea212445b66a2224566fd0
H-yeonJeong <h-yeonjeong@naver.com>
2020-08-14 21:32:08 +0900

    level01.문자열_내림차순으로_배치하기

--- programmers/난이도별/level01.문자열_내림차순으로_배치하기/HyeonJeong.py
+++ programmers/난이도별/level01.문자열_내림차순으로_배치하기/HyeonJeong.py
@@ -4,3 +4,5 @@ def solution(s):
     for x in s:
         capital.append(x) if 65 <= ord(x) <= 90 else small.append(x)
     return "".join(sorted(small + capital, reverse = True))
+
+print(solution("Zbcdefg") == "gfedcbZ")

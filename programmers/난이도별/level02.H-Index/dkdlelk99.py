f50730997debeb6ac1f779ed99b84b16ab595a62
dkdlelk99 <49552766+dkdlelk99@users.noreply.github.com>
2020-08-14 23:35:49 +0900

    Update dkdlelk99.py

--- programmers/난이도별/level02.H-Index/dkdlelk99.py
+++ programmers/난이도별/level02.H-Index/dkdlelk99.py
@@ -1,12 +1,13 @@
 def solution(citations):
     a = []
     for h in range(max(citations)):
-        answer = 0 # 논문 갯수
+        cnt = 0 # 논문 개수
         for nonmun in citations:
             if nonmun >= h:
-                answer += 1
+                cnt += 1
         a.append(h)
-        if answer < h:
+        if cnt < h:
             return a[h - 1]
+    return 0
 
 print(solution([3, 0, 6, 1, 5]), solution([3, 0, 6, 1, 5]) == 3)

93468e528c5129e78d0865070cf8e92519893118
H-yeonJeong <h-yeonjeong@naver.com>
2020-08-16 18:18:15 +0900

    level01.모의고사

--- programmers/난이도별/level01.모의고사/HyeonJeong.py
+++ programmers/난이도별/level01.모의고사/HyeonJeong.py
@@ -3,9 +3,9 @@ def solution(answers):
     check = [[1, 2, 3, 4, 5], [2, 1, 2, 3, 2, 4, 2, 5], [3, 3, 1, 1, 2, 2, 4, 4, 5, 5]]
 
     for i, n in enumerate(answers):
-        for m in range(len(student)):
-            if check[m][i % len(check[m])] == n:
-                student[m] += 1
+        for j, m in enumerate(check):
+            if m[i % len(m)] == n:
+                student[j] += 1
 
     return [s + 1 for s in range(len(student)) if student[s] == max(student)]
 

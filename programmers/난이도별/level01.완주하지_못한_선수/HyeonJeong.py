44c819384365b2a71e6cea3affe821cbaa92560f
H-yeonJeong <h-yeonjeong@naver.com>
2020-08-17 22:48:33 +0900

    level01.완주하지_못한_선수

---
+++ programmers/난이도별/level01.완주하지_못한_선수/HyeonJeong.py
@@ -0,0 +1,13 @@
+def solution(participant, completion):
+    participant.sort()
+    completion.sort()
+    for i in range(len(completion)):
+        if participant[i] != completion[i]:
+            return participant[i]
+    return participant[-1]
+
+print(
+    solution(['leo', 'kiki', 'eden'], ['eden', 'kiki'] ) == 'leo',
+    solution(['marina', 'josipa', 'nikola', 'vinko', 'filipa'], ['josipa', 'filipa', 'marina', 'nikola'] ) == 'vinko',
+    solution(['mislav', 'stanko', 'mislav', 'ana'], ['stanko', 'ana', 'mislav']) == 'mislav'
+)

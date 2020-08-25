96d69d5f9d996c30b476edd09d8188319e8bf5e3
H-yeonJeong <h-yeonjeong@naver.com>
2020-08-25 20:17:31 +0900

    level02.조이스틱

--- programmers/난이도별/level02/조이스틱/HyeonJeong.py
+++ programmers/난이도별/level02/조이스틱/HyeonJeong.py
@@ -1,21 +1,23 @@
 def solution(name):
     answer = 0
-    if name == "AAA" or name == "AAAA":
-        return answer
-    for n in name:
-        if ord(n) > 78:
-            answer += 91 - ord(n) # 이전 알파벳으로
+    locat = 0
+    list = [91 - ord(n) if ord(n) > 78 else ord(n) - 65 for n in name]
+    while 1:
+        answer += list[locat]
+        list[locat] = 0
+        if sum(list) == 0: break
+        left = 1
+        right = 1
+        while list[locat + right] == 0:
+            right += 1
+        while list[locat - left] == 0:
+            left += 1
+        if left >= right:
+            locat += right
+            answer += right
         else:
-            answer += ord(n) - 65 # 다음 알파벳으로
-        answer += 1 # 커서 다음으로 이동
-    answer -= 1 # 마지막 알파벳 완성할 때 위의 이동 필요 없으므로
-    if name[-1]  == 'A' or name[1] == 'A':
-        if name[-1:-3] or name[1:3] == "AA":
-            answer -= 2
-        elif name[1:] == "AAA":
-            answer -= 3
-        else:
-            answer -= 1
+            locat -= left
+            answer += left
     return answer
 
 print(

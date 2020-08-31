3ff5c260755cebc42459cd614d9e5145d097f32b
Jaewon Lee <67958329+Jaewon0702@users.noreply.github.com>
2020-08-31 19:24:53 +0900

    다른 풀이를 하나로 합치기

--- programmers/난이도별/level01.평균_구하기/Jaewon0702.py
+++ programmers/난이도별/level01.평균_구하기/Jaewon0702.py
@@ -1,6 +1,16 @@
-def solution(arr):
+import numpy
+
+
+def solution1(arr):
+    return numpy.mean(arr)
+
+
+def solution2(arr):
     return sum(arr) / len(arr)
 
 
-print(solution([1, 2, 3, 4]) == 2.5)
-print(solution([5, 5]) == 5)
+print(solution1([1, 2, 3, 4]))
+print(solution1([5, 5]))
+
+print(solution2([1, 2, 3, 4]) == 2.5)
+print(solution2([5, 5]) == 5)

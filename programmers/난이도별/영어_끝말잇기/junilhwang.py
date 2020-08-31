0815ea4436548070749c311f95eb61d59b8aad19
junilhwang <tjsdlf4261@naver.com>
2020-08-31 21:59:51 +0900

    feat: 영어 끝말잇기 풀이 완료

--- programmers/난이도별/영어_끝말잇기/junilhwang.py
+++ programmers/난이도별/영어_끝말잇기/junilhwang.py
@@ -1,12 +1,12 @@
-import math;
+import math
 
 def solution(n, words):
     length = len(words)
     i = 0
     while i < length :
         if length == 1: break
-        list = words[0:i]
-        if i != 0 and words[i] in list:
+        temp = words[:i]
+        if i != 0 and words[i] in temp:
             break
         if i != length - 1 and words[i][-1] != words[i+1][0]:
             i += 1
@@ -15,7 +15,7 @@ def solution(n, words):
             return [0, 0]
         i += 1
 
-    return [i % n + 1, Math.ceil((i+1) / n)]
+    return [i % n + 1, math.ceil((i+1) / n)]
 
 print(
     solution(3, ['tank', 'kick', 'know', 'wheel', 'land', 'dream', 'mother', 'robot', 'tank']) == [3, 3],

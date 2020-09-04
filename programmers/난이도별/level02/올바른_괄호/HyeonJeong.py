80411cb1acd04be29a1ccd7994e44fb1ac0ba43c
H-yeonJeong <h-yeonjeong@naver.com>
2020-09-04 15:16:49 +0900

    level02.올바른_괄호

--- programmers/난이도별/level02/올바른_괄호/HyeonJeong.py
+++ programmers/난이도별/level02/올바른_괄호/HyeonJeong.py
@@ -1,15 +1,14 @@
-import math
-
 def solution(s):
-    length = len(s)
-    list(s)
-    if s.count('(') != s.count(')'):
-        return False
-    for j in range(math.ceil(length/2)):
-        for i in range(length-1):
-            if s[i:i+2] == '()':
-                s = s[:i] + s[i+2:]
-    return True if s == '' else False
+    stack = []
+    for i, n in enumerate(s):
+        if n == '(':
+            stack.append('(')
+        elif n == ')':
+            if stack != []:
+                stack.pop()
+            else:
+                return False
+    return True if stack == [] else False
 
 print(
     solution('()()') == True,

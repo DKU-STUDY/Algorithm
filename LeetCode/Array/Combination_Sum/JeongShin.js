5937a992c8de01c47a94814f71cc6c655f9f545f
JeongShin <sjeong1127@gmail.com>
2020-09-05 21:01:48 +0900

    LeetCode/Array/Combination_Sum_II, Permutations, Permutations_II 풀이

--- LeetCode/Array/Combination_Sum/JeongShin.js
+++ LeetCode/Array/Combination_Sum/JeongShin.js
@@ -2,6 +2,7 @@
  * @param {number[]} candidates
  * @param {number} target
  * @return {number[][]}
+ * 모든 조합 중복 X
  */
 const combinationSum = function (candidates, target) {
     const len = candidates.length;
@@ -14,7 +15,7 @@ const combinationSum = function (candidates, target) {
             const v = candidates[i];
             if (v < array[0] || 0)
                 continue;
-            combination([v,...array], sum + v, idx);
+            combination([v, ...array], sum + v, idx);
         }
     };
 
@@ -22,3 +23,24 @@ const combinationSum = function (candidates, target) {
     return answer;
 };
 
+/* LeetCode 에서 참고한 풀이 */
+
+const combinationSum_2 = (candidates, target) => {
+    const answer = [];
+    candidates.sort((a, b) => a - b);
+    backtrack(candidates, [], 0, target, answer);
+    return answer;
+};
+
+function backtrack(candidates, arr, idx, remain, answer, len = candidates.length) {
+    if (remain <= 0)
+        return remain === 0 ? answer.push(arr) : null;
+    for (let i = idx; i < len; i++) {
+        const curr = candidates[i];
+        arr.push(curr);
+        backtrack(candidates, [...arr], i, remain - curr, answer, len);
+        arr.pop();
+    }
+}
+
+combinationSum2([2, 3, 6, 7], 7);
--- LeetCode/Array/Combination_Sum_II/JeongShin.js
+++ LeetCode/Array/Combination_Sum_II/JeongShin.js
@@ -2,6 +2,7 @@
  * @param {number[]} candidates
  * @param {number} target
  * @return {number[][]}
+ * 모든 조합 중복 O
  */
 
 const combinationSum2 = function (candidates, target) {
@@ -27,11 +28,33 @@ const combinationSum2 = function (candidates, target) {
 
     return answer;
 };
+
+/* LeetCode 참고한 코드 */
+const combinationSum2_2 = (candidates, target) => {
+    const answer = [];
+    candidates.sort((a, b) => a - b);
+    backtrack(candidates, [], 0, target, answer);
+    return answer;
+};
+
+function backtrack(candidates, arr, idx, remain, answer, len = candidates.length) {
+    if (remain <= 0)
+        return remain === 0 ? answer.push(arr) : null;
+    for (let i = idx; i < len; i++) {
+        const curr = candidates[i];
+        arr.push(curr);
+        backtrack(candidates, [...arr], i + 1, remain - curr, answer, len);
+        arr.pop();
+        while (candidates[i + 1] === candidates[i]) i++;
+    }
+}
+
 const candidates = [10, 1, 2, 7, 6, 1, 5], target = 8;
-combinationSum2(candidates, target);
+combinationSum2_2(candidates, target);
 // [
 //   [1, 7],
 //   [1, 2, 5],
 //   [2, 6],
 //   [1, 1, 6]
 // ]
+

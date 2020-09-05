5ff2102333cb0370e4f6fd34a2bc9f827950ad58
s2uyeoii <pink99120@gmail.com>
2020-09-06 00:36:08 +0900

    Create s2uyeoii_c.cpp

---
+++ programmers/난이도별/level01.핸드폰_번호_가리기/s2uyeoii_c.cpp
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+#include<string.h>
+
+// 파라미터로 주어지는 문자열은 const로 주어집니다. 변경하려면 문자열을 복사해서 사용하세요.
+char* solution(const char* phone_number) {
+    // return 값은 malloc 등 동적 할당을 사용해주세요. 할당 길이는 상황에 맞게 변경해주세요.
+    char* answer = (char*)malloc(1); //char size 할당
+    strcpy(answer, phone_number);
+    
+    for(int i=0; i<strlen(phone_number)-4; i++)
+        answer[i] = '*';
+    
+    return answer;
+}

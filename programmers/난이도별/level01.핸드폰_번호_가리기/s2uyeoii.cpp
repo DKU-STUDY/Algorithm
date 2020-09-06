5a23d11bcb00d587f82916938ca20c2af4f8b765
s2uyeoii <pink99120@gmail.com>
2020-09-06 16:50:44 +0900

    Update programmers/난이도별/level01.핸드폰_번호_가리기/s2uyeoii.cpp
    Co-authored-by: JunilHwang <tjsdlf4261@naver.com>

--- programmers/난이도별/level01.핸드폰_번호_가리기/s2uyeoii.cpp
+++ programmers/난이도별/level01.핸드폰_번호_가리기/s2uyeoii.cpp
@@ -5,7 +5,7 @@ using namespace std;
 
 string solution(string phone_number) {
     
-    for(int i=0; i<phone_number.length()-4; i++)
+    for(int i = 0, len = phone_number.length();  i < len - 4; i++)
     {
         phone_number[i] = '*';
 }

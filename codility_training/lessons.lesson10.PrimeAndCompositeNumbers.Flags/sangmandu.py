249e202d47b6f1f77f061dc9d672429d482d5842
sangmandu <45033215+sangmandu@users.noreply.github.com>
2020-09-08 22:07:26 +0900

    lessons.lesson10.PrimeAndCompositeNumbers.Flags

---
+++ codility_training/lessons.lesson10.PrimeAndCompositeNumbers.Flags/sangmandu.py
@@ -0,0 +1,36 @@
+def solution(A):
+    # write your code in Python 3.6
+    pass
+    B = []
+    for i in range(1, len(A) - 1):
+        if A[i - 1] < A[i] and A[i] > A[i + 1]:
+            B.append(i)
+
+    size = len(B)
+    if size == 0:
+        return 0
+
+    C = []
+    for i in range(1, size):
+        C.append(B[i] - B[i - 1])
+
+    maxFlags = 1
+    for i in range(2, size + 1):
+        flags = 1
+        dist = 0
+        for j in C:
+            dist += j
+            if flags == i:
+                break
+            if dist < i:
+                continue
+            flags += 1
+            dist = 0
+        maxFlags = max(maxFlags, flags)
+        if flags != i and maxFlags != flags:
+            break
+    return maxFlags
+
+print(
+    solution([1,5,3,4,3,4,1,2,3,4,6,2] == 3)
+)

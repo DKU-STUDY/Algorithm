ffa435d235c3c00f9efb81927868de7b784084b0
s2uyeoii <pink99120@gmail.com>
2020-09-08 18:44:12 +0900

    Update programmers/난이도별/level01.하샤드_수/s2uyeoii.cpp
    Co-authored-by: JunilHwang <tjsdlf4261@naver.com>

--- programmers/난이도별/level01.하샤드_수/s2uyeoii.cpp
+++ programmers/난이도별/level01.하샤드_수/s2uyeoii.cpp
@@ -7,15 +7,11 @@ bool solution(int x) {
     int sum = 0;
     int x1 = x;
 
-    while(x>=1)
+    while(x>=1 || x1 != 0)
     {
-       sum += x1%10;
-        x1/= 10;
-
-        if(x1 == 0)
-            break;
-
-        }
+       sum += x1 % 10;
+        x1 /= 10;
+    }
 
     return x % sum == 0;
 

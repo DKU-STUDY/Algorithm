0e642821618027c1f0e55881311f6e3b6be3bc1a
khw970421 <khw970421@kakao.com>
2020-09-19 19:44:41 +0900

    9/19 피드백 수정

--- programmers/난이도별/level01.K번째수/khw970421.js
+++ programmers/난이도별/level01.K번째수/khw970421.js
@@ -2,7 +2,7 @@ function solution(array, commands) {
     let sort;
     let answer=[];
     const commands_length= commands.length;
-    for(let i=0;i<commands.length;i++)
+    for(let i=0;i<commands_length;i++)
     {
         sort=array.slice(commands[i][0]-1,commands[i][1]);
         sort.sort((A,B)=>(A-B))
--- programmers/난이도별/level01.완주하지_못한_선수/khw970421.js
+++ programmers/난이도별/level01.완주하지_못한_선수/khw970421.js
@@ -11,3 +11,18 @@ console.log(
     solution(['mislav', 'stanko', 'mislav', 'ana'], ['stanko', 'ana', 'mislav']) == 'mislav'
 )
 // 효율성 문제 발생
+
+
+function solution(participant, completion)
+{
+    const participant_length=participant.length;
+
+    participant.sort();
+    completion.sort();
+
+    for(let i=0;i<participant_length;i++)
+        if(participant[i]!=completion[i])       // 정렬후에 같은위치에 다른 값이라면 그값은 완주하지 못한 값
+            return participant[i];          // 완주하지 못한 참가자 return
+}
+
+// 효율성 문제 해결 ( 정렬 후 return 처리)

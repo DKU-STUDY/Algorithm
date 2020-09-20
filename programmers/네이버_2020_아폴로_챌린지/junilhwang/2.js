e4c0096fed9de2fb75fad89e57eb8830698bb96f
junilhwang <tjsdlf4261@naver.com>
2020-09-20 13:41:51 +0900

    feat: 아폴로 챌린지 2번 문제 풀이 중

--- programmers/네이버_2020_아폴로_챌린지/junilhwang/2.js
+++ programmers/네이버_2020_아폴로_챌린지/junilhwang/2.js
@@ -1,9 +1,21 @@
+
 function solution(h, w, n, board) {
+
+  let cnt = 0;
+  for (const row of board) {
+    let before = 0;
+    let next = row.indexOf('0');
+    while (next !== -1) {
+      if (next - before === 4) cnt += 1;
+      [before, next] = [next, row.indexOf('0', next + 1)];
+    }
+  }
+
   var answer = -1;
   return answer;
 }
 
 console.log(
   solution(7, 9, 4, ["111100000","000010011","111100011","111110011","111100011","111100010","111100000"]), 10,
-  solution(5, 5, 5, ["11111","11111","11111","11111","11111"]), 12,
+  // solution(5, 5, 5, ["11111","11111","11111","11111","11111"]), 12,
 )

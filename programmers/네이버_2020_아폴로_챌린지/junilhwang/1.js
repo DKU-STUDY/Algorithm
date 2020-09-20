a87b509d1b7a6052486d5e3b682a755876bb1b00
junilhwang <tjsdlf4261@naver.com>
2020-09-20 13:01:38 +0900

    feat: 네이버 아폴로 챌린지 첫 번째 문제 추가

---
+++ programmers/네이버_2020_아폴로_챌린지/junilhwang/1.js
@@ -0,0 +1,10 @@
+function solution(table) {
+  var answer = 0;
+  return answer;
+}
+
+console.log(
+  solution(["XOXO", "OXXO", "XXOX", "XOOO"]), 2,
+  solution(["OXXO", "XOXO", "XXOO"]), 3,
+  solution(["OXOXO", "OOOOO", "XOXOX"]), 1,
+)

820cf7674513725533d3690c5ff8328efc653f0a
junilhwang <tjsdlf4261@naver.com>
2020-09-20 13:17:31 +0900

    feat: 아폴로 챌린지 1번 풀이 완료

--- programmers/네이버_2020_아폴로_챌린지/junilhwang/1.js
+++ programmers/네이버_2020_아폴로_챌린지/junilhwang/1.js
@@ -1,6 +1,23 @@
 function solution(table) {
-  var answer = 0;
-  return answer;
+  const max = table[0].length;
+  const tableOf = table.map(v => [ ...v ].reduce((arr, acc, key) => {
+    if (acc === 'O') arr.push(key);
+    return arr;
+  }, []));
+  let min = Infinity;
+  const f = (set, stack) => {
+    if (stack.length > min) return;
+    if (set.size === max) {
+      min = Math.min(min, stack.length);
+      return;
+    }
+    tableOf.forEach((v, k) => {
+      if (stack.includes(k)) return;
+      f(new Set([ ...set, ...v]), [ ...stack, k ]);
+    })
+  }
+  f(new Set, []);
+  return min;
 }
 
 console.log(

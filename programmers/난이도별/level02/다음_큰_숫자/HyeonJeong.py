e9b39086447d136e10dbb1c2977e3524eb6768c9
H-yeonJeong <1212dlguswjd@daum.net>
2020-09-21 23:26:35 +0900

    level02.다음_큰_숫자

---
+++ programmers/난이도별/level02/다음_큰_숫자/HyeonJeong.py
@@ -0,0 +1,9 @@
+def solution(n):
+    binary_n = bin(n)
+    num_0 = binary_n.count('0')
+    num_1 = binary_n.count('1')
+    for i in range(1, 100001-n): #n은 1000000이하의 자연수 이므로
+        if bin(n + i).count('1') == num_1:
+            return int(n+i)
+
+print(solution(78)==83, solution(15)==23)

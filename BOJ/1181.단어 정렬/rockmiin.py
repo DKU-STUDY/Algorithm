4d5cf8bf5cda299600cbd489f7d792d292c69d9f
unknown <csm971024@gmail.com>
2020-09-21 19:13:42 +0900

    1181.단어 정렬

--- BOJ/1181.단어 정렬/rockmiin.py
+++ BOJ/1181.단어 정렬/rockmiin.py
@@ -4,12 +4,10 @@ for i in range(n):
     arr.append(input())
 
 arr=set(arr)
-answer=[[] for _ in range(len(arr)+1)]
+answer=[]
 for i in arr:
-    answer[len(i)].append(i) # 인덱스를 단어의 길이인 배열 answer에 길이마다 저장
-# print(answer)
+    answer.append([len(i), i]) # 인덱스를 단어의 길이인 배열 answer에 길이마다 저장
+answer.sort()
+# sort()는 앞 조건이 같으면 뒤 조건으로 정렬한다
 for i in answer:
-    if i: # answer값 중 []처럼 빈 곳이 있어서 값이 있을때만 정렬
-        i=sorted(i)
-        for j in i:
-            print(j)
+    print(i[1])

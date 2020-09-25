59fde4d82a3f5b4ef9ff7bbc35809847dcf884d7
H-yeonJeong <1212dlguswjd@daum.net>
2020-09-25 18:01:01 +0900

    level02.JadenCase_문자열_만들기2

--- programmers/난이도별/level02/JadenCase_문자열_만들기/HyeonJeong.py
+++ programmers/난이도별/level02/JadenCase_문자열_만들기/HyeonJeong.py
@@ -1,16 +1,14 @@
 def solution(s):
     tmp = 0
     answer = ""
-    s.lower()
-    for i, c in enumerate(s):
+    list = []
+    for i, c in enumerate(s.lower()):
         if c == ' ':
             tmp = i+1
-        if tmp == i and c.isalpha() == 1:
-            answer += c.upper()
-        elif  c.isalpha() == 1:
-            answer += c.lower()
-        else:
-            answer += c
+        answer += c.upper() if tmp == i and c.isalpha() == 1 else c
     return answer
 
-print(solution("3people unFollowed me") == "3people Unfollowed Me", solution("for the last week") == "For The Last Week")
+print(
+    solution("3people unFollowed me") == "3people Unfollowed Me",
+    solution("for the last week") == "For The Last Week"
+)

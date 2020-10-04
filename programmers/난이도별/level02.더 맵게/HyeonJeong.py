303806d20b7eddf8458af44c0c231edd651ea309
H-yeonJeong <h-yeonjeong@naver.com>
2020-10-05 00:03:28 +0900

    level02.더_맵게2

--- programmers/난이도별/level02.더 맵게/HyeonJeong.py
+++ programmers/난이도별/level02.더 맵게/HyeonJeong.py
@@ -6,9 +6,11 @@ def solution(scoville, K):
             return answer
         if len(s) < 2:
             return -1
+        # s가 가장 작은 s[0]가 K보다 작고 하나일 경우는 실패
         tmp = s.pop(0) + s.pop(0)*2
+        # 가장 작은 것과 두 번째로 작은 것이 반환된 후 제거됨.
         answer += 1
-        cnt = 0
+        cnt = 0 # tmp가 s의 모든 요소 보다 큰 경우는 마지막 요소가 될 수 있게 하기 위해서
         for i, c in enumerate(s):
             if tmp < c:
                 s.insert(i, tmp)

2477c80dbfb6ba32c7fda3bb517f3c9d96288929
H-yeonJeong <1212dlguswjd@daum.net>
2020-10-06 21:37:28 +0900

    level02.더_맵게(정답 분석)

--- programmers/난이도별/level02.더 맵게/HyeonJeong.py
+++ programmers/난이도별/level02.더 맵게/HyeonJeong.py
@@ -1,22 +1,17 @@
-def solution(scoville, K):
-    answer = 0
-    s = sorted(scoville)
-    while 1:
-        if s[0] >= K:
-            return answer
-        if len(s) < 2:
-            return -1
-        # s가 가장 작은 s[0]가 K보다 작고 하나일 경우는 실패
-        tmp = s.pop(0) + s.pop(0)*2
-        # 가장 작은 것과 두 번째로 작은 것이 반환된 후 제거됨.
-        answer += 1
-        cnt = 0 # tmp가 s의 모든 요소 보다 큰 경우는 마지막 요소가 될 수 있게 하기 위해서
-        for i, c in enumerate(s):
-            if tmp < c:
-                s.insert(i, tmp)
-                cnt = 1
-                break
-        if cnt == 0:
-            s.append(tmp)
+# 실패 -> 정답을 분석한 것
+
+import heapq #이진트리 기반 최소 힙(원소가 항상 정렬된 상태에서 추가, 삭제) 자료구조 제공
+
+def solution(s, K):
+    a = 0 # 섞은 횟수가 추가될 변수
+    heapq.heapify(s)
+    if s[0] >= K : return 0 # 가장 작은 원소가 K이상이면 반환
+    while len(s) > 1 :
+        heapq.heappush(s, heapq.heappop(s) + heapq.heappop(s) * 2)
+        # heappush()는 두 번째 인자를 첫 번째 인자인 리스트에 추가, O(logN)의 시간복잡도를 가짐
+        # heappop()은 대상 리스트에서 가장 작은 원소 삭제후 값 리턴
+        a += 1 # 섞은 횟수 추가
+        if s[0] >= K : return a # 가장 작은 원소가 K이상이면 섞은 횟수 반환
+    return -1 # 다 섞었는데도 K미만인 경우
 
 print(solution([1, 2, 3, 9, 10, 12], 7) == 2)

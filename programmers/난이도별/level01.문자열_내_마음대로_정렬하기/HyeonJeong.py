fb0e046ffaa5fef1b885042b8e61a3af4a6095b7
H-yeonJeong <h-yeonjeong@naver.com>
2020-10-08 23:57:26 +0900

    level01.문자열_내_마음대로_정렬하기

--- programmers/난이도별/level01.문자열_내_마음대로_정렬하기/HyeonJeong.py
+++ programmers/난이도별/level01.문자열_내_마음대로_정렬하기/HyeonJeong.py
@@ -1,11 +1,3 @@
-def solution(strings, n):
-    return sorted(sorted(strings) , key=lambda s: s[n]) # 내부 sorted는 인덱스 n의 값이 같은 것은 사전 순 배열이 필요
-
-print(
-    solution(["sun", "bed", "car"],1) == ['car', 'bed', 'sun'],
-    solution(["abce", "abcd", "cdx"], 2) == ["abcd", "abce", "cdx"]
-)
-
 # 아래의 코드는 왜 실패 되나요?
 '''
 def solution(strings, n):
@@ -22,3 +14,12 @@ def solution(strings, n):
         strings.remove(c)
     return answer
 '''
+
+def solution(strings, n):
+    return sorted(sorted(strings) , key=lambda s: s[n]) # 내부 sorted는 인덱스 n의 값이 같은 것은 사전 순 배열이 필요
+
+print(
+    solution(["sun", "bed", "car"],1) == ['car', 'bed', 'sun'],
+    solution(["abce", "abcd", "cdx"], 2) == ["abcd", "abce", "cdx"]
+)
+

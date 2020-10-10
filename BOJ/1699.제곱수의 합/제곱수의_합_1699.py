28aab98d37555b5e887b3d2f8e505ec802a14b8f
최석민 <csm971024@gmail.com>
2020-10-10 23:47:32 +0900

    #DP

---
+++ BOJ/1699.제곱수의 합/제곱수의_합_1699.py
@@ -0,0 +1,16 @@
+n=int(input())
+dp=[0]*(n+1)
+s=[]
+dp[1]=1
+for i in range(1, 400):
+    if i**2<100000:
+        s.append(i)
+    else: break;
+for i in range(1, n+1):
+    q=[]
+    for j in s:
+        if i>=j**2:
+            q.append(dp[i-j**2])
+        else: break;
+    dp[i]=min(q)+1
+print(dp[n])

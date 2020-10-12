da5bc316ad0de7e76bfeda9ca2d05468014e41ec
JeongShin <sjeong1127@gmail.com>
2020-10-12 22:13:35 +0900

    코드리뷰 수정;

--- LeetCode/Binary_Search/Minimum_Size_Subarray_Sum/JeongShin.js
+++ LeetCode/Binary_Search/Minimum_Size_Subarray_Sum/JeongShin.js
@@ -4,8 +4,8 @@
  * @return {number}
  */
 const minSubArrayLen = function (s, nums) {
-    let sum = 0, start = 0, answer;
-    return (answer = nums.reduce((minLen, num, end) => {
+    let sum = 0, start = 0;
+    const answer = nums.reduce((minLen, num, end) => {
         sum += num;
         // 만약 sum 이 기준치를 넘게 되면 최소 길이를 구하기 위해 minLen 을 구합니다.
         // 앞에서 숫자를 하나씩 빼면서 최소 길이를 유지 합니다.
@@ -14,7 +14,8 @@ const minSubArrayLen = function (s, nums) {
             sum -= nums[start++];
         }
         return minLen;
-    }, Infinity)) === Infinity ? 0 : answer;
+    }, Infinity);
+    return answer === Infinity ? 0 : answer;
 };
 
 console.log(minSubArrayLen(7, [2, 3, 1, 2, 4, 3]));

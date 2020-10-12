61ee7b50a6d540eec549ae455914bc5e9a8584fe
dkdlelk99 <49552766+dkdlelk99@users.noreply.github.com>
2020-10-12 18:37:08 +0900

    pop(0)을 pop()로 사용할수 있게 바꾸어봄
    이전
    테스트 1 〉	통과 (25.60ms, 10.2MB)
    테스트 2 〉	통과 (2955.72ms, 10.4MB)
    테스트 3 〉	통과 (0.02ms, 10.3MB)
    테스트 4 〉	통과 (668.11ms, 10.2MB)
    테스트 5 〉	실패 (시간 초과)
    테스트 6 〉	통과 (3289.85ms, 10.2MB)
    테스트 7 〉	통과 (12.43ms, 10.3MB)
    테스트 8 〉	통과 (0.67ms, 10.2MB)
    테스트 9 〉	통과 (10.84ms, 10.2MB)
    테스트 10 〉	통과 (0.84ms, 10.3MB)
    테스트 11 〉	통과 (0.01ms, 10.3MB)
    테스트 12 〉	통과 (0.73ms, 10.3MB)
    테스트 13 〉	통과 (4.25ms, 10.3MB)
    테스트 14 〉	통과 (0.01ms, 10.3MB)
    
    이후
    테스트 1 〉	통과 (24.45ms, 10.2MB)
    테스트 2 〉	통과 (2965.66ms, 10.2MB)
    테스트 3 〉	통과 (0.02ms, 10.2MB)
    테스트 4 〉	통과 (684.91ms, 10.2MB)
    테스트 5 〉	실패 (시간 초과)
    테스트 6 〉	통과 (3297.31ms, 10.4MB)
    테스트 7 〉	통과 (12.60ms, 10.2MB)
    테스트 8 〉	통과 (0.63ms, 10.4MB)
    테스트 9 〉	통과 (10.83ms, 10.2MB)
    테스트 10 〉	통과 (0.79ms, 10.2MB)
    테스트 11 〉	통과 (0.02ms, 10.2MB)
    테스트 12 〉	통과 (0.75ms, 10.3MB)
    테스트 13 〉	통과 (4.75ms, 10.2MB)
    테스트 14 〉	통과 (0.01ms, 10.3MB)
    
    프로그래머스에서 유의미한 차이를 내지는 않는듯
    알고리즘 자체의 효율성을 더 중요하게 보는것같다.

--- programmers/난이도별/level02.다리를_지나는_트럭/dkdlelk99.py
+++ programmers/난이도별/level02.다리를_지나는_트럭/dkdlelk99.py
@@ -1,10 +1,11 @@
 def solution(bridge_length, weight, truck_weights):
     sec = 0
     on_the_bridge = [0] * bridge_length #FIFO
+    truck_weights = truck_weights[::-1]
     while 1:
         sec += 1
         # 대기 트럭 인덱스 에러 때문에 비어있으면 값 0이라고 해버림
-        next_truck = truck_weights[0] if truck_weights != [] else 0
+        next_truck = truck_weights[-1] if truck_weights != [] else 0
 
         if sum(on_the_bridge) + next_truck <= weight:
         #다음 트럭 올라올수 있을때
@@ -13,13 +14,13 @@ def solution(bridge_length, weight, truck_weights):
                 break
             else:
                 on_the_bridge.pop(0)
-                on_the_bridge.append(truck_weights.pop(0))
+                on_the_bridge.append(truck_weights.pop())
         #다음 트럭 못올라올때
         else:
             #맨앞 트럭 빠지면 올라올수 있을때
             if sum(on_the_bridge) + next_truck - on_the_bridge[0] <= weight:
                 on_the_bridge.pop(0)
-                on_the_bridge.append(truck_weights.pop(0))
+                on_the_bridge.append(truck_weights.pop())
             #빠져도 못올라올때
             else:
                 on_the_bridge.append(0)

3d9f24ef52ce6b036b2cdaf07994155ed0449961
dkdlelk99 <49552766+dkdlelk99@users.noreply.github.com>
2020-10-12 17:53:50 +0900

    Update dkdlelk99.py
    테스트 1 〉	통과 (25.60ms, 10.2MB)
    테스트 2 〉	통과 (2955.72ms, 10.4MB)
    테스트 3 〉	통과 (0.02ms, 10.3MB)
    테스트 4 〉	통과 (668.11ms, 10.2MB)
    테스트 5 〉	실패 (시간 초과)
    테스트 6 〉	통과 (3289.85ms, 10.2MB)
    테스트 7 〉	통과 (12.43ms, 10.3MB)
    테스트 8 〉	통과 (0.67ms, 10.2MB)
    테스트 9 〉	통과 (10.84ms, 10.2MB)
    테스트 10 〉	통과 (0.84ms, 10.3MB)
    테스트 11 〉	통과 (0.01ms, 10.3MB)
    테스트 12 〉	통과 (0.73ms, 10.3MB)
    테스트 13 〉	통과 (4.25ms, 10.3MB)
    테스트 14 〉	통과 (0.01ms, 10.3MB)

--- programmers/난이도별/level02.다리를_지나는_트럭/dkdlelk99.py
+++ programmers/난이도별/level02.다리를_지나는_트럭/dkdlelk99.py
@@ -1,25 +1,31 @@
 def solution(bridge_length, weight, truck_weights):
     sec = 0
-    on_the_bridge = [] #FIFO
+    on_the_bridge = [0] * bridge_length #FIFO
     while 1:
         sec += 1
-        if truck_weights != []:
-            next_truck = truck_weights[0]
-        else:
-            next_truck = 0
+        # 대기 트럭 인덱스 에러 때문에 비어있으면 값 0이라고 해버림
+        next_truck = truck_weights[0] if truck_weights != [] else 0
 
         if sum(on_the_bridge) + next_truck <= weight:
-            if truck_weights == []:
-                sec += bridge_length - 2
-                on_the_bridge.clear()
+        #다음 트럭 올라올수 있을때
+            if truck_weights == []:#마지막일때 실행
+                sec += bridge_length - 1
+                break
             else:
+                on_the_bridge.pop(0)
                 on_the_bridge.append(truck_weights.pop(0))
+        #다음 트럭 못올라올때
         else:
-            sec += bridge_length - 2
-            on_the_bridge.clear()
-        if truck_weights == [] and on_the_bridge == []:
-            break
-    return sec + 1
-print(solution(2,10,[7,4,5,6]))
-print(solution(100,100,[10]))
-print(solution(100,100,[10]*10))
+            #맨앞 트럭 빠지면 올라올수 있을때
+            if sum(on_the_bridge) + next_truck - on_the_bridge[0] <= weight:
+                on_the_bridge.pop(0)
+                on_the_bridge.append(truck_weights.pop(0))
+            #빠져도 못올라올때
+            else:
+                on_the_bridge.append(0)
+                on_the_bridge.pop(0)
+    return sec
+print(solution(2,10,[7,4,5,6]), solution(2,10,[7,4,5,6]) == 8)
+print(solution(100,100,[10]), solution(100,100,[10]) == 101)
+print(solution(100,100,[10]*10), solution(100,100,[10]*10) == 110)
+print(solution(5,6,[1,2,3,1,2]), solution(5,6,[1,2,3,1,2]) == 12) #my test case

94474679bec9013e82267f6bd11fe151a53f6c38
H-yeonJeong <h-yeonjeong@naver.com>
2020-10-13 23:23:59 +0900

    [LeetCode] Array TwoSum

--- LeetCode/Array/TwoSum/HyeonJeong.py
+++ LeetCode/Array/TwoSum/HyeonJeong.py
@@ -19,3 +19,7 @@ class Solution:
             if i == length:
                 j += 1
                 i = j+1
+
+# twoSum([2,7,11,15],9) == [0,1]
+# twoSum([3,2,4],6) == [1,2]
+# twoSum([3,3],6) == [0,1]

93bd1e2ca85de2c015912a5e33353b97f083e913
sjjyy <sjhy013@gmail.com>
2020-10-15 17:30:20 +0900

    Update sjjyy.java

--- programmers/난이도별/level02.위장/sjjyy.java
+++ programmers/난이도별/level02.위장/sjjyy.java
@@ -1,7 +1,15 @@
 import java.util.Arrays;
 import java.util.HashMap;
 
+import static java.util.stream.Collectors.*;
+
 public class sjjyy {
+    /*
+        * 의상의 종류와 이름이 담긴 2차원 배열이 주어질 때 서로 다른 옷 조합의 수를 리턴
+        String[][] clothes = {{의상의 이름, 의상의 종류} ... }
+        * 원소 개수는 1개 이상 30개 이하이며 모두 1 이상 20 이하 길이의 문자열로 이루어져 있음
+        * 하루에 최소 한 개 이상의 옷을 입어야 함
+    */
     public static int solution(String[][] clothes) {
         int answer = 1; // multiple
         HashMap<String, Integer> hashMap = new HashMap<>();
@@ -17,6 +25,14 @@ public class sjjyy {
         return answer - 1;
         // 전부 입지 않는 경우 제외
     }
+
+    public static int solution2(String[][] clothes) {
+        return Arrays.stream(clothes)
+                .collect(groupingBy(p -> p[1], mapping(p -> p[0], counting())))
+                .values()
+                .stream()
+                .collect(reducing(1L, (x, y) -> x * (y + 1))).intValue() - 1;
+    } // 다른 사람 풀이
 
     public static void main(String[] args) {
         String[][] clothes1 = {{"yellow_hat", "headgear"}, {"blue_sunglasses", "eyewear"}, {"green_turban", "headgear"}};

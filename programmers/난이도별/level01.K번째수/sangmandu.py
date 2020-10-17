6b9a1d7ffffdd68eaa65cd904ac2ef8bcb0c50be
sangmandu <45033215+sangmandu@users.noreply.github.com>
2020-10-17 21:26:43 +0900

    Update programmers/난이도별/level01.K번째수/sangmandu.py
    Co-authored-by: JunilHwang <tjsdlf4261@naver.com>

--- programmers/난이도별/level01.K번째수/sangmandu.py
+++ programmers/난이도별/level01.K번째수/sangmandu.py
@@ -4,4 +4,4 @@
 #문제 그대로 풀이
 
 def solution(array, commands):
-    return [sorted(array[i[0]-1:i[1]])[i[2]-1] for i in commands]
+    return [ sorted(array[ a - 1 : b ])[ c - 1 ] for (a, b, c) in commands ]

6190df4a3ef46a5bcc66b740424c0e403e5b3d5b
H-yeonJeong <1212dlguswjd@daum.net>
2020-10-20 12:24:52 +0900

    [LeetCode] Add Digits2

--- LeetCode/Math/Add_Digits/HyeonJeong.py
+++ LeetCode/Math/Add_Digits/HyeonJeong.py
@@ -2,10 +2,7 @@ class Solution:
     def addDigits(self, num: int) -> int:
         s = str(num)
         while 1:
-            m = 0
             if len(s) == 1:
                 return int(s) #한 자리 수이면 문자를 숫자로 리턴
-            for i in s:
-                m += int(i)
-
-            s = str(m) #숫자를 문자로 만들어서 while문에서 다시 길이 검사 할 수 있게 함
+            s = str(sum([int(i) for i in s]))
+           #숫자를 문자로 만들어서 while문에서 다시 길이 검사 할 수 있게 함

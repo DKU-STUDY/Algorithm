e8bea7badaa46b06fb0017529bc30157553914cc
sangmandu <45033215+sangmandu@users.noreply.github.com>
2020-10-20 20:54:24 +0900

    20201020

---
+++ programmers/난이도별/level01.나누어_떨어지는_숫자_배열/sangmandu.py
@@ -0,0 +1,16 @@
+'''
+https://programmers.co.kr/learn/courses/30/lessons/12910
+나누어 떨어지는 숫자 배열 : 나머지가 0 인 수 찾기
+divmod 사용
+'''
+
+def solution(arr, divisor):
+    answer = [i for i in sorted(arr) if divmod(i, divisor)[1] == 0]
+    return answer if len(answer) != 0 else [-1]
+
+'''
+배울 점이 있는 코드
+def solution(arr, divisor): return sorted([n for n in arr if n%divisor == 0]) or [-1]
+
+len == 0 을 비교할 때는 if else 대신 False or True 를 이용할 수 있음
+'''

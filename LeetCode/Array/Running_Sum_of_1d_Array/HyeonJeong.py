2619090d7d4eae59108a99f862fe411b87a29b02
H-yeonJeong <1212dlguswjd@daum.net>
2020-10-26 18:04:12 +0900

    [LeetCode] Running Sum of 1d Array2

--- LeetCode/Array/Running_Sum_of_1d_Array/HyeonJeong.py
+++ LeetCode/Array/Running_Sum_of_1d_Array/HyeonJeong.py
@@ -1,9 +1,4 @@
 class Solution:
     def runningSum(self, nums: List[int]) -> List[int]:
-        sum = []
-        for i in range(1, len(nums)+1): # 더해져야하는 마지막 번째 수가 1 ~ len(nums)+1
-            n = 0
-            for j in range(0, i):
-                n += nums[j]
-            sum.append(n)
-        return sum
+        return [sum([nums[j] for j in range(0, i)]) for i in range(1, len(nums)+1)]
+        # 더해져야하는 마지막 번째 수가 1 ~ len(nums)+1

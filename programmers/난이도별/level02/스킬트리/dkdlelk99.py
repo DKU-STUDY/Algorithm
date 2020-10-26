fdc30ca840d1ee0ced71ecc79bdb940c960f476c
dkdlelk99 <49552766+dkdlelk99@users.noreply.github.com>
2020-10-26 19:31:56 +0900

    2차 시도
    테스트 1 〉	실패 (런타임 에러)
    테스트 2 〉	통과 (0.01ms, 10.2MB)
    테스트 3 〉	통과 (0.03ms, 10.3MB)
    테스트 4 〉	통과 (0.02ms, 10.2MB)
    테스트 5 〉	실패 (런타임 에러)
    테스트 6 〉	실패 (런타임 에러)
    테스트 7 〉	실패 (런타임 에러)
    테스트 8 〉	실패 (런타임 에러)
    테스트 9 〉	실패 (런타임 에러)
    테스트 10 〉	실패 (런타임 에러)
    테스트 11 〉	실패 (런타임 에러)
    테스트 12 〉	통과 (0.14ms, 9.99MB)
    테스트 13 〉	실패 (런타임 에러)
    테스트 14 〉	실패 (런타임 에러)
    
    알고리즘은 괜찮다 생각했는데 ..ㅠ

--- programmers/난이도별/level02/스킬트리/dkdlelk99.py
+++ programmers/난이도별/level02/스킬트리/dkdlelk99.py
@@ -1,13 +1,23 @@
 def solution(skill, skill_trees):
     answer = 0
+    len_skill = len(skill)
     for tree in skill_trees:
-        for i in range(len(skill) - 1):
-            if tree.find(skill[i]) > tree.find(skill[i+1]):
-                break
+        order = []
+        for i in range(len_skill):
+            order.append(tree.find(skill[i])) #앞에꺼보다 크면 들어가게 변경 아래 싹 필요 없음
+
+        for i in range(len_skill):
+            if order[-1] == -1:
+                order.pop()
             else:
-                if i == len(skill) - 2:
-                    answer += 1
-                continue
+                break
+
+        if order[0] == -1:
+            if sum(order) == -len(order): #선후수 스킬과 관계 없는 스킬만 찍을 경우
+                answer += 1
+        else:
+            if order == sorted(order):
+                answer += 1
     return answer
     
   print(solution("abc", ["adbic", "bcida","aicls"]), solution("abc", ["adbic", "bcida","aicls"]) == 1)

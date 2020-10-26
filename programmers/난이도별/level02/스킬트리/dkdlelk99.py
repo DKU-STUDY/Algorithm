912eb7fb6ea5fbb87cbef0b774787c420dfef8af
dkdlelk99 <49552766+dkdlelk99@users.noreply.github.com>
2020-10-26 20:17:13 +0900

    성공
    성공하긴 했으나 코드가 좀 더럽다.
    
    line 21 ~ 29를 손봐야 할 것 같다.

--- programmers/난이도별/level02/스킬트리/dkdlelk99.py
+++ programmers/난이도별/level02/스킬트리/dkdlelk99.py
@@ -1,23 +1,38 @@
+# 출처 https://programmers.co.kr/learn/courses/30/lessons/49993
+# skill 선행 스킬 순서 문자열, (1이상 26이하)
+# skill_trees 검사해봐야할 스킬트리 문자열(2이상 26이하)들의 리스트(1이상 20이하)
+# 선후수를 만족하는 스킬트리의 갯수 return
+
+
 def solution(skill, skill_trees):
     answer = 0
     len_skill = len(skill)
     for tree in skill_trees:
         order = []
-        for i in range(len_skill):
-            order.append(tree.find(skill[i])) #앞에꺼보다 크면 들어가게 변경 아래 싹 필요 없음
+        for i in range(len_skill):# order 리스트에 skill의 index를 찍어줌
+            order.append(tree.find(skill[i]))
 
-        for i in range(len_skill):
+        for i in range(len_skill):# ex) [3, 5, 1, -1, -1] => [3, 5, 1]로 만들어줌
             if order[-1] == -1:
                 order.pop()
             else:
                 break
-        if order != []: # 런타임 에러 해결
-            if order[0] == -1:
-                if sum(order) == -len(order): #선후수 스킬과 관계 없는 스킬만 찍을 경우
+
+        if order != []:
+            if order[0] == -1:               # 첫번째 스킬 안찍으면 뒷 스킬은 하나도 찍으면 안됨
+                if sum(order) == -len(order):# 의도 : order의 모든 요소가 -1 일때
                     answer += 1
             else:
-                if order == sorted(order):
+                if order == sorted(order):   # 모두 -1이 아니라면 순서대로 찍혀있어야됨
                     answer += 1
+        else:                                # skill에 있는 스킬 하나도 안찍었을때
+            answer += 1                      # 윗부분(line23)이랑 겹치는데 이상함 둘중에 하나라도 빠지면 안됨
+
     return answer
     
-  print(solution("abc", ["adbic", "bcida","aicls"]), solution("abc", ["adbic", "bcida","aicls"]) == 1)
+print(solution("CBD",["BACDE", "CBADF", "AECB", "BDA"]), \
+      solution("CBD",["BACDE", "CBADF", "AECB", "BDA"]) == 2)
+
+print(solution("abc", ["adbic", "ajrgp", "bcida","aicls", "bdgs", "abc","qwer"]), \
+      solution("abc", ["adbic", "ajrgp", "bcida","aicls", "bdgs", "abc","qwer"]) == 4)
+# my test case

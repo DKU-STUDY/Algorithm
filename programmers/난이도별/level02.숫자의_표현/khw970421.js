ad83483a88a493396977a290d26f76e083eb2303
khw970421 <khw970421@kakao.com>
2020-10-27 23:19:17 +0900

    10/27 1차 비밀지도 숫자의_표현

---
+++ programmers/난이도별/level02.숫자의_표현/khw970421.js
@@ -0,0 +1,23 @@
+function solution(n) {
+    let answer=0;
+    let count=0;
+    for(let i=1;i<=n;i++)
+    {
+        answer=0;
+        for(let j=i;j<=n;j++)
+        {
+            answer+=j;      // i부터 하나씩 계속 더하기
+            if(answer==n){      //값이 같아지면 count 증가와 break
+                count++;
+                break;
+            }
+            else if(answer>n){  //값이 더 커지면 필요없으므로 진행 안하게 break
+                break;
+            }
+        }
+    }
+    return count;       // 연속으로 더한 값들의 count 반환
+}
+
+//출처 : https://programmers.co.kr/learn/courses/30/lessons/12924
+// 이게 가장 효율도 문제없이 한것 같습니다....

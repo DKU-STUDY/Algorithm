5faea0eeedff39006563fc10af8e665f1bc1e759
sAp00n <won7963kr@gmail.com>
2020-10-28 11:40:19 +0900

    Add some discription at BOJ.solvedac.class03.11726.2xn 타일링

--- BOJ/solved.ac_class/Class03/11726.2xn 타일링/sAp00n.py
+++ BOJ/solved.ac_class/Class03/11726.2xn 타일링/sAp00n.py
@@ -13,13 +13,15 @@
 출력
     첫째 줄에 2×n 크기의 직사각형을 채우는 방법의 수를 10,007로 나눈 나머지를 출력한다.
 """
+# 제대로 코드 짯는데 뎁스 터져버리는 경험은 처음이였다.
+# 다행이 이전에 최대 깊이 수정하는 방법을 알아둬서 그걸로 풀이 완료.
+# 재귀적으로 구현하면 생각이 편해져서 좋긴 한데, 다른 방법으로 풀이하는것도 연습해야할꺼같다.
+
 import sys
-#from sys import stdin
 sys.setrecursionlimit(2000)
 
 def sol():
     n = int(sys.stdin.readline())
-    #n = int(stdin.readline())
     global memo
     memo = {0: 0, 1: 1, 2: 2}
     print(cal(n) % 10007)

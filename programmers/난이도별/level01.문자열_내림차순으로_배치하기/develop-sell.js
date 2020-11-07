1903d7fac8b3ee459cfa48b2665a76259f120bf4
insomenia-seil <insomenia-seil@gmail.com>
2020-11-07 12:24:32 +0900

    desc string solution

--- programmers/난이도별/level01.문자열_내림차순으로_배치하기/develop-sell.js
+++ programmers/난이도별/level01.문자열_내림차순으로_배치하기/develop-sell.js
@@ -2,13 +2,15 @@ function solution(s) {
   var answer = '';
   var ans = [];
   for(let i=0; i < s.length; i++ ){
-      ans.push(s.charAt(i))
+      ans.push(s.charAt(i));
   }
-  ans.sort((a,b) => { return(a-b);})
+  ans.sort((a, b) => a < b ? 1 : -1)
   
-  for(let j =0; j<ans.length; j++){
-      answer+= ans(j);
-  }
+  answer = ans.join('');
   
   return answer;
 }
+
+// sort하는 부분에서 헤매다가 junilhwang님 코드를 보고 풀었습니다
+// join하고도 seperater부분에 ''을 넣어줘야 ','가 안 들어갑니다 (,가 default인가..??)
+

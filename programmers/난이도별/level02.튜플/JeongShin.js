73cc1bf20eab1c3ef208cb3f1b238bc12447ec72
JeongShin <sjeong1127@gmail.com>
2020-11-10 22:02:55 +0900

    [level2] 튜플 풀이

---
+++ programmers/난이도별/level02.튜플/JeongShin.js
@@ -0,0 +1,8 @@
+function solution(s) {
+    const map = s.replace(/([{}.,])/g, " ").match(/\S+/g).reduce((map, curr) => {
+        curr *= 1;
+        map.set(curr, (map.get(curr) || 0) + 1);
+        return map
+    }, new Map());
+    return [...map].sort((a,b)=> b[1]-a[1]).map(v=> v[0]);
+}

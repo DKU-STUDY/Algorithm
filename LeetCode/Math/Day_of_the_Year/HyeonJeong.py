cfd6654d46d3915adaca58eacaf45883fd5879e3
H-yeonJeong <1212dlguswjd@daum.net>
2020-12-04 13:08:27 +0900

    [LeetCode] Day of the Year2

--- LeetCode/Math/Day_of_the_Year/HyeonJeong.py
+++ LeetCode/Math/Day_of_the_Year/HyeonJeong.py
@@ -1,9 +1,10 @@
 class Solution:
     def dayOfYear(self, date: str) -> int:
         mlist = [31,28,31,30,31,30,31,31,30,31,30,31]
-        year = int(date[0:4])
-        month = int(date[5:7])
-        day = int(date[8:10])
+        year, month, day = map(int, dat.split('-')
+        # year = int(date[0:4])
+        # month = int(date[5:7])
+        # day = int(date[8:10])
         if (year % 100 != 0 and year % 4 == 0) or year % 400 == 0:
             mlist[1] = 29
         answer = 0

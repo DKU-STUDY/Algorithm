a9d2db105412a36477662a4d8c2c173b2cd44308
H-yeonJeong <1212dlguswjd@daum.net>
2020-12-28 23:40:43 +0900

    [LeetCode] Search Insert Position & Plus One

--- LeetCode/Array/Plus_One/HyeonJeong.py
+++ LeetCode/Array/Plus_One/HyeonJeong.py
@@ -4,7 +4,7 @@ class Solution:
         i = len(digits)-1
         while i >= 0:
             if digits[i] == 10:
-                if i == 0: # 가장 앞의 인덱스가 10이 되는 경우 숫자 분리
+                if i == 0: # 가장 앞의 인덱스가 10이 되는 경우 한자리가 되게 숫자(1과 0) 분리
                     digits.insert(0, 1)
                     digits[i+1] = 0
                 else:

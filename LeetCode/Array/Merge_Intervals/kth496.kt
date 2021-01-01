b805cf75ec6154c03ddca39281ca58d5d15c2999
kth496 <taehongkim496@gmail.com>
2021-01-01 21:34:14 +0900

    Nullable로 개선

--- LeetCode/Array/Merge_Intervals/kth496.kt
+++ LeetCode/Array/Merge_Intervals/kth496.kt
@@ -1,15 +1,17 @@
 class Solution {
-    fun merge(intervals: Array<IntArray>): Array<IntArray> {
-        val answer: MutableList<IntArray> = ArrayList();
+    fun merge(intervals: Array<IntArray>): Array<IntArray?> {
+        val answer: MutableList<IntArray?> = ArrayList();
         intervals.sortWith(compareBy { it[0] })
 
-        var temp: IntArray = intervals[0]
+        var temp: IntArray? = null
         for (interval in intervals) {
-            if (temp[1] >= interval[0])
-                temp[1] = temp[1].coerceAtLeast(interval[1])
-            else {
-                answer.add(temp)
-                temp = interval
+            when {
+                temp == null -> temp = interval
+                temp[1] >= interval[0] -> temp[1] = max(temp[1], interval[1])
+                else -> {
+                    answer.add(temp)
+                    temp = interval
+                }
             }
         }
         answer.add(temp)

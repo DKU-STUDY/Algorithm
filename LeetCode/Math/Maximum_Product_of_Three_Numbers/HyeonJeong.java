a208b7bedbf553374a926f43ba6d95211391666a
H-yeonJeong <1212dlguswjd@daum.net>
2021-01-02 22:22:44 +0900

    [LeetCode] Maximum Product of Three Numbers

--- LeetCode/Math/Maximum_Product_of_Three_Numbers/HyeonJeong.java
+++ LeetCode/Math/Maximum_Product_of_Three_Numbers/HyeonJeong.java
@@ -5,11 +5,11 @@ import java.util.Arrays;
 class HyeonJeong {
     public int maximumProduct(int[] nums) {
         int result = 1;
-        Arrays.sort(nums);
+        Arrays.sort(nums); // 배열을 오름차순으로 정렬
         for (int i = nums.length-3; i<nums.length; i++) {
             result *= nums[i];
         }
-        if (nums[1] < 0) {
+        if (nums[1] < 0) { // 음수가 2개이상 존재하는 경우
             int n = nums[0]*nums[1]*nums[nums.length-1];
             if (result < n)
                 result = n;

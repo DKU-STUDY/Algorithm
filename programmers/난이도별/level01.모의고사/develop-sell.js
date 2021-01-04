5e343622f6eaa4dbc50069c6f0951238b490ef49
Seil Park <seilpark@Seilui-MacBookPro.local>
2021-01-05 00:00:02 +0900

    주석처리;

--- programmers/난이도별/level01.모의고사/develop-sell.js
+++ programmers/난이도별/level01.모의고사/develop-sell.js
@@ -16,3 +16,17 @@ function solution(answers) {
     
     return answer.filter((v, i) => (arr[i] >= max ? v : ""));
 }
+
+// 문제풀이: 
+// player 1,2,3의 답이 계속 중복되는데, 
+// 이때 중복되는 구간의 사이즈가 다르다. 1부터 5,8,10 사이즈 
+// 그래서 먼저 그 구간을 값을 넣은 배열로 const 지정해줬다.
+// 그 후 filter 함수를 통해 answers 배열과 맞는 경우만 탐색해서 그것의 length를 구해서
+// 맞은 갯수가 알게 되었다. 
+// 이때 p2[i % 8] 이런식으로 하면 딱 안 나눠지는 부분까지 캐치하게 된다 (중요 포인트!)
+
+// 그 후 값들의 가장 큰 값을 max 구해서, max과 같거나 큰 answer를 filter해서 제출한다. 
+
+// 배운점: 
+// Math.max 함수 존재
+// var max = Math.max(a1c,a2c,a3c);

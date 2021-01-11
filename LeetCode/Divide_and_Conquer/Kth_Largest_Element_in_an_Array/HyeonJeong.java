72cc7b20c9e4f642fde3f5afe04a21b55a0c72a8
H-yeonJeong <1212dlguswjd@daum.net>
2021-01-11 23:48:22 +0900

    [LeetCode] Kth Largest Element in an Array

---
+++ LeetCode/Divide_and_Conquer/Kth_Largest_Element_in_an_Array/HyeonJeong.java
@@ -0,0 +1,10 @@
+package LeetCode.Divide_and_Conquer.Kth_Largest_Element_in_an_Array;
+
+import java.util.Arrays;
+
+class HyeonJeong {
+    public int findKthLargest(int[] nums, int k) {
+        Arrays.sort(nums);
+        return nums[nums.length-k];
+    }
+}

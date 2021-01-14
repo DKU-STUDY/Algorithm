4324ea4393ece300d41ec799998457d99cdfb9b5
Jaewon Lee <67958329+Jaewon0702@users.noreply.github.com>
2021-01-14 16:24:46 +0900

    Update programmers/난이도별/level01.이상한_문자_만들기/Jaewon0702.py
    Co-authored-by: JunilHwang <tjsdlf4261@naver.com>

--- programmers/난이도별/level01.이상한_문자_만들기/Jaewon0702.py
+++ programmers/난이도별/level01.이상한_문자_만들기/Jaewon0702.py
@@ -1,11 +1,5 @@
 def solution(s):
-    ss = ""
-    s = s.split(" ")
-    for n in s:
-        for i, st in enumerate(n):
-            ss += st.lower() if i % 2 else st.upper()
-        ss += " "
-    return ss[:-1]
+    return " ".join([ "".join([ st.lower() if i % 2 else st.upper() for i, st in enumerate(n)  ]) for n in s.split(" ")])
 
 
 print(solution("try Hello world") == "TrY HeLlO WoRlD")

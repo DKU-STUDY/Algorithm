bfc7593cd8e785909e6ff2d1692b2df0f29b3b82
H-yeonJeong <1212dlguswjd@daum.net>
2021-02-02 19:25:34 +0900

    [LeetCode] Number of Recent Calls

--- LeetCode/Queue/Number_of_Recent_Calls/HyeonJeong.py
+++ LeetCode/Queue/Number_of_Recent_Calls/HyeonJeong.py
@@ -1,16 +1,14 @@
+from collections import deque
+
 class RecentCounter:
 # 최근 3000밀리 초 이내에 요청된 개수를 계산하는 클래스
     def __init__(self):
-        self.time = []
-        self.t = 0
+        self.time = deque()
 
     def ping(self, t: int) -> int:
-        newTime = []
-        for i in self.time:
-            if t-3000 <= i <= t:
-                newTime += [i]
-        newTime += [t]
-        self.time = newTime
-        if t == 0:
+        self.time.append(t) # 인덱스 0이 항상 존재하게 됨
+        while t-3000 > self.time[0]:
+            self.time.popleft() # 큐에서 처음 요소 제거
+        if len(self.time) == 0:
             return null
-        return len(newTime)
+        return len(self.time)

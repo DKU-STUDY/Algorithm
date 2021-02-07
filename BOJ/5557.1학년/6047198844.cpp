8f38428af43884bcec84e71734408df2d995a0eb
windsor <2615240@gmail.com>
2021-02-08 01:01:46 +0900

    수정

--- BOJ/5557.1학년/6047198844.cpp
+++ BOJ/5557.1학년/6047198844.cpp
@@ -4,16 +4,16 @@
 
 using namespace std;
 
-int cache[100][20];
+long long cache[100][21];
 int arr[100];
 int N;
 
-int dp(int idx, int sum) {
+long long dp(int idx, int sum) {
 	if (sum < 0 || sum>20)
 		return 0;
 	if (idx == N - 2)
 		return sum == arr[N - 1];
-	int &res = cache[idx][sum];
+	long long& res = cache[idx][sum];
 	if (res != -1)
 		return res;
 	res = 0;
@@ -22,9 +22,12 @@ int dp(int idx, int sum) {
 }
 
 int main() {
-	memset(cache, -1, sizeof(cache));
 	cin >> N;
 	for (int i = 0; i < N; i++)
-		cin >> arr[i];	
+		for (int j = 0; j < 21; j++)
+			cache[i][j] = -1;
+
+	for (int i = 0; i < N; i++)
+		cin >> arr[i];
 	cout << dp(0, arr[0]);
 }

1ee95ca153ef4a0289ac04789a33f59f73e726b1
H-yeonJeong <1212dlguswjd@daum.net>
2021-02-27 23:40:47 +0900

    Richest_Customer_Wealth

--- LeetCode/Array/Richest_Customer_Wealth/HyeonJeong.java
+++ LeetCode/Array/Richest_Customer_Wealth/HyeonJeong.java
@@ -2,8 +2,8 @@ package Richest_Customer_Wealth;
 
 class HyeonJeong {
     public int maximumWealth(int[][] accounts) {
-        int []sum = new int[accounts.length];
         int result = 0;
+        int []sum = new int[accounts.length];
         for (int i = 0; i < accounts.length; i++) {
             sum[i] = 0;
             for (int j = 0; j < accounts[0].length; j++) {

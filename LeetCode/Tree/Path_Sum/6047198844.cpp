ed2d75c21b00ef8df6c1f26277f6a42c7fe7811c
windsor <2615240@gmail.com>
2021-04-10 00:47:06 +0900

    [leetcode]Path_Sum

---
+++ LeetCode/Tree/Path_Sum/6047198844.cpp
@@ -0,0 +1,24 @@
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    bool hasPathSum(TreeNode* root, int targetSum) {
+        if(root==NULL)
+            return false;
+        
+        int _ = targetSum-root->val;
+        if(root->left==NULL&&root->right==NULL)
+            return !(_);
+        return hasPathSum(root->left,_)||hasPathSum(root->right,_);
+    }
+};

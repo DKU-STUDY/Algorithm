79046b1ffa18811a1c3b2039fda6b8e9d91d5770
windsor <2615240@gmail.com>
2021-05-02 19:35:44 +0900

    [leetcode]Add Two Numbers

--- LeetCode/Linked_List/Add_Two_Numbers/6047198844.py
+++ LeetCode/Linked_List/Add_Two_Numbers/6047198844.py
@@ -1,8 +1,9 @@
 # Definition for singly-linked list.
- class ListNode:
-     def __init__(self, val=0, next=None):
-         self.val = val
-         self.next = next
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
 class Solution:
     def addTwoNumbers(self, l1: ListNode, l2: ListNode) -> ListNode:
         carry = 0

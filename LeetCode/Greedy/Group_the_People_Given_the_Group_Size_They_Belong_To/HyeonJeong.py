1dcb28e324abc64cb62cebee232c0bc51a99a26f
H-yeonJeong <1212dlguswjd@daum.net>
2021-05-28 00:13:34 +0900

    [LeetCode] Group the People Given the Group Size They Belong To2

--- LeetCode/Greedy/Group_the_People_Given_the_Group_Size_They_Belong_To/HyeonJeong.py
+++ LeetCode/Greedy/Group_the_People_Given_the_Group_Size_They_Belong_To/HyeonJeong.py
@@ -3,6 +3,7 @@ class Solution:
         order_s = [[] for x in range(max(groupSizes))]
         result = []
         for i, x in enumerate(groupSizes):
+        # 그룹의 크기별로 사람들을 나누는 과정
             order_s[x-1] += [i]
         for i, n in enumerate(order_s):
             length = len(n)
@@ -11,6 +12,7 @@ class Solution:
             if length > i+1:
                 index = 0
                 for j in range(int(length/(i+1))):
+                # 필요한 크기로 그룹을 쪼개는 과정
                     result.append(n[index:index+(i+1)])
                     index += i+1
         return result

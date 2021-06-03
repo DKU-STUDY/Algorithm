7c7bc0015b8960d0335b64ec2cad40ce22d78992
windsor <2615240@gmail.com>
2021-06-03 22:53:33 +0900

    [leetcode]Maximum_Depth_of_Binary_Tree

---
+++ LeetCode/Tree/Maximum_Depth_of_Binary_Tree/6047198844.py
@@ -0,0 +1,12 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def maxDepth(self, root: TreeNode) -> int:
+        if not root:
+            return 0
+        return 1 + max(self.maxDepth(root.left), self.maxDepth(root.right))
+        

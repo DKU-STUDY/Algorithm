aa67a278f86ca0396b6f95c0f6b6131a63f2e0ab
windsor <2615240@gmail.com>
2021-06-08 14:00:02 +0900

    [leetcode]Kth Largest Element in an Array

---
+++ LeetCode/Divide_and_Conquer/Kth_Largest_Element_in_an_Array/6047198844.py
@@ -0,0 +1,8 @@
+class Solution:
+    def findKthLargest(self, nums: List[int], k: int) -> int:
+        heapq.heapify(nums)
+        
+        for _ in range(len(nums) - k):
+            heapq.heappop(nums)
+            
+        return heapq.heappop(nums)

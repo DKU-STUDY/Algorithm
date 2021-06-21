ae8fa2e9a922e7ff881d0dc77a7d5f6ebef3e4d7
windsor <2615240@gmail.com>
2021-06-21 21:06:59 +0900

    [programmers]가장 긴 팰린드롬(수정)

--- programmers/난이도별/level03.가장_긴_팰린드롬/6047198844.py
+++ programmers/난이도별/level03.가장_긴_팰린드롬/6047198844.py
@@ -1,6 +1,5 @@
 def solution(s):
     for l in range(len(s),0,-1):
         for i in range(0,len(s)-l+1):
-            check = s[i:i+l]
-            if check == check[::-1]:
+            if s[i:i+l] == s[i:i+l][::-1]:
                 return l

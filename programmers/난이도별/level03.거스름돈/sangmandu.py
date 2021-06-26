79685a73b7c9b88784f771a394cc43327d09540e
sangmandu <45033215+sangmandu@users.noreply.github.com>
2021-06-26 21:06:28 +0900

    Update sangmandu.py

--- programmers/난이도별/level03.거스름돈/sangmandu.py
+++ programmers/난이도별/level03.거스름돈/sangmandu.py
@@ -24,8 +24,7 @@ def solution(n, money):
     dp = [1] + [0] * n
     for coin in money:
         for price in range(coin, n + 1):
-            if price >= coin:
-                dp[price] += dp[price - coin]
+            dp[price] += dp[price - coin]
     return dp[n] % 1000000007
 '''
 bfs 문제로 풀었더니 시간초과.

2fe868fef9ee1bc4ea7b732154f741dc19467939
windsor <2615240@gmail.com>
2021-06-28 22:26:31 +0900

    [programmers]음양 더하기

--- programmers/난이도별/level01.음양_더하기/6047198844.py
+++ programmers/난이도별/level01.음양_더하기/6047198844.py
@@ -1,8 +1,2 @@
 def solution(absolutes, signs):
-    answer = 0
-    for absolute, sign in zip(absolutes, signs):
-        if sign:
-            answer += absolute
-        else:
-            answer -= absolute
-    return answer
+    return sum([absolute if sign else -absolute for absolute, sign in zip(absolutes, signs)])

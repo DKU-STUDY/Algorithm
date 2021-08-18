3f7f2ed36f44a36afcb8d9e252e91f197f85c8d1
이준수 <jslee@miridih.com>
2021-08-18 23:49:58 +0900

    [leetcode]Longest Valid Parentheses

--- programmers/난이도별/level04.지형_이동/6047198844.py
+++ programmers/난이도별/level04.지형_이동/6047198844.py
@@ -1,11 +1,32 @@
 import heapq
 import collections
 
+parent = list()
+
+
+def find(color):
+    if color == parent[color]:
+        return color
+    else:
+        p = find(parent[color])
+        parent[color] = p
+        return p
+
+
+def union(color1, color2):
+    color1 = find(color1)
+    color2 = find(color2)
+    if color1 != color2:
+        parent[color2] = color1
+        return True
+    return False
+
 
 def solution(land, height):
     answer = 0
     N = len(land)
     area = [[-1 for i in range(N)] for j in range(N)]
+    graph = set()
     color = 0
 
     # 각 구간에 색깔을 칠해보자.
@@ -16,48 +37,31 @@ def solution(land, height):
                 area[y][x] = color
                 while Q:
                     yy, xx = Q.pop()
+                    area[yy][xx] = color
                     # bfs시작
                     for dy, dx in (-1, 0), (+1, 0), (0, -1), (0, +1):
                         ny = yy + dy
                         nx = xx + dx
-                        if 0 <= ny < N and 0 <= nx < N and area[ny][nx] == -1 and abs(
-                                land[yy][xx] - land[ny][nx]) <= height:
-                            area[ny][nx] = color
-                            Q.append((ny, nx))
+                        # 범위 체크
+                        if 0 <= ny < N and 0 <= nx < N:
+                            color1 = area[ny][nx]
+                            color2 = area[yy][xx]
+
+                            weight = abs(land[ny][nx] - land[yy][xx])
+
+                            if color1 == -1 and weight <= height:
+                                Q.append((ny, nx))
+                            elif color1 != -1 and color1 != color2:
+                                graph.add((weight, color1, color2))
                 color += 1
+    global parent
+    parent = [i for i in range(color)]
+    graph = list(graph)
+    heapq.heapify(graph)
 
-    graph = [[0 for i in range(color)] for j in range(color)]
-    # 구간에서 다른 구간을 이동할때 설치해야하는 사다리에 대해 계산한다. -> 색깔이 다르면 구간을 구한뒤 구간거리 갱신.
-    for y in range(N):
-        for x in range(N):
-            for dy, dx in (-1, 0), (+1, 0), (0, -1), (0, +1):
-
-                ny = y + dy
-                nx = x + dx
-                if 0 <= ny < N and 0 <= nx < N:
-                    color1 = area[ny][nx]
-                    color2 = area[y][x]
-
-                    if color1 != color2:
-                        dif = abs(land[y][x] - land[ny][nx])
-                        if graph[color1][color2] == 0 or graph[color1][color2] > dif:
-                            graph[color1][color2] = dif
-                            graph[color2][color1] = dif
-
-    spanning_tree = set()
-
-    Q = [(0, 0)]
-    # 해당 정보를 토대로 구간의 루트를 계산한다. -> 어떻게? 최소비용 간선트리.
-    while Q and len(spanning_tree) != color:
-        cost, cur_color = heapq.heappop(Q)
-        if cur_color in spanning_tree:
-            continue
-        answer += cost
-        spanning_tree.add(cur_color)
-        for x in range(color):
-            if graph[cur_color][x] != 0 and x not in spanning_tree:
-                heapq.heappush(Q, (graph[cur_color][x], x))
+    while graph:
+        weight, color1, color2 = heapq.heappop(graph)
+        if union(color1, color2):
+            answer += weight
 
     return answer
-
-# 시간초과 . 추후 수정 필요.

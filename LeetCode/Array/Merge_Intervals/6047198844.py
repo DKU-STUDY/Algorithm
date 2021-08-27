3088939aa642487bdcff0e081e86c4f98f9f85d9
6047198844 <2615240@gmail.com>
2021-08-27 23:32:52 +0900

    [leetcode]Find First and Last Position of Element in Sorted Array

--- LeetCode/Array/Merge_Intervals/6047198844.py
+++ LeetCode/Array/Merge_Intervals/6047198844.py
@@ -1,28 +1,14 @@
 class Solution:
     def merge(self, intervals: List[List[int]]) -> List[List[int]]:
         ans = []
-
         intervals = sorted(intervals, key=lambda L: (L[0], -L[1]))
-        print(intervals)
-
         for nbegin, nend in intervals:
-            if not ans:
+            #ans가 없는 경우 && 교집합이 없는 경우
+            if not ans or ans[-1][1] < nbegin:
                 ans.append([nbegin, nend])
             else:
-                # 가장 최근에 추가된 interval
-                rbegin, rend = ans[-1]
-
                 # 교집합을 구한다.
-                # 교집합이 없는 경우
-                if rend < nbegin:
-                    ans.append([nbegin, nend])
                 # 교집합이 있는 경우
-                else:
-                    ans.pop()
-                    # 포함되는 경우.
-                    if nend <= rend:
-                        ans.append([rbegin, rend])
-                    # 교집합이 있는 경우.
-                    else:
-                        ans.append([rbegin, nend])
+                # 포함되는 경우.
+                ans[-1][1] = ans[-1][1] if nend <= ans[-1][1] else nend
         return ans

2711231163ed4b792570319330f3b5768151ef50
6047198844 <2615240@gmail.com>
2021-09-05 21:27:35 +0900

    [leetcode]Restore IP Addresses

--- LeetCode/Math/Unique Binary Search Trees/6047198844.py
+++ LeetCode/Math/Unique Binary Search Trees/6047198844.py
@@ -1,8 +1,8 @@
 class Solution:
     def numTrees(self, n: int) -> int:
-        memo = [0]*(n+2)
-        memo[1] = 1
-        for i in range(2, n+2):
+        memo = [0]*(n+1)
+        memo[0] = memo[1] = 1
+        for i in range(2, n+1):
             for j in range(i):
-                memo[i] += memo[j]*memo[i-j]
-        return memo[n+1]
+                memo[i] += memo[j]*memo[i-j-1]
+        return memo[n]

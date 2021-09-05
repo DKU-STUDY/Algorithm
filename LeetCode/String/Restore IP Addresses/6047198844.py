fa6a0b154131ca44fc2b858679468c65cd4fa945
6047198844 <2615240@gmail.com>
2021-09-05 21:33:51 +0900

    [leetcode]Restore IP Addresses

--- LeetCode/String/Restore IP Addresses/6047198844.py
+++ LeetCode/String/Restore IP Addresses/6047198844.py
@@ -1,20 +1,21 @@
 class Solution:
     def restoreIpAddresses(self, s: str) -> List[str]:
-        #최소4개~12개사이여야한다.
-        if not(4 <= len(s) <= 12):
+        # IP길이가 4개~12개사이여야한다.
+        if not (4 <= len(s) <= 12):
             return []
         res = []
-        for i in product([1,2,3],repeat=4) :
-            if sum(i)==len(s):
-                IP = []
+        for i in product([1, 2, 3], repeat=4):
+            if sum(i) == len(s):
+                IP = ''
                 pointer = 0
                 for j in i:
-                    k = s[pointer:pointer+j]
-                    if 0 <= int(k) <= 255 and (len(k)==1 or k[0] != '0'):
-                        IP.append(k)
+                    k = s[pointer:pointer + j]
+                    if 0 <= int(k) <= 255:
+                        IP += '.' + str(int(k))
                         pointer += j
                     else:
                         break
                 else:
-                    res.append('.'.join(IP))
+                    if len(IP) == len(s) + 4:
+                        res.append(IP[1:])
         return res

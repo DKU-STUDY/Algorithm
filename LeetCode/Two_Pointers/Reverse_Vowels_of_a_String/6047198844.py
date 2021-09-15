94d7f3c66d1d5bf15e12478aec7e79429e146459
6047198844 <2615240@gmail.com>
2021-09-15 23:14:43 +0900

    [leetcode]Reverse Vowels of a String

---
+++ LeetCode/Two_Pointers/Reverse_Vowels_of_a_String/6047198844.py
@@ -0,0 +1,18 @@
+class Solution:
+    def reverseVowels(self, s: str) -> str:
+        word = ''
+        N = len(s)
+        begin, end = 0, N - 1
+        vowels = set(['a', 'e', 'i', 'o', 'u'])
+
+        while begin < N:
+            if s[begin] in vowels:
+                while s[end] not in vowels:
+                    end -= 1
+                word += s[end]
+                end -= 1
+            else:
+                word += s[begin]
+            begin += 1
+
+        return word

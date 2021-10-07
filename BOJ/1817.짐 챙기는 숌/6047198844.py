a00876f63d11e43f8f29cea6c7261d3e58de34a2
6047198844 <2615240@gmail.com>
2021-10-07 23:38:28 +0900

    [백준]짐 챙기는 숍

--- BOJ/1817.짐 챙기는 숌/6047198844.py
+++ BOJ/1817.짐 챙기는 숌/6047198844.py
@@ -1,11 +1,14 @@
 N, M = map(int, input().split())
-books = map(int, input().split())
-ans = 1
-cur_weight = 0
-for book in books:
-    if cur_weight + book <= M:
-        cur_weight += book
-    else:
-        cur_weight = book
-        ans += 1
-print(ans)
+if N != 0:
+    books = map(int, input().split())
+    ans = 1
+    cur_weight = 0
+    for book in books:
+        if cur_weight + book <= M:
+            cur_weight += book
+        else:
+            cur_weight = book
+            ans += 1
+    print(ans)
+else:
+    print(0)

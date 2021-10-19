8243a32c77b7f9200ecd50bba5ba4ff440e90d96
6047198844 <2615240@gmail.com>
2021-10-20 00:11:26 +0900

    [백준]이진수 덧셈

--- BOJ/2729.이진수 덧셈/6047198844.py
+++ BOJ/2729.이진수 덧셈/6047198844.py
@@ -1,16 +1,17 @@
 T = int(input())
-res = '0'
-for _ in range(T):
-    A, B = input().split()
-    A, B = A[::-1], B[::-1]
-    acc = '0'
-    for idx in range(max(len(A), len(B))):
-        tmp = int(res[idx])
-        if idx < len(A):
-            tmp += int(A[idx])
-        if idx < len(B):
-            tmp += int(B[idx])
-        res[idx] = str(tmp % 2)
-        res += str(tmp // 2)
 
-    print(res)
+for _ in range(T):
+    A, B = input()[::-1].split()
+    acc = ['0']
+    for idx in range(max(len(A), len(B))):
+        cur = int(acc[idx])
+        if idx < len(A):
+            cur += int(A[idx])
+        if idx < len(B):
+            cur += int(B[idx])
+        acc[idx] = str(cur % 2)
+        acc.append(str(cur // 2))
+
+    while len(acc) > 1 and acc[-1] == '0':
+        acc.pop()
+    print(''.join(acc[::-1]))

d5cdf0968ec96b7a65bbfd23364c7fb8d20ebc2e
6047198844 <2615240@gmail.com>
2021-10-22 23:57:35 +0900

    [백준]단어 뒤집기2

---
+++ BOJ/17413.단어 뒤집기2/6047198844.py
@@ -0,0 +1,25 @@
+tag = ''
+word = ''
+res = ''
+
+S = input()
+for char in S:
+    if char == ' ':
+        res += word
+        word = ''
+        res += char
+    #flag가 false면 word. True면 tag라는 뜻이다.
+    if char == '<':
+        #쌓여있던 word를 뒤집어서 결과에 붙인다.
+        res += word[::-1]
+        word = ''
+        tag += char
+    elif char == '>':
+        tag += char
+        res += tag
+        tag = ''
+    elif tag == '':
+        word += char
+    else:
+        tag += char
+print(res)

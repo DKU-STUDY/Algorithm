c19df6bf9eefcf9e84c8867fe29ccf90fbf9275a
6047198844 <2615240@gmail.com>
2021-11-08 23:45:53 +0900

    [백준]가희와 3단 고음

--- LeetCode/String/Defanging_an_IP_Address/6047198844.py
+++ LeetCode/String/Defanging_an_IP_Address/6047198844.py
@@ -1,5 +1,3 @@
 class Solution:
     def defangIPaddr(self, address: str) -> str:
-        for _ in range(3):
-            address = address.replace(".","[.]")
-        return address
+        return address.replace(".","[.]")

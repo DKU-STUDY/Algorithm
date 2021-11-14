3dd923b9e035f426cbfd8a11ad4a0618677b6fed
6047198844 <2615240@gmail.com>
2021-11-14 23:59:10 +0900

    [백준]세 개의 소수 문제

--- BOJ/11502.세 개의 소수 문제/6047198844.py
+++ BOJ/11502.세 개의 소수 문제/6047198844.py
@@ -1,6 +1,6 @@
 import sys
 from collections import defaultdict
-from itertools import combinations
+from itertools import combinations, combinations_with_replacement
 
 is_prime = [True for _ in range(1001)]
 is_prime[0] = is_prime[1] = False
@@ -13,13 +13,14 @@ for i in range(1001):
 primes = [i for i in range(1001) if is_prime[i]]
 
 T = int(sys.stdin.readline().rstrip())
-tests = [sys.stdin.readline().rstrip() for _ in range(T)]
+tests = [int(sys.stdin.readline().rstrip()) for _ in range(T)]
 sols = defaultdict(tuple)
 
-for s in combinations(primes, 3):
+for s in combinations_with_replacement(primes, 3):
     acc = sum(s)
     if acc in tests:
         sols[acc] = tuple(s)
 
-print(sols)
+for test in tests:
+    print(sols[test])
 

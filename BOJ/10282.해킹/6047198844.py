e42b11efea774a65841feda6f88c45364975eee7
6047198844 <2615240@gmail.com>
2021-12-20 00:13:06 +0900

    [백준]해킹

--- BOJ/10282.해킹/6047198844.py
+++ BOJ/10282.해킹/6047198844.py
@@ -1,16 +1,17 @@
 # 현시점
 import heapq
+import sys
 from collections import defaultdict
 
-T = int(input())
+T = int(sys.stdin.readline())
 for _ in range(T):
     # 컴퓨터 대수 / 의존성 개수 / 해킹당한 컴퓨터 번호
-    n, d, c = map(int, input().split())
+    n, d, c = map(int, sys.stdin.readline().split())
 
     edges = defaultdict(list)
     for __ in range(d):
         # b가 감염시 s초 후에 a가 감염된다.
-        a, b, s = map(int, input().split())
+        a, b, s = map(int, sys.stdin.readline().split())
         edges[b].append((s, a))
 
     PQ = []
@@ -20,13 +21,17 @@ for _ in range(T):
     # 현재 시각
     cur = 0
     hacked_pc_num = 1
+    visited_pc = set()
+    visited_pc.add(c)
     while PQ:
-        hacked_pc_num += 1
-
         s, hacked_pc = heapq.heappop(PQ)
-        cur += s
+        if hacked_pc in visited_pc:
+            continue
+        hacked_pc_num += 1
+        cur = s
 
-        for pc, s in edges[hacked_pc]:
+        visited_pc.add(hacked_pc)
+        for s, pc in edges[hacked_pc]:
             heapq.heappush(PQ, (s + cur, pc))
 
     print(hacked_pc_num, cur)

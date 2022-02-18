e9bd387f21b599d57849cd999380facb588de8b1
6047198844 <2615240@gmail.com>
2022-02-18 16:59:57 +0900

    [백준]문제집

--- BOJ/10942.팰린드롬?/6047198844.py
+++ BOJ/10942.팰린드롬?/6047198844.py
@@ -11,19 +11,14 @@ memo = [[False for _ in range(N)] for _ in range(N)]
 # 구간의 길이가 1일때 -> 무조건 True
 # 구간의 길이가 2일때 -> 둘이 같으면 True
 
-for k in range(2):
-    for i in range(N - k):
-        j = i + k
-        memo[i][j] = numbers[i] == numbers[j]
-
 # 구간이 3이상일때
 # 3은 기저가 1이고, 4는 기저가 2이다.
 # 5는 3, 6은 4 ... 따라서 구간 1, 구간 2이 기저가된다.
 # 구간의 길이는 3에서 N - 1 이다.
-for k in range(2, N):
+for k in range(N):
     for i in range(N - k):
         j = i + k
-        if memo[i + 1][j - 1] and numbers[i] == numbers[j]:
+        if numbers[i] == numbers[j] and (k < 2 or memo[i + 1][j - 1]):
             memo[i][j] = True
 
 for _ in range(M):

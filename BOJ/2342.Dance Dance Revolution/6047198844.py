641ae83e7f28276070d113ce5491f16a1be70a0d
6047198844 <2615240@gmail.com>
2022-03-13 21:53:19 +0900

    [백준]조합

--- BOJ/2342.Dance Dance Revolution/6047198844.py
+++ BOJ/2342.Dance Dance Revolution/6047198844.py
@@ -19,18 +19,16 @@ for idx, command in enumerate(commands[:-1]):
         # 이전 왼발의 상태. 왼발은 움직인다.
         for move in range(4 + 1):
             if move == command:
-                memo[idx + 1][command][fix] = min(memo[idx + 1][command][fix], memo[idx][move][fix] + 1)
-                memo[idx + 1][fix][command] = min(memo[idx + 1][fix][command], memo[idx][fix][move] + 1)
+                dn = 1
             elif move == 0:
-                memo[idx + 1][command][fix] = min(memo[idx + 1][command][fix], memo[idx][move][fix] + 2)
-                memo[idx + 1][fix][command] = min(memo[idx + 1][fix][command], memo[idx][fix][move] + 2)
+                dn = 2
             elif ((move == 1 or move == 3) and (command == 2 or command == 4)) or (
                     (move == 2 or move == 4) and (command == 1 or command == 3)):
-                memo[idx + 1][command][fix] = min(memo[idx + 1][command][fix], memo[idx][move][fix] + 3)
-                memo[idx + 1][fix][command] = min(memo[idx + 1][fix][command], memo[idx][fix][move] + 3)
+                dn = 3
             else:
-                memo[idx + 1][command][fix] = min(memo[idx + 1][command][fix], memo[idx][move][fix] + 4)
-                memo[idx + 1][fix][command] = min(memo[idx + 1][fix][command], memo[idx][fix][move] + 4)
+                dn = 4
+            memo[idx + 1][command][fix] = min(memo[idx + 1][command][fix], memo[idx][move][fix] + dn)
+            memo[idx + 1][fix][command] = min(memo[idx + 1][fix][command], memo[idx][fix][move] + dn)
 
 res = math.inf
 for r in memo[-1]:

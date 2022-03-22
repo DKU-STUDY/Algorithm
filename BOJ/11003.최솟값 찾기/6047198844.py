1a7944f4f43c1d8c780b5f74a64c7e9644049c2c
6047198844 <2615240@gmail.com>
2022-03-22 23:37:20 +0900

    [백준]최소값 찾기

--- BOJ/11003.최솟값 찾기/6047198844.py
+++ BOJ/11003.최솟값 찾기/6047198844.py
@@ -17,32 +17,14 @@ from collections import deque
 N, L = map(int, input().split())
 arr = list(map(int, input().split()))
 
-res = deque([])
+res = deque()
 for i, num in enumerate(arr):
-    # res 최적화. 현재 범위에 맞지 않는 경우를 모두 없엔다.
-    while res and not (i-L+1 <= res[0][0] <= i):
+    # res 최적화. 현재 범위에 맞지 않는 경우 없엔다.
+    if res and not (i-L+1 <= res[0][0] <= i):
         res.popleft()
-
     # res가 없으면 삽입 -> 무조건 최소값임.
-    if not res:
-        res.append((i, num))
-    # res의 길이가 1이면 삽입 -> 무조건 최소값 또는 최소값 후보임.
-    elif len(res) == 1:
-        if res[0][1] >= num:
-            res.appendleft((i, num))
-        else:
-            res.append((i, num))
+    while res and res[-1][1] >= num:
+        res.pop()
+    res.append((i, num))
 
-    # res의 길이가 2일때.
-    # 현재값이 최소값이 될수도 있고, 최소값의 후보가 될수있다. 또 아무것도 아닐수도 있다.
-    else:
-        # 최소값인 경우
-        if res[0][1] >= num:
-            res.popleft()
-            res.appendleft((i, num))
-
-        # 최소값의 후보인 경우
-        elif res[1][1] >= num:
-            res.pop()
-            res.append((i, num))
     print(res[0][1], end=' ')

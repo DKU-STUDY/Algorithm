94bdf7e7590d6f69484d57752ced1034fe280e7e
6047198844 <2615240@gmail.com>
2022-04-06 23:56:17 +0900

    [백준]나부 함대 데이터

---
+++ BOJ/9654.나부 함대 데이터/6047198844.py
@@ -0,0 +1,6 @@
+print('SHIP NAME      CLASS          DEPLOYMENT IN SERVICE')
+print('N2 Bomber      Heavy Fighter  Limited    21        ')
+print('J-Type 327     Light Combat   Unlimited  1         ')
+print('NX Cruiser     Medium Fighter Limited    18        ')
+print('N1 Starfighter Medium Fighter Unlimited  25        ')
+print('Royal Cruiser  Light Combat   Limited    4         ')

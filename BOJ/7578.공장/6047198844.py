01db67adaeeb38592635bc377869a0f75359bc5b
6047198844 <2615240@gmail.com>
2022-04-14 23:41:54 +0900

    [백준]공장

--- BOJ/7578.공장/6047198844.py
+++ BOJ/7578.공장/6047198844.py
@@ -10,27 +10,28 @@ for num in sys.stdin.readline().split():
     line1[num] = idx
     idx += 1
 
-segment_tree = [0 for _ in range(N + 1)]
+tree = [0] * (N + 1)
 
 
-# init 필요없음
-# sum 현재구간 / 구하고자 하는구간
-def sum(sgi):
+def update(index, value):
+    while index < len(tree):
+        tree[index] += value
+        index += (index & -index)
+
+    return tree
+
+
+def query(index):
     res = 0
-    while sgi > 0:
-        res = res + segment_tree[sgi]
-        res = res - (res & -res)
+    while index > 0:
+        res += tree[index]
+        index -= (index & -index)
+
     return res
-
-
-def update(sgi):
-    while sgi < N + 1:
-        segment_tree[sgi] = segment_tree[sgi] + 1
-        sgi = sgi + (sgi & -sgi)
 
 
 res = 0
 for node in sys.stdin.readline().split():
-    res += sum(line1[node] + 1)
-    update(1, 1, N, line1[node])
+    res += query(N) - query(line1[node])
+    update(line1[node], 1)
 print(res)

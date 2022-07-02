a8af50b4305d9682dc3b1434051433022ded5f8e
6047198844 <2615240@gmail.com>
2022-07-02 23:59:51 +0900

    [백준]파스칼의 삼각형

--- BOJ/16395.파스칼의 삼각형/6047198844.py
+++ BOJ/16395.파스칼의 삼각형/6047198844.py
@@ -1,8 +1,9 @@
-pascal = [[1 for _ in range(i)] for i in range(1, 31)]
+# (n-1)! / (n-m)! * m!
+import sys
 
-for i in range(2, 30):
-    for j in range(1, i):
-        pascal[i][j] = pascal[i-1][j-1] + pascal[i-1][j]
+N, K = map(int, sys.stdin.readline().split())
 
-n, k = map(int, input().split())
-print(pascal[n-1][k-1])
+arr = [1]
+for i in range(1, 31):
+    arr.append(arr[-1] * i)
+print(arr[N - 1] // arr[(N - 1) - (K - 1)] // arr[K - 1])

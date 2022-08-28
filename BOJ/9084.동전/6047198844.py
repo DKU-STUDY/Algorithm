933121ac4e9f836be200e180efe68e75dc34edd6
6047198844 <2615240@gmail.com>
2022-08-28 23:10:44 +0900

    [백준]동전

--- BOJ/9084.동전/6047198844.py
+++ BOJ/9084.동전/6047198844.py
@@ -1,44 +1,25 @@
-# 문제
-# 다솜이는 은진이의 옆집에 새로 이사왔다. 다솜이는 자기 방 번호를 예쁜 플라스틱 숫자로 문에 붙이려고 한다.
-#
-# 다솜이의 옆집에서는 플라스틱 숫자를 한 세트로 판다. 한 세트에는 0번부터 9번까지 숫자가 하나씩 들어있다.
-# 다솜이의 방 번호가 주어졌을 때, 필요한 세트의 개수의 최솟값을 출력하시오. (6은 9를 뒤집어서 이용할 수 있고, 9는 6을 뒤집어서 이용할 수 있다.)
-#
-# 입력
-# 첫째 줄에 다솜이의 방 번호 N이 주어진다. N은 1,000,000보다 작거나 같은 자연수이다.
-#
-# 출력
-# 첫째 줄에 필요한 세트의 개수를 출력한다.
+import sys
 
-N = input()
-arr = []
-# 숫자 하나씩
-for i in N:
-    if not arr:
-        arr.append(set())
+T = int(sys.stdin.readline())
 
-    exists = True
-    # 전체 세트에 존재했는지 확인한다.
-    for s in arr:
-        # 6인데 6이 있고 9가 없는 경우
-        if i == '6' and i in s and '9' not in s:
-            s.add('9')
-            exists = False
-            break
-        # 9인데 9가 있고 6이 없는 경우
-        elif i == '9' and i in s and '6' not in s:
-            s.add('6')
-            exists = False
-            break
-        # i가 없는 경우
-        elif i not in s:
-            s.add(i)
-            exists = False
-            break
 
-    # 존재 했었다면.
-    if exists:
-        arr.append({i})
+# i: 현재 코인 인덱스
+# coins: 코인
+# K: 목표치
+def dp(K, coins):
+    memo = [0] * (K + 1)
+    # memo[K] = K 원을 만들 수 있는 경우의 수
+    # 0 원을 만드는 경우의 수는 하나이다.
+    memo[0] = 1
+    for coin in coins:
+        for i in range(K+1):
+            if i - coin >= 0:
+                memo[i] = memo[i] + memo[i-coin]
+    return memo[K]
 
-print(len(arr))
 
+for _ in range(T):
+    N = int(sys.stdin.readline())
+    coins = list(map(int, sys.stdin.readline().split()))
+    K = int(sys.stdin.readline())
+    print(dp(K, coins))

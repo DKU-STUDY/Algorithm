e8c9a53c1c5e0c4932e12f2e973291cc84ccaae0
6047198844 <2615240@gmail.com>
2022-09-14 00:11:40 +0900

    [백준]다이어트

--- BOJ/1484.다이어트/6047198844.py
+++ BOJ/1484.다이어트/6047198844.py
@@ -1,23 +1,24 @@
 import sys
 
 G = int(sys.stdin.readline())
-weight = [x for x in range(1, 100001)]
-N, M = 100000, 100000
-left, right = 0, 0  # 투 포인터
-arr = set()
+arr = list()
 # i = x+y
 # j = x-y
-ans = []
-while left < N and right < M:
-    tmp = (weight[left] + weight[right]) * (weight[left] - weight[right])
-    if tmp == G:
-        ans.append(weight[left])
-    if tmp < G:
-        left += 1
-    else:
-        right += 1
-if not ans:
+for i in range(G, 0, -1):
+    # 나누어 떨어진다면.
+    if G % i == 0:
+        j = G // i
+        if j > i:
+            break
+        x = (i + j) // 2
+        y = (i - j) // 2
+        if y > 0 and (i + j) % 2 == 0:
+            arr.append((i + j) // 2)
+arr.sort()
+if arr:
+    for i in arr:
+        print(i)
+else:
     print(-1)
-    exit()
-for y in ans:
-    print(y)
+
+# 4

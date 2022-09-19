e9eb4692a353160efb121a4b496260b4a517823f
6047198844 <2615240@gmail.com>
2022-09-19 23:54:51 +0900

    [백준]비밀번호 발음하기

--- BOJ/4659.비밀번호 발음하기/6047198844.py
+++ BOJ/4659.비밀번호 발음하기/6047198844.py
@@ -1,14 +1,29 @@
 moem = ['a', 'e', 'i', 'o', 'u']
 while True:
     line = input()
+    if line == 'end':
+        break
+
     moem_cnt = 0
     jaem_cnt = 0
-    for i in range(len(line) - 1):
-        if line[i] == line[i+1] and (line[i] != 'e' or line[i] != 'o'):
+    for i in range(len(line)):
+        if i > 1 and line[i] == line[i-1] and not (line[i] == 'e' or line[i] == 'o'):
+            print(f'<{line}> is not acceptable')
             break
         if line[i] in moem:
             moem_cnt += 1
             jaem_cnt = 0
         else:
-            moem_cnt = 1
-            jaem_cnt += 0
+            moem_cnt = 0
+            jaem_cnt += 1
+
+        if moem_cnt == 3 or jaem_cnt == 3:
+            print(f'<{line}> is not acceptable')
+            break
+    else:
+        for m in moem:
+            if m in line:
+                print(f'<{line}> is acceptable')
+                break
+        else:
+            print(f'<{line}> is not acceptable')

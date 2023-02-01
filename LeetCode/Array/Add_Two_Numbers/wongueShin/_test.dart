1bcaaf0d5751e1c8b082fb3d69e2c659b6dfb85f
신원규 <won7963kr@gmail.com>
2023-02-02 00:58:19 +0900

    [Leetcode] 2.Add_Two_Numbers

---
+++ LeetCode/Array/Add_Two_Numbers/wongueShin/_test.dart
@@ -0,0 +1,33 @@
+import 'package:flutter_test/flutter_test.dart';
+import './solution.dart';
+
+List<int> _setUpTest(List<int> nums, int target) {
+  final solution = Solution();
+  return solution.twoSum(nums, target);
+}
+
+void main() {
+  test(
+    "case01",
+    () => expect(
+      _setUpTest([2, 7, 11, 15], 9),
+      equals([0, 1]),
+    ),
+  );
+
+  test(
+    "case02",
+    () => expect(
+      _setUpTest([3, 2, 4], 6),
+      equals([1, 2]),
+    ),
+  );
+
+  test(
+    "case03",
+    () => expect(
+      _setUpTest([3, 3], 6),
+      equals([0, 1]),
+    ),
+  );
+}
